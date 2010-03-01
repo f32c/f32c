ed5c7b5eaab2dbc0a37f161239d08b2e97f1da97
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2010-03-01 09:39:02 +0000

    Add a header file for mul / div emulation functions.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@20 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ prog/math.h
@@ -0,0 +1,12 @@
+
+
+#ifndef _MATH_H_
+#define _MATH_H_
+
+unsigned long __mulsi3(unsigned long a, unsigned long b);
+unsigned long __udivsi3(unsigned long a, unsigned long b);
+long __divsi3(long a, long b);
+unsigned long __umodsi3(unsigned long a, unsigned long b);
+
+#endif /* !_MATH_H_ */
+
