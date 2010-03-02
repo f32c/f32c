bdc9c65d0693eff630c1b0e0d07a0478e5ed0e73
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2010-03-02 02:18:12 +0000

    Add three different demo semaphore programs, selectable via pressing the rotary knob down.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@25 6164e1d0-f123-df11-80f4-0019d13f85ef

--- prog/libc.c
+++ prog/libc.c
@@ -2,18 +2,34 @@
 #include "io.h"
 #include "libc.h"
 
-void msleep(int ms)
+int newkey;
+int oldkey;
+
+int msleep(int ms)
 {
 	int ticks, start, current;
+	int debounce = 0;
 
 	/* approximately ticks = ms * 50000 */
 	ticks = (ms << 15) + (ms << 14) + (ms << 9) + (ms << 8);
 
 	INW(start, IO_TSC);
 	do {
+		INW(newkey, IO_LED);
+		newkey &= 0x0100;
+		if (newkey != oldkey)
+			debounce++;
+		if (debounce > 10000) {
+			if (newkey)
+				return (1);		/* Button pressed */
+			else
+				oldkey = newkey;	/* Button released */
+		}
+
 		INW(current, IO_TSC);
 	} while (current - start < (ticks));
 
+	return (0);
 }
 
 void bcopy(const char *src, char *dst, int len)
--- prog/libc.h
+++ prog/libc.h
@@ -1,7 +1,11 @@
 
 
-void msleep(int);
 void bcopy(const char *, char *, int);
 void memset(char *, int, int);
 int strlen(const char *);
+int msleep(int);
+
+extern int keymask;
+extern int newkey;
+extern int oldkey;
 
