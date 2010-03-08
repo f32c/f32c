fa4caa827445a08fe29e7488a1ea93ab2d91d3c8
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2010-03-08 22:24:25 +0000

    Rearrange barrel shifter pipeline so that shift by 8 and 16 bits takes place in EX stage, and shift by 1, 2 and 4 bits takes place in MEM stage.  Future change will hopefully leverage on such an arrangement, so that parts of the EX part of the barrel shifter can be used for aligning memory loads and stores.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@46 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/shift.vhd
+++ rtl/shift.vhd
@@ -34,64 +34,67 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity shift is
 	port(
-		shamt_1_2: in std_logic_vector(1 downto 0);
-		shamt_4_8_16: in std_logic_vector(2 downto 0);
-		funct_1_2, funct_4_8_16: in std_logic_vector(1 downto 0);
-		stage1_in, stage4_in: in std_logic_vector(31 downto 0);
-		stage2_out, stage16_out: out std_logic_vector(31 downto 0)
+		shamt_8_16: in std_logic_vector(1 downto 0);
+		shamt_1_2_4: in std_logic_vector(2 downto 0);
+		funct_8_16, funct_1_2_4: in std_logic_vector(1 downto 0);
+		stage8_in, stage1_in: in std_logic_vector(31 downto 0);
+		stage16_out, stage4_out: out std_logic_vector(31 downto 0)
 	);
 end shift;
 
 architecture Behavioral of shift is
-	signal stage1, stage4, stage8: std_logic_vector(31 downto 0);
+	signal stage1, stage2, stage8: std_logic_vector(31 downto 0);
 	signal sel16, sel8, sel4, sel2, sel1: std_logic_vector(1 downto 0);
 begin
-	sel1 <= funct_1_2 when shamt_1_2(0) = '1' else "01";
-	sel2 <= funct_1_2 when shamt_1_2(1) = '1' else "01";
-	sel4 <= funct_4_8_16 when shamt_4_8_16(0) = '1' else "01";
-	sel8 <= funct_4_8_16 when shamt_4_8_16(1) = '1' else "01";
-	sel16 <= funct_4_8_16 when shamt_4_8_16(2) = '1' else "01";
+	-- shift by 8 and 16 occurs in EX stage; shift by 1, 2 and 4 is in MEM stage
+
+	sel8 <= funct_8_16 when shamt_8_16(0) = '1' else "01";
+	sel16 <= funct_8_16 when shamt_8_16(1) = '1' else "01";
+	sel1 <= funct_1_2_4 when shamt_1_2_4(0) = '1' else "01";
+	sel2 <= funct_1_2_4 when shamt_1_2_4(1) = '1' else "01";
+	sel4 <= funct_1_2_4 when shamt_1_2_4(2) = '1' else "01";
 
 	-- sel: "00" left logical, "10" right logical, "11" right artihm, "01" bypass
 	
-	with sel1 select
-		stage1 <= stage1_in(30 downto 0) & "0" when "00",
-						"0" & stage1_in(31 downto 1) when "10",
-						stage1_in(31) & stage1_in(31 downto 1) when "11",
-						stage1_in when others;
-
-	with sel2 select
-		stage2_out <= stage1(29 downto 0) & "00" when "00",
-						"00" & stage1(31 downto 2) when "10",
-						stage1(31) & stage1(31) & stage1(31 downto 2) when "11",
-						stage1 when others;
-
-	-- the bottom part is separated from the upper by a register on EX to MEM boundary.
-
-	with sel4 select
-		stage4 <= stage4_in(27 downto 0) & x"0" when "00",
-						x"0" & stage4_in(31 downto 4) when "10",
-						stage4_in(31) & stage4_in(31) & stage4_in(31) & stage4_in(31) &
-						stage4_in(31 downto 4) when "11",
-						stage4_in when others;
-   	
 	with sel8 select
-		stage8 <= stage4(23 downto 0) & x"00" when "00",
-						x"00" & stage4(31 downto 8) when "10",
-						stage4(31) & stage4(31) & stage4(31) & stage4(31) &
-						stage4(31) & stage4(31) & stage4(31) & stage4(31) &
-						stage4(31 downto 8) when "11",
-						stage4 when others;
+		stage8 <= stage8_in(23 downto 0) & x"00" when "00",
+			x"00" & stage8_in(31 downto 8) when "10",
+			stage8_in(31) & stage8_in(31) & stage8_in(31) & stage8_in(31) &
+			stage8_in(31) & stage8_in(31) & stage8_in(31) & stage8_in(31) &
+			stage8_in(31 downto 8) when "11",
+			stage8_in when others;
 
 	with sel16 select
 		stage16_out <= stage8(15 downto 0) & x"0000" when "00",
-						x"0000" & stage8(31 downto 16) when "10",
-						stage8(31) & stage8(31) & stage8(31) & stage8(31) &
-						stage8(31) & stage8(31) & stage8(31) & stage8(31) &
-						stage8(31) & stage8(31) & stage8(31) & stage8(31) &
-						stage8(31) & stage8(31) & stage8(31) & stage8(31) &
-						stage8(31 downto 16) when "11",
-						stage8 when others;
+			x"0000" & stage8(31 downto 16) when "10",
+			stage8(31) & stage8(31) & stage8(31) & stage8(31) &
+			stage8(31) & stage8(31) & stage8(31) & stage8(31) &
+			stage8(31) & stage8(31) & stage8(31) & stage8(31) &
+			stage8(31) & stage8(31) & stage8(31) & stage8(31) &
+			stage8(31 downto 16) when "11",
+			stage8 when others;
 	
+	-- the bottom part is separated from the upper by a register
+	-- on EX to MEM boundary.
+
+	with sel1 select
+		stage1 <= stage1_in(30 downto 0) & "0" when "00",
+			"0" & stage1_in(31 downto 1) when "10",
+			stage1_in(31) & stage1_in(31 downto 1) when "11",
+			stage1_in when others;
+
+	with sel2 select
+		stage2 <= stage1(29 downto 0) & "00" when "00",
+			"00" & stage1(31 downto 2) when "10",
+			stage1(31) & stage1(31) & stage1(31 downto 2) when "11",
+			stage1 when others;
+
+	with sel4 select
+		stage4_out <= stage2(27 downto 0) & x"0" when "00",
+			x"0" & stage2(31 downto 4) when "10",
+			stage2(31) & stage2(31) & stage2(31) & stage2(31) &
+			stage2(31 downto 4) when "11",
+			stage2 when others;
+   	
 end Behavioral;
 
