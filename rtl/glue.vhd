9254172144b49d7b17bda43d96ffc52babb933f6
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2010-03-13 08:02:22 +0000

    Decouple ID_running from MEM_take_branch.  This change has no functional impact (yet).
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@62 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/glue.vhd
+++ rtl/glue.vhd
@@ -40,10 +40,10 @@ entity glue is
 		reg_trace: string := "true";
 		bus_trace: string := "true" -- XXX reduce clock rate if bus_trace enabled!
 --
--- Number of Slice Flip Flops:           826 out of  11,776    7%
--- Number of 4 input LUTs:             2,425 out of  11,776   20%
--- Number of occupied Slices:          1,403 out of   5,888   23%
--- Total Number of 4 input LUTs:       2,565 out of  11,776   21%
+-- Number of Slice Flip Flops:           825 out of  11,776    7%
+-- Number of 4 input LUTs:             2,427 out of  11,776   20%
+-- Number of occupied Slices:          1,405 out of   5,888   23%
+-- Total Number of 4 input LUTs:       2,562 out of  11,776   21%
 --
 	);
 	port (
