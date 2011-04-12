8def33563b5c58ef4edfbe2bb1d7795969be5241
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-12 22:06:12 +0000

    Add a small (16 bytes) RX buffer to sio routines, in order to reduce the chances of losing inbound characters while sending data to RS232.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@174 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/sio.c
+++ src/lib/sio.c
@@ -2,26 +2,51 @@
 #include <io.h>
 #include <sio.h>
 
+#define	SIO_RXBUFSIZE	16
+#define	SIO_RXBUFMASK	0x0f
+
+static char sio_rxbuf[SIO_RXBUFSIZE];
+static int sio_rxbuf_head = 1;
+static int sio_rxbuf_tail = 1;
+
+
+static int
+sio_probe_rx(void)
+{
+	register int c;
+
+	INW(c, IO_SIO);
+	if (c & SIO_RX_BYTES) {
+		sio_rxbuf[sio_rxbuf_head++] = c >> 8;
+		sio_rxbuf_head &= SIO_RXBUFMASK;
+	}
+	return(c);
+}
+
 
 int
 sio_getchar(void)
 {
 	register int c;
 
+	/* Any new characters received from RS-232? */
 	do {
-		INW(c, IO_SIO);
-	} while ((c & SIO_RX_BYTES) == 0);
+		sio_probe_rx();
+	} while (sio_rxbuf_head == sio_rxbuf_tail);
 
-	return (c >> 8);
+	c = sio_rxbuf[sio_rxbuf_tail++];
+	sio_rxbuf_tail &= SIO_RXBUFMASK;
+	return (c);
 }
+
 
 void
 sio_putchar(int c)
 {
-	register int status;
+	register int in;
 
 	do {
-		INW(status, IO_SIO);
-	} while (status & SIO_TX_BUSY);
+		in = sio_probe_rx();
+	} while (in & SIO_TX_BUSY);
 	OUTB(IO_SIO, c);
 }
