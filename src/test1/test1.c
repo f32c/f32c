81d43c047a829c2a180c040f07856c6ffc9d38fb
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-13 20:38:20 +0000

    Update test program with printf() tests.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@186 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test1.c
+++ src/test1/test1.c
@@ -1,30 +1,47 @@
 
-#include <types.h>
+#ifndef XXX
 #include <io.h>
 #include <sio.h>
+#include <stdio.h>
+#include <types.h>
+#else
+#include <stdio.h>
+#define sio_getchar() getchar()
+#define sio_putchar(c) putchar(c)
+#endif
 
-static char *msg = "\n\rFER ULXP2 board rev 0; f32c core\r\n";
 
 int
 main(void)
 {
+	int cnt = 0;
 	int c;
-	char *cp;
 	
 	do {
 		c = sio_getchar();
 
-		if (c == '\r') {
-			for (cp = msg; *cp != 0; cp++)
-				sio_putchar(*cp);
+		if (c == '\r' || c == '\n') {
+			printf("\n");
+			printf("Hello, world!\n");
+			printf("  %%\n");
+			printf("  s: %s (null %s)\n", "Hello, world!", NULL);
+			printf("  c: %c\n", '0' + (cnt & 0x3f));
+			printf("  d: cnt = %d (neg %d)\n", cnt, -cnt);
+			printf(" 8d: cnt = %8d (neg %8d)\n", cnt, -cnt);
+			printf("08d: cnt = %08d (neg %08d)\n", cnt, -cnt);
+			printf("  u: cnt = %u (neg %u)\n", cnt, -cnt);
+			printf("  y: cnt = %y (neg %y)\n", cnt, -cnt);
+			printf("  x: cnt = %x (neg %x)\n", cnt, -cnt);
+			printf(" 8x: cnt = %8x (neg %8x)\n", cnt, -cnt);
+			printf("08x: cnt = %08x (neg %08x)\n", cnt, -cnt);
+			printf("  p: cnt = %p (neg %p)\n", cnt, -cnt);
+			printf("  o: cnt = %o (neg %o)\n", cnt, -cnt);
+			printf("  b: cnt = %b (neg %b)\n", cnt, -cnt);
+			cnt++;
 			continue;
 		}
 
-		// Convert to upper case
-		if (c >= 'a' && c <= 'z')
-			c -= 32;
-
 		sio_putchar(c);
-	} while (1);
+	} while (cnt < 100);
 }
 
