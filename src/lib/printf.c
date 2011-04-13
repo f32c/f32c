b247e7eafc28e87c3f1d09fc164ec2d3284bdc04
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-13 14:22:06 +0000

    Add padding and field width specification capability to printf().
    Fix %p format.
    
    Add the copyright notice from the original FreeBSD source file.
    
    
    
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@182 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/printf.c
+++ src/lib/printf.c
@@ -1,3 +1,38 @@
+/*-
+ * Copyright (c) 1986, 1988, 1991, 1993
+ *      The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2011 University of Zagreb
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *                      
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 
 #include <sio.h>
 #include <stdarg.h>
@@ -15,7 +50,8 @@ vprintf(char const *fmt, va_list ap)
 	char *cp;
 	u_int num;
 	int ch;
-	int base, sign, neg, n;
+	int base, sign, neg, n, width;
+	char padc;
 	int retval = 0;
 
 	for (;;) {
@@ -31,13 +67,36 @@ vprintf(char const *fmt, va_list ap)
 		sign = 0;
 		neg = 0;
 		base = 10;
+		padc = ' ';
+		width = 0;
 		
+reswitch:
 		switch (ch = (u_char)*fmt++) {
 		case 0:
 			return (-1);	/* XXX use proper errno */
 		case '%':
 			PCHAR(ch);
 			break;
+		case '0':
+			padc = '0';
+			goto reswitch;
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			for (n = 0;; ++fmt) {
+				n = n * 10 + ch - '0';
+				ch = *fmt;
+				if (ch < '0' || ch > '9')
+				break;
+			}
+			width = n;
+			goto reswitch;
 		case 's':
 			cp = va_arg(ap, char *);
 			if (cp == NULL)
@@ -60,7 +119,10 @@ vprintf(char const *fmt, va_list ap)
 		case 'u':
 			goto handle_nosign;
 		case 'p':
-			/* XXX fixme */
+			width = 8;
+			padc = '0';
+			PCHAR(padc);
+			PCHAR('x');
 		case 'x':
 			base = 16;
 			goto handle_nosign;
@@ -88,8 +150,16 @@ number:
 				num /= base;
 				n++;
 			} while (num != 0);
-			if (neg)
+
+			if (neg && padc == '0')
 				PCHAR('-');
+
+			for (;width > n + neg; width--)
+				PCHAR(padc);
+
+			if (neg && padc != '0')
+				PCHAR('-');
+
 			for (; n > 0;) {
 				PCHAR(nbuf[--n]);
 			}
