b8cac1d8009ed0e2786164b6cddc8915f0c199f7
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-16 07:38:25 +0000

    The program is supposed to program SPI flash, but it doesn't work yet. Commit it in its current state for further reference.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@201 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test1.c
+++ src/test1/test1.c
@@ -10,10 +10,117 @@
 int
 main(void)
 {
+	int wav_stream_locked = 0;
 	int cnt = 0;
+	int pcm_out = 0;
 	int c = '\n';
+	int i = 0;
 	
+	printf("\n");
+	OUTB(IO_LED, 0xf0);
+	spi_stop_transaction();
+	/* SPI: Read JEDEC ID */
+	spi_start_transaction();
+	spi_byte(0x9f);	/* JEDEC Read-ID */
+	printf("%02x", spi_byte(0) & 0xff);
+	printf("%02x", spi_byte(0) & 0xff);
+	printf("%02x\n\n", spi_byte(0) & 0xff);
+	spi_stop_transaction();
+
+	/* SPI: Read a few bytes, starting at addr 0x000000 */
+	spi_start_transaction();
+	spi_byte(0x0b);	/* High-speed read */
+	spi_byte(0x0);
+	spi_byte(0x0);
+	spi_byte(0x0);
+	spi_byte(0x0);
+	for (c = 0; c < 16; c++) {
+		for (i = 0; i < 4; i++)
+			printf("%02x", spi_byte(0) & 0xff);
+		printf("\n");
+	}
+	spi_stop_transaction();
+
+	/* SPI: Poll status */
+	spi_start_transaction();
+	spi_byte(0x05); /* RDSR */
+	printf("%02x\n\n", spi_byte(0) & 0xff);
+	spi_stop_transaction();
+
+	/* SPI: Send WREN command */
+	spi_start_transaction();
+	spi_byte(0x06); /* WREN */
+	spi_stop_transaction();
+
+	/* SPI: Poll status */
+	spi_start_transaction();
+	spi_byte(0x05); /* RDSR */
+	printf("%02x\n\n", spi_byte(0) & 0xff);
+	spi_stop_transaction();
+
+	/* SPI: Send AAI word-program command, starting at addr 0x000000 */
+	spi_start_transaction();
+	spi_byte(0xad); /* AAI */
+	spi_byte(0x0);
+	spi_byte(0x0);
+	spi_byte(0x0);
+	/* SPI: Send two dummy bytes */
+	spi_byte(0x2);
+	spi_byte(0x3);
+	spi_stop_transaction();
+
 	do {
+		c = getchar();
+		pcm_out <<= 8;
+		pcm_out |= (c & 0xff);
+		if (!wav_stream_locked) {
+			/* Search for the "WAVE" pattern */
+			if (pcm_out == 0x57415645) {
+				wav_stream_locked = 1;
+				OUTB(IO_LED, 0x0f);
+				/* SPI: store two dummy bytes */
+				spi_start_transaction();
+				spi_byte(0xad); /* AAI */
+				spi_byte(0x0);
+				spi_byte(0x1);
+				spi_stop_transaction();
+			}
+			continue;
+		}
+
+		/* Terminate AAI write sequence */
+		spi_start_transaction();
+		spi_byte(0x04); /* WRDI */
+		spi_stop_transaction();
+		OUTB(IO_LED, 0xff);
+
+		i += 1;
+		if (i == 4) {
+			/* Byte swapping */
+			pcm_out = ((pcm_out >> 8) & 0x00ff00ff) |
+			    ((pcm_out << 8) & 0xff00ff00);
+			pcm_out = pcm_out ^ 0x80008000;
+			OUTW(IO_PCM, pcm_out);
+			i = 0;
+		}
+	} while (1);
+
+	cnt = 0;
+	do {
+		i = c;
+		c = getchar();
+		if (i == c)
+			cnt++;
+		else
+			cnt = 0;
+	} while (cnt < 5);
+
+	return (0);
+}
+
+#if 0
+		continue;
+
 		if (c == '\r' || c == '\n') {
 			printf("\n");
 			printf("Hello, world!\n");
@@ -34,6 +141,31 @@ main(void)
 			cnt++;
 		}
 
+		vol = 100;
+		pcm_out = 0x4000;
+		for (c = 0; c < 30000; c++) {
+			pcm_out = -pcm_out;
+			i = (c / 5000) + 9;
+			if (vol != i) {
+				vol = i;
+				printf("Vol: %d\n", i);
+				i = getchar();
+				/* Exit to bootloader on CTRL+C */
+				if (i == 3)
+					return(0);
+			}
+			i = (((pcm_out << 16) + 0x80000000) >> (vol + 16)) & 0xffff;
+			OUTW(IO_PCM, i + (i << 16));
+			i = rdtsc();
+			do {
+				j = rdtsc() - i;
+				if (j < 0)
+					j = -j;
+			} while (j < 40000);
+		}
+continue;
+		printf("Done.\n");
+
 		c = getchar();
 
 		/* Exit to bootloader on CTRL+C */
@@ -42,5 +174,5 @@ main(void)
 
 		putchar(c);
 	} while (cnt < 100);
-}
 
+#endif
