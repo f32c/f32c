efd2a661fb59acd3406d2e000714becb15ea9a20
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-17 20:09:46 +0000

    Add more basic types, and byteorder manipulation null-macros.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@203 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/types.h
+++ src/include/types.h
@@ -1,4 +1,6 @@
 
+#ifndef _TYPES_H_
+#define _TYPES_H_
 
 /* types and consts */
 #define	NULL		((void *) 0)
@@ -14,8 +16,13 @@ typedef	unsigned int		__uint32_t;
 typedef	long long		__int64_t;
 typedef	unsigned long long	__uint64_t;
 
+typedef	__int32_t		__intptr_t;
+typedef	__uint32_t		__uintptr_t;
+
 typedef	__uint8_t		u_char;
+typedef	__uint16_t		u_short;
 typedef	__uint32_t		u_int;
+typedef	__uint64_t		u_long;
 
 /* POSIX sized integers */
 typedef	__int8_t		int8_t;
@@ -26,4 +33,14 @@ typedef	__uint8_t		uint8_t;
 typedef	__uint16_t		uint16_t;
 typedef	__uint32_t		uint32_t;
 typedef	__uint64_t		uint64_t;
+typedef	__intptr_t		intptr_t;
+typedef	__uintptr_t		uintptr_t;
+
+/* Byteorder manipulation null-macros */
+#define	htonl(x)		(x)
+#define	htons(x)		(x)
+#define	ntohl(x)		(x)
+#define	ntohs(x)		(x)
+
+#endif /* !_TYPES_H_ */
 
