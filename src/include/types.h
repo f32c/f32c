1699872d544aa1189004dc7e8362efbdb65d034c
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-17 08:24:13 +0000

    Add definitions of POSIX integer types.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@202 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/types.h
+++ src/include/types.h
@@ -16,3 +16,14 @@ typedef	unsigned long long	__uint64_t;
 
 typedef	__uint8_t		u_char;
 typedef	__uint32_t		u_int;
+
+/* POSIX sized integers */
+typedef	__int8_t		int8_t;
+typedef	__int16_t		int16_t;
+typedef	__int32_t		int32_t;
+typedef	__int64_t		int64_t;
+typedef	__uint8_t		uint8_t;
+typedef	__uint16_t		uint16_t;
+typedef	__uint32_t		uint32_t;
+typedef	__uint64_t		uint64_t;
+
