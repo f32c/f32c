ba154e96c56db62aa655e209ffd044c622b5fa42
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-19 15:00:11 +0000

    First attempt to make host and FPGA card exchange some formatted data.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@212 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/fprog/fprog.c
+++ src/fprog/fprog.c
@@ -1,7 +1,13 @@
 
+#ifndef X
 #include <io.h>
 #include <sio.h>
 #include <types.h>
+#else
+#include <sys/types.h>
+#include <ftdi.h>
+#include <fcntl.h>
+#endif
 #include <stdio.h>
 
 
@@ -14,7 +20,7 @@
 #define	FP_CMD_SET_BAUD		6
 #define	FP_CMD_DONE		7
 
-#define	FP_START_MARK		0x7e81
+#define	FP_START_MARK		0x817e
 
 #define	FP_PAGESIZE		4096
 
@@ -24,17 +30,46 @@ typedef struct fp_header {
 	uint16_t	start_mark;
 	uint8_t		fp_cmd;
 	uint8_t		fp_res;
+	uint32_t	fp_addr;
 	uint32_t	payload_csum;
 	uint16_t	payload_len;
 	uint16_t	header_csum;
 } fph_t;
 
 
-static char buf[FP_MAX_BLOCKSIZE];
+static char buf[FP_MAX_BLOCKSIZE + 8];
 
 
+#ifndef X
 #define	rxbyte()	getchar()
 #define	txbyte(x)	putchar(x)
+#else
+#ifdef FTDI
+static struct ftdi_context fc;  /* USB port handle */
+static int
+rxbyte(void)
+{
+	char b, res;
+	
+	do {
+		res = ftdi_read_data(&fc, &b, 1);
+	} while (res != 1);
+	return(b);
+}
+#else
+static int sioid;
+static int
+rxbyte(void)
+{
+	char b, res;
+	
+	do {
+		res = read(sioid, &b, 1);
+	} while (res != 1);
+	return(b);
+}
+#endif
+#endif
 
 
 static int
@@ -46,7 +81,6 @@ rx_frame(void)
 	int csum;
 
 	cp = buf;
-	fphp = (fph_t *) buf;
 	fphp->start_mark = FP_START_MARK;
 	
 	do {
@@ -54,9 +88,9 @@ rx_frame(void)
 
 		/* Detect start of record marker */
 		if (cp < &buf[2]) {
-			if (cp == &buf[0] && c == buf[0])
+			if (cp == &buf[0] && c == buf[0]) {
 				cp++;
-			else if (cp == &buf[1]) {
+			} else if (cp == &buf[1]) {
 				if (c == buf[1])
 					cp++;
 				else 
@@ -70,19 +104,21 @@ rx_frame(void)
 		if (cp == &buf[sizeof(*fphp)]) {
 			for (csum = 0, cp = buf;
 			    cp < (char *) &fphp->header_csum; cp++)
-				csum += *cp;
-			if (csum != ntohs(fphp->header_csum))
+				csum += (*cp & 0xff);
+			if (csum != fphp->header_csum)
 				return (-1);
+			if (fphp->payload_len > FP_PAGESIZE)
+				return (-2);
 			cp = &buf[sizeof(*fphp)];
 		}
 
-	} while (cp < &buf[sizeof(*fphp) + ntohs(fphp->payload_len)]);
+	} while (cp < &buf[sizeof(*fphp) + fphp->payload_len]);
 
 	/* Payload checksum OK? */
 	for (csum = 0; cp > &buf[sizeof(*fphp)];)
-		csum += *--cp;
-	if (csum != ntohl(fphp->payload_csum))
-		return (-1);
+		csum += (*--cp & 0xff);
+	if (csum != fphp->payload_csum)
+		return (-3);
 	else
 		return(0);
 }
@@ -93,38 +129,125 @@ tx_frame(void)
 {
 	char *cp;
 	fph_t *fphp = (fph_t *) buf;
-	int csum;
+	int csum, len;
 	int i;
+
+#ifdef FTDI
+	ftdi_usb_purge_buffers(&fc);
+#endif
 
 	fphp->start_mark = FP_START_MARK;
 
 	/* Compute payload checksum */
 	for (csum = 0, cp = &buf[sizeof(*fphp)];
 	    cp < &buf[sizeof(*fphp) + fphp->payload_len]; cp++)
-		csum += *cp;
+		csum += (*cp & 0xff);
 	fphp->payload_csum = csum;
 
 	/* Compute header checsum */
 	for (csum = 0, cp = buf; cp < (char *) &fphp->header_csum; cp++)
-		csum += *cp;
+		csum += (*cp & 0xff);
 	fphp->header_csum = csum;
 
-	/* Send it */
-	for (cp = buf; cp < &buf[sizeof(*fphp) + fphp->payload_len]; cp++)
-		txbyte(*cp);
+	/* Compute frame length, and add a few dummy trailer bytes */
+	len = sizeof(*fphp) + fphp->payload_len;
+	for (i = 0; i < 4; i++, len++)
+		buf[len] = 0;
 
-	/* Send a few padding bytes, just in case */
-	for (i = 0; i < 4; i++)
-		txbyte(0);
+	/* Send it */
+#ifndef X
+	for (cp = buf; cp < &buf[len]; cp++)
+		txbyte(*cp);
+#else
+#ifdef FTDI
+	ftdi_write_data(&fc, buf, len);
+#else
+	write(sioid, buf, len);
+#endif
+#endif
+
 }
 
 
+#ifndef X
 int
 main(void)
 {
+	int error;
+	fph_t *fphp = (fph_t *) buf;
 
-	tx_frame();
-	rx_frame();
+	/* Set baudrate */
+//	OUTW(IO_SIO + 2, 632); /* 115200 */
+//	OUTW(IO_SIO + 2, 316); /* 230400 */
+//	OUTW(IO_SIO + 2, 158); /* 460800 */
+//	OUTW(IO_SIO + 2, 79); /* 921600 */
+//	OUTW(IO_SIO + 2, 47); /* 1500000 */
+//	OUTW(IO_SIO + 2, 23); /* 3000000 not reliable */
+
+	do {
+		error = rx_frame();
+		OUTB(IO_LED, error);
+
+		fphp->fp_cmd = 123;
+		fphp->fp_res = error;
+		fphp->payload_len = 4;
+		tx_frame();
+	} while (1);
 
 	return (0);
 }
+#endif
+
+#ifdef X
+int
+main(void)
+{
+	int i, j, error;
+	int *ip;
+	fph_t *fphp = (fph_t *) buf;
+
+#ifdef FTDI
+	ftdi_init(&fc);
+        error = ftdi_usb_open_desc(&fc, 0x0403, 0x6001,
+            "FER ULXP2 board JTAG / UART", NULL);
+        if (error < 0)
+                return (error);
+	ftdi_set_baudrate(&fc, 115200);
+	ftdi_set_line_property(&fc, BITS_8, STOP_BIT_2, NONE);
+	ftdi_set_latency_timer(&fc, 1);
+#else
+	error = system("stty -f /dev/cuaU0.init cs8 -parenb speed 115200 -crtscts clocal -cstopb -onlcr -opost -inlcr -igncr -icrnl -ixon -ixoff -echo -echoe -echoke -echoctl");
+        if (error < 0)
+                return (error);
+	sioid = open("/dev/cuaU0", O_RDWR|O_NONBLOCK|O_DIRECT|O_TTY_INIT);
+        if (sioid < 0)
+                return (sioid);
+#endif
+
+	i = 10000;
+	do {
+		ip = (int *) &buf[(sizeof *fphp)];
+		for (j = 0; j < 1024; j++)
+			*ip++ = random();
+		ip = (int *) &buf[(sizeof *fphp)];
+		*ip = i++;
+
+		fphp->fp_cmd = FP_CMD_READID;
+		fphp->payload_len = 4096;
+		tx_frame();
+		*ip = 0;
+
+		error = rx_frame();
+		if (error) {
+			fprintf(stderr, "rx_frame() failed\n");
+			return(1);
+		}
+		ip = (int *) &buf[(sizeof *fphp)];
+		printf("fp_cmd: %d\n", fphp->fp_cmd);
+		printf("fp_res: %d\n", fphp->fp_res);
+		printf("seq: %d\n", *ip);
+	} while (fphp->fp_res == 0);
+
+	return (0);
+}
+#endif
