7f9f5b9aa66e6b117b03d96f2f87d475c40870e0
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-21 10:49:22 +0000

    Add balance and mute setting, which are all (including the volume setting) controllable via pushbuttons.
    Simplify VU meter deccay function.
    
    
    
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@228 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/demo/demo.c
+++ src/demo/demo.c
@@ -9,13 +9,19 @@
 #define	PCM_END		3000000
 #define	PCM_TSC_CYCLES	71	/* 3.125 MHz / 44.1 kHz = 71 TSC cycles */
 
+#define	PCM_VOL_MAX	12
+#define	PCM_VOL_MIN	1
+
 
 static int pcm_addr = PCM_END;
 static int pcm_vol = 8;
+static int pcm_bal;
+static int pcm_mute;
 static int pcm_avg[2];
-static int pcm_evol[2];
 static int pcm_vu[2];
+static int pcm_evol[2];
 static int pcm_tsc;
+static int pcm_pushbtn_old;
 
 static int pcm_tsc_len;
 static int pcm_cnt;
@@ -56,35 +62,59 @@ pcm_cnt ++;
 //		pcm_out[i] = (pcm_out[i] << pcm_vol) >> 12;
 		pcm_out[i] = (pcm_out[i] * (pcm_evol[i] >> 4)) >> 12;
 	}
-	OUTW(IO_PCM_OUT, (pcm_out[0] << 16) | pcm_out[1]);
+	OUTH(IO_PCM_OUT + 2, pcm_out[0]);
+	OUTH(IO_PCM_OUT, pcm_out[1]);
 	
 	/* Update volume and VU meter */
 	if ((pcm_addr & 0xfff) == 0) {
 		vu = 0;
 		for (i = 0; i < 2; i++) {
 			/* Volume */
-			pcm_evol[i] = (((pcm_evol[i] << 4) - pcm_evol[i]) +
-			    (0x10 << pcm_vol) + 0xf) >> 4;
+			if (i)
+				c = pcm_bal;
+			else
+				c = -pcm_bal;
+			if (c > 0)
+				c = 0;
+			c += pcm_vol;
+			if (pcm_mute)
+				c = 0;
+			pcm_evol[i] =
+			    (((pcm_evol[i] << 4) - (pcm_evol[i] << pcm_mute))
+			    + (0x10 << c) + 0xf) >> 4;
 
 			/* VU meter */
+			pcm_vu[i] = pcm_vu[i] - 0x0400;
 			if (pcm_avg[i] >= pcm_vu[i])
 				pcm_vu[i] = pcm_avg[i];
-			else
-				pcm_vu[i] =
-				    ((pcm_vu[i] << 4) - pcm_vu[i]) >> 4 ;
 			if (pcm_vu[i] > 0x6800)
 				c = 0xf;
 			else if (pcm_vu[i] > 0x5400)
 				c = 0x7;
 			else if (pcm_vu[i] > 0x3800)
 				c = 0x3;
-			else if (pcm_vu[i] > 0x2000)
+			else if (pcm_vu[i] > 0x1e00)
 				c = 0x1;
 			else
 				c = 0;
 			vu |= (c << (i << 2));
 		}
 		OUTB(IO_LED, vu);
+
+		INB(vu, IO_PUSHBTN);
+		if (vu != pcm_pushbtn_old) {
+			if ((vu & BTN_UP) && pcm_vol < PCM_VOL_MAX)
+				pcm_vol++;
+			if ((vu & BTN_DOWN) && pcm_vol > PCM_VOL_MIN)
+				pcm_vol--;
+			if ((vu & BTN_LEFT) && pcm_bal > -PCM_VOL_MAX >> 1)
+				pcm_bal--;
+			if ((vu & BTN_RIGHT) && pcm_bal < PCM_VOL_MAX >> 1)
+				pcm_bal++;
+			if (vu & BTN_CENTER)
+				pcm_mute ^= 1;
+		}
+		pcm_pushbtn_old = vu;
 	}
 
 	/* End of file, rewind SPI flash read position */
@@ -117,14 +147,6 @@ main(void)
 {
 	char c;
 
-	/* Just in case we got started without a bootloader... */
-	__asm __volatile__(
-		"li $29, (0x80000000);"
-		"li $27, %0"
-		:
-		: "i" (IO_BASE)
-	);
-
 	/* Register our PCM output function as idle loop handler */
 	sio_idle_fn = pcm_play;
 
@@ -136,11 +158,5 @@ main(void)
 printf("pcm_tsc_len: %d pcm_cnt: %d avg: %d\n", pcm_tsc_len, pcm_cnt, pcm_tsc_len / pcm_cnt);
 pcm_tsc_len = 0;
 pcm_cnt = 0;
-		if (c == '+' && pcm_vol < 12)
-			pcm_vol++;
-		if (c == '-' && pcm_vol > 0)
-			pcm_vol--;
-
-		printf("%d\n", pcm_vol);
 	} while (1);
 }
