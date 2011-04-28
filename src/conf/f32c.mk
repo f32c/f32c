b7285fb78e763caf82e6dcaa49c7dd4152e0f402
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-04-28 23:25:35 +0000

    Simplify Makefiles by moving most of the black magic to conf/f32c.mk, which has to be .included at the bottom of each individual Makefile.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@258 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/conf/f32c.mk
+++ src/conf/f32c.mk
@@ -1,54 +1,93 @@
 
 # Default load offset - bootloader is at 0x00000000
+.if !(defined(LOADADDR))
 LOADADDR = 0x00000180
+.endif
 
 # -EB big-endian (default); -EL little-endian
+.if !(defined(ENDIANFLAGS))
 ENDIANFLAGS = -EL
+.endif
 
 # Includes
-CFLAGS = -nostdinc -I../include
+MK_CFLAGS = -nostdinc -I../include -I.
 
 # MIPS-specific flags
-CFLAGS += -march=mips3 ${ENDIANFLAGS}
-CFLAGS += -mtune=mips32 -mno-branch-likely
-CFLAGS += -mno-mips16 -mno-dsp -mno-mips3d -mno-mdmx -msoft-float
-CFLAGS += -G 32768
+MK_CFLAGS += -march=mips3 ${ENDIANFLAGS}
+MK_CFLAGS += -mtune=mips32 -mno-branch-likely
+MK_CFLAGS += -mno-mips16 -mno-dsp -mno-mips3d -mno-mdmx -msoft-float
+MK_CFLAGS += -G 32768
 
 # f32c-specific flags
-CFLAGS += -msoft-mul -msoft-div
+MK_CFLAGS += -msoft-mul -msoft-div
 
 # Language flags
-CFLAGS += -std=c99 -Wall -Werror
-CFLAGS += -Wextra -Wsystem-headers -Wshadow -Wpadded -Winline
-CFLAGS += -c
-#CFLAGS += -s -n -nostdlib -fno-builtin
-CFLAGS += -ffreestanding
+MK_CFLAGS += -std=c99 -Wall -Werror
+MK_CFLAGS += -Wextra -Wsystem-headers -Wshadow -Wpadded -Winline
+MK_CFLAGS += -c
+MK_CFLAGS += -ffreestanding
+#MK_CFLAGS += -s -n -nostdlib -fno-builtin
 
 # Debugging options
-CFLAGS += -g
+MK_CFLAGS += -g
 
 # Optimization options
-CFLAGS += -Os
-CFLAGS += -finline-limit=16 -fmerge-all-constants
-CFLAGS += -falign-functions=4 -falign-labels=4
-CFLAGS += -falign-jumps=4 -falign-loops=4
-CFLAGS += -fweb -frename-registers
-CFLAGS += -freorder-blocks
-#CFLAGS += -funsafe-loop-optimizations -Wunsafe-loop-optimizations
-#CFLAGS += --param max-delay-slot-insn-search=16
-#CFLAGS += --param max-delay-slot-live-search=16
+MK_CFLAGS += -Os
+MK_CFLAGS += -finline-limit=16 -fmerge-all-constants
+MK_CFLAGS += -falign-functions=4 -falign-labels=4
+MK_CFLAGS += -falign-jumps=4 -falign-loops=4
+MK_CFLAGS += -fweb -frename-registers
+MK_CFLAGS += -freorder-blocks
+#MK_CFLAGS += -funsafe-loop-optimizations -Wunsafe-loop-optimizations
+#MK_CFLAGS += --param max-delay-slot-insn-search=16
+#MK_CFLAGS += --param max-delay-slot-live-search=16
 
 # No zero-filled BSS
-#CFLAGS += -fno-zero-initialized-in-bss
+#MK_CFLAGS += -fno-zero-initialized-in-bss
 
 # Other interesting options:
-# CFLAGS += -fPIC -fpic
+# MK_CFLAGS += -fPIC -fpic
 # -membedded-data
 # -no-shared
 
-LDFLAGS += -Ttext ${LOADADDR} -N ${ENDIANFLAGS}
+MK_LDFLAGS += -Ttext ${LOADADDR} -N ${ENDIANFLAGS}
 
-AS = mips-rtems-gcc ${CFLAGS} ${ASFLAGS}
-CC = mips-rtems-gcc ${CFLAGS}
-LD = mips-rtems-ld ${LDFLAGS}
+# Sweep CFLAGS from leading "-O2 -pipe" noise, inserted by sys.mk
+.if (defined(CFLAGS))
+ECFLAGS = ${CFLAGS:S/^-O2//:S/^-pipe//}
+.endif
+
+AS = mips-rtems-gcc ${MK_CFLAGS} ${ASFLAGS}
+CC = mips-rtems-gcc ${MK_CFLAGS} ${ECFLAGS}
+LD = mips-rtems-ld ${MK_LDFLAGS} ${LDFLAGS}
+ELF2HEX = ../tools/elf2hex.tcl
+
+
+#
+# Autogenerate targets
+#
+
+OBJS += ${ASFILES:T:.S=.o}
+OBJS += ${CFILES:T:.c=.o}
+
+HEX = ${PROG}.hex
+
+${HEX}: ${PROG}
+	${ELF2HEX} ${PROG} > ${HEX}
+
+${PROG}: ${OBJS}
+	${LD} -o ${PROG} ${OBJS}
+
+.for _src in ${CFILES}
+${_src:T:.c=.o}: ${_src}
+	${CC} ${.IMPSRC}
+.endfor
+
+.for _src in ${ASFILES}
+${_src:T:.S=.o}: ${_src}
+	${AS} ${.IMPSRC}
+.endfor
+
+clean:
+	rm -f ${OBJS} ${PROG} ${HEX}
 
