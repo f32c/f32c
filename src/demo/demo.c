719799d08677e91f7414a7388a8531e4fc6f1140
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-05-11 08:18:51 +0000

    Allow for DDS frequency to be specified in fractions of MHz with resolution of 50 Hz.  For frequencies above 174 MHz, f/3 is synthesized, so that 3rd harmonic can be received.  Limit max DDS frequency to 512 MHz due to wrapping of integer counters.
    With all this in place, we can generate FM signals at arbitrary
    frequencies from DC to 512 MHz.
    
    
    
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@306 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/demo/demo.c
+++ src/demo/demo.c
@@ -15,37 +15,60 @@ int
 main(void)
 {
 	u_char c;
-	int freq;
+	int freq_i, freq_f, f_c, f_d;
 
 	/* Register PCM output function as idle loop handler */
 	sio_idle_fn = pcm_play;
 
 	do {
 		printf("Enter DDS frequency: ");
-		for (freq = 0; (c = getchar()) != '\r';) {
+		for (freq_i = 0, freq_f = 0, f_c = -1, f_d = 1;
+		    (c = getchar()) != '\r';) {
 			/* Exit to bootloader on CTRL+C */
 			if (c == 3)
 				return(0);
 			putchar(c);
 			if (c >= '0' && c <= '9') {
-				freq = freq * 10 + c - '0';
-			} else
-				break;
+				if (f_c >= 0) {
+					if (f_d == 100000)
+						continue;
+					freq_f = freq_f * 10 + c - '0';
+					f_d *= 10;
+					if (freq_f == 0 && c == '0')
+						f_c++;
+				} else
+					freq_i = freq_i * 10 + c - '0';
+			}
+			if (c == '.')
+				f_c = 0;
 		}
 		printf("\n\n");
-		printf("Using %d MHz as DDS frequency, ", freq);
-		if (freq >= 76 && freq <= 108) {
+		if (freq_i >= 512) {
+			printf("Can't synthesize frequencies above"
+			    " 512 MHz\n\n");
+			continue;
+		}
+		printf("Using %d.", freq_i);
+		while (f_c > 0) {
+			f_c--;
+			printf("0");
+		}
+		printf("%d MHz as DDS frequency, ", freq_f);
+		if (freq_i >= 76 && freq_i < 108) {
 			printf("wide modulation.");
 			fm_mode = 4;
 		} else {
 			printf("narrow modulation.");
-			if (freq > 255)
+			if (freq_i >= 174)
 				fm_mode = 10;
 			else
 				fm_mode = 8;
 		}
 		printf("\n\n");
-		dds_base = (freq << 22) / 300;
+		dds_base = freq_i << 22;
+		if (f_d > 1)
+			dds_base += (1 << 22) / f_d * freq_f;
+		dds_base /= 300;
 		if (fm_mode == 10)
 			dds_base /= 3;
 	} while (1);
