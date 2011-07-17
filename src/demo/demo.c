ccfccf88ad1d136dec2a7d9f83b9809fb731e7a4
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-07-17 11:09:43 +0000

    Use gets() instead of hand-crafted input loop in demo.c.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@396 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/demo/demo.c
+++ src/demo/demo.c
@@ -10,39 +10,40 @@ extern void pcm_play(void);
 extern int dds_base;
 extern int fm_mode;
 
+#define BUFSIZE 80
+
+char buf[BUFSIZE];
+
 
 int
 main(void)
 {
-	u_char c;
+	char *c;
 	int freq_i, freq_f, f_c, f_d;
 
 	/* Register PCM output function as idle loop handler */
 	sio_idle_fn = pcm_play;
 
 	do {
-		printf("Enter DDS frequency: ");
-		for (freq_i = 0, freq_f = 0, f_c = -1, f_d = 1;
-		    (c = getchar()) != '\r';) {
-			/* Exit to bootloader on CTRL+C */
-			if (c == 3)
-				return(0);
-			putchar(c);
-			if (c >= '0' && c <= '9') {
+		printf("\nEnter DDS frequency: ");
+		if (gets(buf, BUFSIZE) != 0)
+			return (0);	/* Got CTRL + C */
+		for (c = buf, freq_i = 0, freq_f = 0, f_c = -1, f_d = 1;
+		    *c != '\0'; c++) {
+			if (*c >= '0' && *c <= '9') {
 				if (f_c >= 0) {
 					if (f_d == 10000)
 						continue;
-					freq_f = freq_f * 10 + c - '0';
+					freq_f = freq_f * 10 + *c - '0';
 					f_d *= 10;
-					if (freq_f == 0 && c == '0')
+					if (freq_f == 0 && *c == '0')
 						f_c++;
 				} else
-					freq_i = freq_i * 10 + c - '0';
+					freq_i = freq_i * 10 + *c - '0';
 			}
-			if (c == '.')
+			if (*c == '.')
 				f_c = 0;
 		}
-		printf("\n\n");
 		if (freq_i >= 512) {
 			printf("Can't synthesize frequencies above"
 			    " 512 MHz\n\n");
@@ -55,16 +56,15 @@ main(void)
 		}
 		printf("%d MHz as DDS frequency, ", freq_f);
 		if (freq_i >= 76 && freq_i < 108) {
-			printf("wide modulation.");
+			printf("wide modulation.\n");
 			fm_mode = 4;
 		} else {
-			printf("narrow modulation.");
+			printf("narrow modulation.\n");
 			if (freq_i >= 174)
 				fm_mode = 10;
 			else
 				fm_mode = 8;
 		}
-		printf("\n\n");
 		dds_base = freq_i << 22;
 		if (f_d > 1)
 			dds_base += ((1 << 16) * freq_f / f_d) << 6;
