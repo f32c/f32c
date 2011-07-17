d5a7b18b1258c9c5dd600246bfccebf8e40bbb7e
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-07-17 10:51:53 +0000

    Add a gets() implementation, borrowed from FreeBSD.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@394 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/lib/gets.c
@@ -0,0 +1,66 @@
+/*-
+ * Copyright (c) 1999 Michael Smith
+ * Copyright (c) 2005 Pawel Jakub Dawidek <pjd@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+
+
+int
+gets(char *cp, int size)
+{
+	char *lp, *end;
+	int c;
+	int error = 0;
+
+	lp = cp;
+	end = cp + size - 1;
+	for (;;) {
+		c = getchar() & 0177;
+		switch (c) {
+		case 3:	/* CTRL + C */
+			error = -1;
+		case '\n':
+		case '\r':
+			printf("\n");
+			*lp = '\0';
+			return (error);
+		case '\b':
+		case '\177':
+			if (lp > cp) {
+				printf("%c \b", c);
+				lp--;
+			}
+			continue;
+		case '\0':
+			continue;
+		default:
+			if (lp < end) {
+				printf("%c", c);
+				*lp++ = c;
+			}
+		}
+	}
+}
