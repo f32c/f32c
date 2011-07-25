0ed93f1fd1b57ba6a0abead6da4da191ca6dd80e
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-07-25 09:57:19 +0000

    Initial implementation of strcmp(), strcpy(), memcpy() and malloc() library functions, which are required for the dhrystone benchmark to work.  Completely untested ATM.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@412 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/dhry/miscfn.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void *
+malloc(int size)
+{
+
+	printf("malloc: %d\n\n", size);
+	exit(0);
+	return(NULL); /* XXX unreached */
+}
+
+
+int
+strcmp(const char *s1, const char *s2)
+{
+	char c1, c2;
+ 
+	do {
+		c1 = *s1++;
+		c2 = *s2++;
+	} while (c1 != 0 && c1 == c2);
+	return (c1 - c2);
+}
+
+
+void
+strcpy(char *dst, const char *src)
+{
+ 
+	do {
+		*dst++ = *src++;
+	} while (*dst);
+}
+
+
+void
+memcpy(char *dst, const char *src, int len)
+{
+
+	while (len--)
+		*dst++ = *src++;
+}
+
