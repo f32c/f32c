cbfeeeb205cc672179a088f25e8b08a4c1562528
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-07-28 22:02:16 +0000

    Optimize (signed) integer division, and hopefully fix signed integer modulo computation when operand(s) are negative.  Performance gain: 149.9 -> 153.7 DMIPS @ 137.5 MHz.  DMIPS / MHz ratio is now 1.12.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@441 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/div.c
+++ src/lib/div.c
@@ -2,45 +2,78 @@
 #include <types.h>
  
 
+#define	UDIVMOD_BODY()						\
+	uint32_t lo = 0;					\
+	uint32_t bit = 1;					\
+								\
+	while (b < a && bit && (b & 0x80000000) == 0) {		\
+		b <<= 1;					\
+		bit <<= 1;					\
+	}							\
+	while (bit) {						\
+		if (a >= b) {					\
+			lo |= bit;				\
+			a -= b;					\
+		}						\
+		bit >>= 1;					\
+		b >>= 1;					\
+	}
+
+
 static uint32_t
-udivmod(uint32_t hi, uint32_t b, int do_mod)
+udivmod(uint32_t a, uint32_t b, int do_mod)
 {
-	uint32_t lo = 0;
-#if 0
-	uint32_t a = b << 31;
-	int i;
 
-	for (i = 0; i < 32; i++) {
-		lo <<= 1;
-		if (a != 0 && hi >= a && b < 2) {
-			hi -= a;
-			lo |= 1;
-		}
-		a = (a >> 1) | ((b & 2) << 30);
-		b >>= 1;
-	}
-#else
-	uint32_t bit = 1;
+	UDIVMOD_BODY();
 
-	while (b < hi && bit && !(b & 0x80000000)) {
-		b <<= 1;
-		bit <<= 1;
-	}
-	while (bit) {
-		if (hi >= b) {
-			hi -= b;
-			lo |= bit;
-		}
-		bit >>= 1;
-		b >>= 1;
-	}
-#endif
 	if (do_mod)
-		return (hi);
+		return (a);
 	return (lo);
 }
  
  
+int32_t
+__divsi3(int32_t a, int32_t b)
+{
+	int neg = 0;
+
+	if (a < 0) {
+		a = -a;
+		neg = !neg;
+	}
+	if (b < 0) {
+		b = -b;
+		neg = !neg;
+	}
+
+	UDIVMOD_BODY();
+
+	if (neg)
+		lo = -lo;
+	return (lo);
+}
+ 
+ 
+uint32_t
+__modsi3(int32_t a, int32_t b)
+{
+	int res, neg = 0;
+
+	if (b < 0)
+		b = -b;
+	if (a < 0) {
+		a = -a;
+		neg = !neg;
+	}
+
+	res = udivmod(a, b, 1);
+
+	if (neg)
+		res = -res;
+	return (res);
+}
+
+
 uint32_t
 __udivsi3(uint32_t a, uint32_t b)
 {
@@ -49,41 +82,9 @@ __udivsi3(uint32_t a, uint32_t b)
 }
  
 
-int32_t
-__divsi3(int32_t a, int32_t b)
-{
-	int res, neg = 0;
-
-	if(a < 0) {
-		a = -a;
-		neg = !neg;
-	}
-	if (b < 0) {
-		b = -b;
-		neg = !neg;
-	}
-	res = udivmod(a, b, 0);
-	if (neg)
-		res = -res;
-	return (res);
-}
- 
- 
 uint32_t
 __umodsi3(uint32_t a, uint32_t b)
 {
 
-	return (udivmod(a, b, 1));
-}
- 
- 
-uint32_t
-__modsi3(int32_t a, int32_t b)
-{
-
-	if (a < 0)
-		a = -a;
-	if (b < 0)
-		b = -b;
 	return (udivmod(a, b, 1));
 }
