a5253c989596fa6cc70b46315064bdfc6e2f59df
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-07-29 15:56:46 +0000

    More clearly separate c and asm implementations of strcmp().  No functional changes.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@444 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/dhry/miscfn.c
+++ src/dhry/miscfn.c
@@ -20,10 +20,10 @@ malloc(int size __attribute__((unused)))
 }
 
 
+#if 0
 int
 strcmp(const char *s1, const char *s2)
 {
-#if 0
 	int c1, c2;
  
 	/* Check for aligned pointers for faster operation */
@@ -47,55 +47,55 @@ strcmp(const char *s1, const char *s2)
 		c2 = *(const unsigned char *)s2++;
 	} while (c1 != 0 && c1 == c2);
 	return (c1 - c2);
-#else
-	int res;
-
-	__asm (
-		".set noreorder			\n"
-		"	or	$2, $4, $5	\n"
-		"	andi	$2, $2, 0x3	\n"
-		"	bnez	$2, 3f		\n"
-		"	lui	$6, 0x00ff	\n"
-		"	lui	$7, 0xff00	\n"
-		"	lw	$2, 0($4)	\n"
-		"1:				\n"
-		"	lw	$3, 0($5)	\n"
-		"	addiu	$4, $4, 4	\n"
-		"	bne	$2, $3, 2f	\n"
-		"	and	$3, $2, $6	\n"
-		"	beqz	$3, 5f		\n"
-		"	and	$3, $2, $7	\n"
-		"	beqz	$3, 5f		\n"
-		"	andi	$3, $2, 0xff00	\n"
-		"	beqz	$3, 5f		\n"
-		"	andi	$3, $2, 0x00ff	\n"
-		"	beqz	$3, 5f		\n"
-		"	addiu	$5, $5, 4	\n"
-		"	b	1b		\n"
-		"	lw	$2, 0($4)	\n"
-		"2:				\n"
-		"	addiu	$4, $4, -4	\n"
-		"3:				\n"
-		"	lbu	$2, 0($4)	\n"
-		"	lbu	$3, 0($5)	\n"
-		"	beqz	$2, 4f		\n"
-		"	addiu	$4, $4, 1	\n"
-		"	beq	$2, $3, 3b	\n"
-		"	addiu	$5, $5, 1	\n"
-		"4:				\n"
-		"	jr	$31		\n"
-		"	subu	$2, $2, $3	\n"
-		"5:				\n"
-		"	jr	$31		\n"
-		"	subu	$2, $2, $2	\n"
-		: "=r" (res)
-		: "r" (s1), "r" (s2)
-	);
-
-	/* XXX revisit return - missing a nop after jr ra */
-	return(res);
-#endif
 }
+#else
+__asm (
+	".set noreorder			\n"
+	".set nomacro			\n"
+	".globl strcmp			\n"
+	".ent strcmp			\n"
+	"strcmp:			\n"
+	"	or	$2, $4, $5	\n"
+	"	andi	$2, $2, 0x3	\n"
+	"	bnez	$2, 3f		\n"
+	"	lui	$6, 0x00ff	\n"
+	"	lui	$7, 0xff00	\n"
+	"	lw	$2, 0($4)	\n"
+	"1:				\n"
+	"	lw	$3, 0($5)	\n"
+	"	addiu	$4, $4, 4	\n"
+	"	bne	$2, $3, 2f	\n"
+	"	and	$3, $2, $6	\n"
+	"	beqz	$3, 5f		\n"
+	"	and	$3, $2, $7	\n"
+	"	beqz	$3, 5f		\n"
+	"	andi	$3, $2, 0xff00	\n"
+	"	beqz	$3, 5f		\n"
+	"	andi	$3, $2, 0x00ff	\n"
+	"	beqz	$3, 5f		\n"
+	"	addiu	$5, $5, 4	\n"
+	"	b	1b		\n"
+	"	lw	$2, 0($4)	\n"
+	"2:				\n"
+	"	addiu	$4, $4, -4	\n"
+	"3:				\n"
+	"	lbu	$2, 0($4)	\n"
+	"	lbu	$3, 0($5)	\n"
+	"	beqz	$2, 4f		\n"
+	"	addiu	$4, $4, 1	\n"
+	"	beq	$2, $3, 3b	\n"
+	"	addiu	$5, $5, 1	\n"
+	"4:				\n"
+	"	jr	$31		\n"
+	"	subu	$2, $2, $3	\n"
+	"5:				\n"
+	"	jr	$31		\n"
+	"	subu	$2, $2, $2	\n"
+	".end strcmp			\n"
+	".set macro			\n"
+	".set reorder			\n"
+);
+#endif
 
 
 /* memcpy() is required for -Os builds */
