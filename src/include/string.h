4f17e4605518b8af77d7f290032059cb2af57445
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-07-29 18:31:47 +0000

    Move strcmp() in a separate file under src/lib, so that it can be used (and tested) by other programs.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@447 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/string.h
+++ src/include/string.h
@@ -2,7 +2,19 @@
 #ifndef _STRING_H_
 #define _STRING_H_
 
+
+int strcmp(const char *, const char *);
+
+
 #define memcpy(dst, src, len) _memcpy(dst, src, len)
+
+
+#define	strcpy(dst, src)					\
+	if (sizeof(src) != sizeof(void *))			\
+		_memcpy((dst), (src), sizeof(src));		\
+	else							\
+		_strcpy(dst, src);
+
 
 static inline void
 _memcpy(char *dst, const char *src, int len)
@@ -11,6 +23,7 @@ _memcpy(char *dst, const char *src, int len)
 	while (len--)
 		*dst++ = *src++;
 }
+
 
 static inline void
 _strcpy(char *dst, const char *src)
@@ -22,11 +35,5 @@ _strcpy(char *dst, const char *src)
 		*dst++ = c;
 	} while (c != 0);
 }
-
-#define	strcpy(dst, src)					\
-	if (sizeof(src) != sizeof(void *))			\
-		memcpy((dst), (src), sizeof(src));		\
-	else							\
-		_strcpy(dst, src);
 
 #endif /* !_STRING_H_ */
