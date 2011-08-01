d20b309dab6aff09e21ca89ae2f380094a794499
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-08-01 13:05:17 +0000

    Enable pipelined load aligner, which currently unconditionally stalls the pipeline for each half word / byte load.
    DMIPS is increased from 160.7 to 165.8 @ 137.5 MHz, i.e. to 1.21 DMIPS/MHz.
    
    
    
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@474 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/idecode.vhd
+++ rtl/idecode.vhd
@@ -195,7 +195,8 @@ begin
 
 	if opcode(5 downto 4) = "10" then
 	    mem_cycle <= '1';
-	    latency <= "01"; -- resolve load-use hazard
+	    latency(0) <= '1'; -- resolve load-use hazard
+	    latency(1) <= not mem_size(1); -- resolve load aligner hazard
 	end if;
     end process;
 	
