6cd16679112227ad47d9ea281cacd0d7ef1ed4b0
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-08-05 21:37:11 +0000

    In integer division routines, use integer addition instead of bit shift for multiplying values by 2, because addition takes one cycle whereas the (pipelined) barell shifter has a latency of 2 cycles.
    The result is performance increase from 1.40 to 1.41 DMIPS/MHz.
    
    
    
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@497 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/div.c
+++ src/lib/div.c
@@ -2,13 +2,16 @@
 #include <types.h>
  
 
+static uint32_t udivmod(uint32_t, uint32_t, int);
+
+
 #define	UDIVMOD_BODY()						\
 	uint32_t lo = 0;					\
 	uint32_t bit = 1;					\
 								\
-	while (b < a && bit && (b & 0x80000000) == 0) {		\
-		b <<= 1;					\
-		bit <<= 1;					\
+	while (b < a && (b & 0x80000000) == 0 && bit) {		\
+		b += b;						\
+		bit += bit;					\
 	}							\
 	while (bit) {						\
 		if (a >= b) {					\
@@ -20,18 +23,6 @@
 	}
 
 
-static uint32_t
-udivmod(uint32_t a, uint32_t b, int do_mod)
-{
-
-	UDIVMOD_BODY();
-
-	if (do_mod)
-		return (a);
-	return (lo);
-}
- 
- 
 int32_t
 __divsi3(int32_t a, int32_t b)
 {
@@ -72,6 +63,14 @@ __modsi3(int32_t a, int32_t b)
 		res = -res;
 	return (res);
 }
+ 
+ 
+uint32_t
+__umodsi3(uint32_t a, uint32_t b)
+{
+
+	return (udivmod(a, b, 1));
+}
 
 
 uint32_t
@@ -82,9 +81,13 @@ __udivsi3(uint32_t a, uint32_t b)
 }
  
 
-uint32_t
-__umodsi3(uint32_t a, uint32_t b)
+static uint32_t
+udivmod(uint32_t a, uint32_t b, int do_mod)
 {
 
-	return (udivmod(a, b, 1));
+	UDIVMOD_BODY();
+
+	if (do_mod)
+		return (a);
+	return (lo);
 }
