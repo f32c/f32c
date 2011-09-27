c285fc4f1a2ef755e8aaa9b8d7d90888516cab5e
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2011-09-27 10:41:58 +0000

    atoi() ide iz demo/demo.c u lib/atoi.c.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@607 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/lib/atoi.c
@@ -0,0 +1,22 @@
+
+#include <types.h>
+
+int
+atoi(const char *c)
+{
+	int i = 0;
+	int sign = 1;
+
+	if (*c == '-') {
+		sign = -1;
+		c++;
+	}
+	for (; *c != '\0'; c++) {
+		if (*c >= '0' && *c <= '9')  
+			i = i * 10 + (*c - '0');
+		else
+			break;
+	}
+
+	return (sign * i);
+}
