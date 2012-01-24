d7f6ea5d03f5287478d32a7b81323aa554ff8ef0
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-01-24 14:51:53 +0000

    Xilinx synthesises: LEDs blink, bootloader prompt visible on RS232.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@822 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/xilinx/glue.vhd
+++ rtl/xilinx/glue.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity glue is
     generic(
 	-- Main clock: 50, 62, 75, 81, 87, 100, 112, 125, 137, 150 MHz
-	C_clk_freq: integer := 81;
+	C_clk_freq: integer := 50;
 
 	-- ISA options
 	C_big_endian: boolean := true;
@@ -50,19 +50,16 @@ entity glue is
 
 	-- These may negatively influence timing closure:
 	C_movn_movz: boolean := false; -- true: +16 LUT4, -DMIPS, incomplete
-	C_fast_ID: boolean := true; -- false: +7 LUT4, -Fmax
+	C_fast_ID: boolean := true;
 
 	-- debugging options
-	C_debug: boolean := false; -- true: +883 LUT4, -Fmax
+	C_debug: boolean := false;
 
 	-- SoC configuration options
 	C_mem_size: string := "16k";
-	C_tsc: boolean := true; -- true: +54 LUT4
-	C_sio: boolean := true; -- true: +101 LUT4
-	C_gpio: boolean := true; -- true: -6 LUT4 (?)
-	C_flash: boolean := true; -- true: -1 LUT4 (?)
-	C_pcmdac: boolean := true; -- true: +34 LUT4
-	C_ddsfm: boolean := true -- true: +28 LUT4
+	C_tsc: boolean := true;
+	C_sio: boolean := true;
+	C_gpio: boolean := true
     );
     port (
 	clk_50m: in std_logic;
@@ -70,36 +67,41 @@ entity glue is
 	rs232_dce_rxd: in std_logic;
 	lcd_db: out std_logic_vector(7 downto 0);
 	lcd_e, lcd_rs, lcd_rw: out std_logic;
+	j1, j2: out std_logic_vector(3 downto 0);
 	led: out std_logic_vector(7 downto 0);
-	rot_a, rot_b, rot_center: std_logic;
+	rot_a, rot_b, rot_center: in std_logic;
 	btn_south, btn_north, btn_east, btn_west: in std_logic;
-	sw: in std_logic_vector(3 downto 0);
-	j1, j2: out std_logic_vector(3 downto 0)
+	sw: in std_logic_vector(3 downto 0)
     );
 end glue;
 
 architecture Behavioral of glue is
-	signal clk, slowclk: std_logic;
-	signal imem_addr: std_logic_vector(31 downto 2);
-	signal imem_data_read: std_logic_vector(31 downto 0);
-	signal imem_addr_strobe, imem_data_ready: std_logic;
-	signal dmem_addr: std_logic_vector(31 downto 2);
-	signal dmem_addr_strobe, dmem_bram_enable, dmem_data_ready: std_logic;
-	signal dmem_byte_we: std_logic_vector(3 downto 0);
-	signal dmem_to_cpu, cpu_to_dmem: std_logic_vector(31 downto 0);
-	signal io_to_cpu, final_to_cpu: std_logic_vector(31 downto 0);
+    signal clk: std_logic;
+    signal imem_addr: std_logic_vector(31 downto 2);
+    signal imem_data_read: std_logic_vector(31 downto 0);
+    signal imem_addr_strobe, imem_data_ready: std_logic;
+    signal dmem_addr: std_logic_vector(31 downto 2);
+    signal dmem_addr_strobe, dmem_bram_enable, dmem_data_ready: std_logic;
+    signal dmem_byte_we: std_logic_vector(3 downto 0);
+    signal dmem_to_cpu, cpu_to_dmem: std_logic_vector(31 downto 0);
+    signal io_to_cpu, final_to_cpu: std_logic_vector(31 downto 0);
 
-   -- I/O
-	signal led_reg: std_logic_vector(7 downto 0);
-	signal lcd_data: std_logic_vector(7 downto 0);
-	signal lcd_ctrl: std_logic_vector(1 downto 0);
-	signal tsc: std_logic_vector(31 downto 0);
-	signal input: std_logic_vector(31 downto 0);
+    -- I/O
+    signal from_sio: std_logic_vector(31 downto 0);
+    signal sio_txd, sio_rxd, sio_ce: std_logic;
+    signal R_led: std_logic_vector(7 downto 0);
+    signal tsc_50m: std_logic_vector(35 downto 0);
+    signal tsc: std_logic_vector(31 downto 0);
+    signal R_sw: std_logic_vector(3 downto 0);
+    signal R_btns: std_logic_vector(6 downto 0);
 
-	-- debugging only
-	signal clk_key: std_logic;
-	signal trace_addr: std_logic_vector(5 downto 0);
-	signal trace_data: std_logic_vector(31 downto 0);
+    -- debugging only
+    signal trace_addr: std_logic_vector(5 downto 0);
+    signal trace_data: std_logic_vector(31 downto 0);
+    signal debug_txd: std_logic;
+    signal debug_res: std_logic;
+    signal clk_key: std_logic;
+
 begin
 
     -- f32c core
@@ -130,62 +132,110 @@ begin
 	trace_addr => trace_addr, trace_data => trace_data
     );
 
+    -- RS232 sio
+    G_sio:
+    if C_sio generate
+    sio: entity sio
+    generic map (
+	C_big_endian => C_big_endian,
+	C_clk_freq => C_clk_freq
+    )
+    port map (
+	clk => clk, ce => sio_ce, txd => sio_txd, rxd => sio_rxd,
+	byte_we => dmem_byte_we, bus_in => cpu_to_dmem, bus_out => from_sio
+    );
+    sio_ce <= dmem_addr_strobe when dmem_addr(31 downto 28) = x"f" and
+      dmem_addr(4 downto 2) = "001" else '0';
+    rs232_dce_txd <= debug_txd when C_debug and sw(3) = '1' else sio_txd;
+    sio_rxd <= rs232_dce_rxd;
+    end generate;
 
     -- I/O port map:
-    -- 0xe******0:	(1B, WR) LED
-    -- 0xe******4:	(4B, RD) TSC
-    -- 0xe******8:	(1B, WR) LCD data
-    -- 0xe******c:	(1B, WR) LCD ctrl
+    -- 0x8*******: SRAM
+    -- 0xf*****00: (4B, RW) GPIO (LED, switches/buttons)
+    -- 0xf*****04: (4B, RW) SIO
+    -- 0xf*****08: (4B, RD) TSC
+    -- 0xf*****0c: (4B, WR) PCM signal
+    -- 0xf*****10: (1B, RW) SPI Flash
+    -- 0xf*****14: (1B, RW) SPI MicroSD
+    -- 0xf*****1c: (4B, WR) FM DDS register
     -- I/O write access:
     process(clk)
     begin
-	if rising_edge(clk) then
-	    tsc <= tsc + 1;
-	    if dmem_addr(31 downto 28) = "1110" and dmem_addr_strobe = '1' then
-		if dmem_byte_we /= "0000" then
-		    if dmem_addr(3 downto 2) = "00" then
-			led_reg <= cpu_to_dmem(7 downto 0);
-		    elsif dmem_addr(3 downto 2) = "10" then
-			lcd_data <= cpu_to_dmem(7 downto 0);
-		    elsif dmem_addr(3 downto 2) = "11" then
-			lcd_ctrl <= cpu_to_dmem(1 downto 0);
-		    end if;
+	if rising_edge(clk) and dmem_addr_strobe = '1'
+	  and dmem_addr(31 downto 28) = x"f" then
+	    -- GPIO
+	    if C_gpio and dmem_addr(4 downto 2) = "000" then
+		if dmem_byte_we(0) = '1' then
+		    R_led <= cpu_to_dmem(7 downto 0);
 		end if;
-	   end if;
+	    end if;
 	end if;
     end process;
+    led <= R_led when C_gpio else "--------";
 
     process(clk)
     begin
-	if rising_edge(clk) then
-	    input <= x"00000" &
-	      rs232_dce_rxd & rot_a & rot_b & rot_center &
-	      btn_south & btn_north & btn_east & btn_west & sw;
+	if C_gpio and rising_edge(clk) then
+	    R_sw <= sw;
+	    R_btns <= rot_a & rot_b & rot_center &
+	      btn_north & btn_south & btn_west & btn_east;
 	end if;
     end process;
 
-    io_to_cpu <= input when dmem_addr(3 downto 2) = "00" else tsc;
+    G_tsc:
+    if C_tsc generate
+    process(clk_50m)
+    begin
+	if rising_edge(clk_50m) then
+	    tsc_50m <= tsc_50m + 1;
+	end if;
+    end process;
+    -- Safely move upper bits of tsc_50m over clock domain boundary
+    process(clk, tsc_50m)
+    begin
+	if rising_edge(clk) and tsc_50m(3 downto 2) = "10" then
+	    if C_big_endian then
+		tsc <= tsc_50m(11 downto 4) & tsc_50m(19 downto 12) &
+		  tsc_50m(27 downto 20) & tsc_50m(35 downto 28);
+	    else
+		tsc <= tsc_50m(35 downto 4);
+	    end if;
+	end if;
+    end process;
+    end generate;
 
-    final_to_cpu <= io_to_cpu when dmem_addr(31 downto 28) = "1110"
+    -- XXX replace with a balanced multiplexer
+    process(dmem_addr, R_sw, R_btns, from_sio, tsc)
+    begin
+	case dmem_addr(4 downto 2) is
+	when "000"  =>
+	    io_to_cpu <="----------------" & "----" & R_sw & "-" & R_btns;
+	when "001"  => io_to_cpu <= from_sio;
+	when "010"  => io_to_cpu <= tsc;
+	when others =>
+	    io_to_cpu <= "--------------------------------";
+	end case;
+    end process;
+
+    -- XXX fix or remove these!
+    lcd_db <= "--------";
+    lcd_e <= '-';
+    lcd_rs <= '-';
+    lcd_rw <= '-';
+    j1 <= "----";
+    j2 <= "----";
+
+    final_to_cpu <= io_to_cpu when dmem_addr(31 downto 28) = x"f"
       else dmem_to_cpu;
-	
-    led <= led_reg;
-    lcd_db <= lcd_data;
-    lcd_rs <= lcd_ctrl(0);
-    lcd_e <= lcd_ctrl(1);
-    lcd_rw <= '0';
 
-    -- mirror leds to J1 / J2 pins
-    j1 <= led_reg(3 downto 0);
-    j2 <= led_reg(7 downto 4);
-	
     -- a DLL clock synthesizer
     clkgen: entity clkgen
     generic map(
-	C_clk_mhz => 50 -- XXX hardcoded!
+	C_clk_mhz => C_clk_freq
     )
     port map(
-	clk_in => clk_50m, clk_out => clk, clk_out_slow => slowclk,
+	clk_in => clk_50m, clk_out => clk, clk_out_slow => open,
 	key => clk_key, sel => sw(1 downto 0)
     );
 	
@@ -211,7 +261,7 @@ begin
     debug_serial: entity serial_debug
     port map(
 	clk_50m => clk_50m,
-	rs232_txd => rs232_dce_txd,
+	rs232_txd => debug_txd,
 	trace_addr => trace_addr,
 	trace_data => trace_data
     );
@@ -220,7 +270,6 @@ begin
     G_nodebug:
     if not C_debug generate
     clk_key <= '1'; -- clk selector
-    rs232_dce_txd <= '1'; -- appease XST
     end generate; -- nodebug
 	
 end Behavioral;
