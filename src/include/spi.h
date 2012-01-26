16553c1e9380e492462300091b788615ebccb70d
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-01-26 08:27:40 +0000

    Malo modularnije sdcard_* rutine.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@836 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/spi.h
+++ src/include/spi.h
@@ -2,16 +2,16 @@
 #ifndef _SPI_H_
 #define	_SPI_H_
 
-#define SPI_PORT_FLASH		0
-#define SPI_PORT_SDCARD		4
+#include <io.h>
+
+#define	SPI_PORT_FLASH		0
+#define	SPI_PORT_SDCARD		4
 
 #define	spi_start_transaction(port)					\
-	SB(SPI_SI, IO_SPI_FLASH, port)
-#define	spi_stop_transaction(port)					\
 	SB(SPI_CEN | SPI_SI, IO_SPI_FLASH, port)
 
 int spi_byte(int, int);
 int spi_byte_in(int);
 
-#endif /* !_IO_H_ */
+#endif /* !_SPI_H_ */
 
