372a4fcc244476526cf34a9d273ad8f7161ded14
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-01-27 10:00:27 +0000

    Radi citanje blokova podataka s MicroSD kartice.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@839 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/sdcard.h
+++ src/include/sdcard.h
@@ -2,10 +2,21 @@
 #ifndef _SDCARD_H_
 #define	_SDCARD_H_
 
-int sdcard_cmd(int , uint32_t);
-int sdcard_idle(void);
 int sdcard_init(void);
+int sdcard_read(char *, int);
+int sdcard_cmd(int , uint32_t);
 
+#define	SDCARD_CMD_GO_IDLE_STATE	0	
+#define	SDCARD_CMD_SEND_OP_COND		1		
+#define	SDCARD_CMD_SEND_CSD		9
+#define	SDCARD_CMD_SEND_CID		10
+#define	SDCARD_CMD_SET_BLOCKLEN		16
+#define	SDCARD_CMD_READ_BLOCK		17
+#define	SDCARD_CMD_READ_MULTI_BLOCK	18
+#define	SDCARD_CMD_WRITE_BLOCK		17
+#define	SDCARD_CMD_WRITE_MULTI_BLOCK	18
+
+#define	SDCARD_BLOCK_SIZE		512
 
 #endif /* !_SDCARD_H_ */
 
--- src/test1/test1.c
+++ src/test1/test1.c
@@ -5,75 +5,63 @@
 #include <stdio.h>
 
 
-static void
-read_spi(int n)
-{
-	unsigned char buf[16];
-	int i, j;
-
-	for (i = 0; i < n; i++) {
-		buf[i & 0xf] = spi_byte_in(SPI_PORT_SDCARD);
-		if ((i & 0xf) == 0xf) {
-			for (j = 0; j < 15; j++) {
-				printf("%02x ", buf[j]);
-			}
-			printf("  ");
-			for (j = 0; j < 15; j++) {
-				if (buf[j] >= 31 && buf[j] < 127)
-					putchar(buf[j]);
-				else
-					putchar('.');
-			}
-			printf("\n");
-		}
-	}
-}
-
-
 int
 main(void)
 {
-	int res;
+	int i, j, res;
+	char buf[SDCARD_BLOCK_SIZE];
 
 	printf("\n");
 
+	printf("sdcard_init()\n");
 	res = sdcard_init();
 	if (res) {
 		printf("sdcard_init() returned %d\n", res);
 		return (1);
 	}
 
-	res = sdcard_cmd(9, 0);
+	res = sdcard_cmd(SDCARD_CMD_SEND_CSD, 0);
 	if (res) {
 		printf("sdcard_cmd(9, 0) returned %d\n", res);
 		return (1);
 	}
-	printf("CMD09:\n");
-	read_spi(32);
+	printf("CMD9: %d\n", sdcard_read(buf, 32));
 
-	res = sdcard_cmd(10, 0);
+	res = sdcard_cmd(SDCARD_CMD_SEND_CID, 0);
 	if (res) {
 		printf("sdcard_cmd(10, 0) returned %d\n", res);
 		return (1);
 	}
-	printf("CMD10:\n");
-	read_spi(32);
+	printf("CMD10: %d\n", sdcard_read(buf, 32));
 
-	res = sdcard_cmd(58, 0);
+	printf("CMD16:\n");
+	res = sdcard_cmd(SDCARD_CMD_SET_BLOCKLEN, SDCARD_BLOCK_SIZE);
 	if (res) {
-		printf("sdcard_cmd(58, 0) returned %d\n", res);
+		printf("sdcard_cmd(16, %d) returned %d\n", SDCARD_BLOCK_SIZE, res);
 		return (1);
 	}
-	printf("CMD58:\n");
-	read_spi(32);
 
-	res = sdcard_cmd(17, 0);
-	if (res) {
-		printf("sdcard_cmd(17, 0) returned %d\n", res);
-		return (1);
+	for (i = 0; i < 2047 * (1 << 20); i += SDCARD_BLOCK_SIZE) {
+		res = sdcard_cmd(SDCARD_CMD_READ_BLOCK, i);
+		if (res) {
+			printf("sdcard_cmd(17, %d) returned %d\n", i, res);
+			return (1);
+		}
+		sdcard_read(buf, 512);
+#if 1
+		for (j = 0; j < SDCARD_BLOCK_SIZE; j++)
+			if (buf[j] >= 32 && buf[j] < 127)
+				putchar(buf[j]);
+#else
+		if ((i & 0xffe00) == 0)
+			printf("CMD17 %08x\n", i);
+#endif
+		j = sio_getchar(0);
+		if (j == 3)
+			break;
+		if (j == ' ')
+			do {} while (sio_getchar(0) != ' ');
 	}
-	printf("CMD17:\n");
-	read_spi(1024);
 
 	printf("Done\n");
 
