901f8e36073792802f910b9ce9267165728118d6
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-02-24 13:20:16 +0000

    Pronadjen i popravljen bug u strcmp().  DMIPS/MHz pada s 146.3 na 139.4.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@927 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/string.h
+++ src/include/string.h
@@ -20,78 +20,36 @@
 static inline int
 strcmp(const char *s1, const char *s2)
 {
-	int c1, c2, b1, b2;
+	int c1, c2;
 	uint32_t v0;
 	const uint32_t t0 = 0x01010101;
 	const uint32_t t1 = 0x80808080;
 
 	/* Check for aligned pointers for faster operation on 32-bit words */
-	if ((((int)s1 | (int)s2) & 3) == 0) {
-		for (;;) {
-			/* Check whether words are equal */
-			c1 = *((int *)s1);
-			c2 = *((int *)s2);
-			if (c1 != c2) {
-#if _BYTE_ORDER == _LITTLE_ENDIAN
-				if ((c1 & 0xffff) != (c2 & 0xffff)) {
-					b1 = c1 & 0xff;
-					b2 = c2 & 0xff;
-					if (b1 == 0 || b1 != b2)
-						return (b1 - b2);
-					b1 = c1 & 0xff00;
-					b2 = c2 & 0xff00;
-					return (b1 - b2);
-				} else {
-					b1 = c1 & 0xff0000;
-					b2 = c2 & 0xff0000;
-					if (b1 == 0 || b1 != b2)
-						return (b1 - b2);
-					c1 >>= 24;
-					c2 >>= 24;
-					b1 = c1 & 0xff;
-					b2 = c2 & 0xff;
-					return (b1 - b2);
-				}
-#elif _BYTE_ORDER == _BIG_ENDIAN
-				if ((c1 & 0xffff0000) != (c2 & 0xffff0000)) {
-					c1 >>= 16;
-					c2 >>= 16;
-					b1 = c1 & 0xff00;
-					b2 = c2 & 0xff00;
-					if (b1 == 0 || b1 != b2)
-						return (b1 - b2);
-					b1 = c1 & 0xff;
-					b2 = c2 & 0xff;
-					return (b1 - b2);
-				} else {
-					b1 = c1 & 0xff00;
-					b2 = c2 & 0xff00;
-					if (b1 == 0 || b1 != b2)
-						return (b1 - b2);
-					b1 = c1 & 0xff;
-					b2 = c2 & 0xff;
-					return (b1 - b2);
-				}
-#else
-#error "Unsupported byte order."
-#endif
-			}
-			/* Check if the word contains any zero bytes */
-			v0 = (((uint32_t)c1) - t0) & t1;
-			if (v0) {
-				/* Maybe */           
-				if (__predict_false(v0 & ~((uint32_t)c1))) 
-					return(0);
-			}
-			s1 += 4;
-			s2 += 4;
+	if (((int)s1 | (int)s2) & 0x3)
+		goto slow;
+
+	for (;;) {
+		/* Check whether words are equal */
+		c1 = *((int *)s1);
+		c2 = *((int *)s2);
+		v0 = (((uint32_t)c1) - t0) & t1;
+		if (c1 != c2) {
+slow:
+			do {
+				c1 = *(const unsigned char *)s1++;
+				c2 = *(const unsigned char *)s2++;
+			} while (c1 != 0 && c1 == c2);
+			return (c1 - c2);
 		}
-	} else {
-		do {
-			c1 = *(const unsigned char *)s1++;
-			c2 = *(const unsigned char *)s2++;
-		} while (c1 != 0 && c1 == c2);
-		return (c1 - c2);
+		/* Check if the word contains any zero bytes */
+		if (v0) {
+			/* Maybe */           
+			if (__predict_true(v0 & ~((uint32_t)c1))) 
+				return(0);
+		}
+		s1 += 4;
+		s2 += 4;
 	}
 }
 
