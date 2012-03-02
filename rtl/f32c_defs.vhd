1d2bac314cde0962e5dd3cc6da1e093115cb0d10
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-03-02 07:57:52 +0000

    Priprema za interrupte.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@950 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/f32c_defs.vhd
+++ rtl/f32c_defs.vhd
@@ -245,7 +245,7 @@ constant OP_MAJOR_ALT:		std_logic_vector := "11";
 
 constant ALT_HI:		std_logic_vector := "000";
 constant ALT_LO:		std_logic_vector := "001";
-constant ALT_COP0_COUNT:	std_logic_vector := "011";
-constant ALT_PC_8:		std_logic_vector := "111";
+constant ALT_PC_8:		std_logic_vector := "010";
+constant ALT_COP0:		std_logic_vector := "011";
 
 end;
