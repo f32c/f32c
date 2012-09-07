d6afd0c4e5a1260121cb86f40e16709d6b506a7d
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-09-07 20:24:40 +0000

    Programiranje SPI flasha sad radi tak da se s MicroSD kartice procita file /ulx2s_~1.img i to presnimi u Flash.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1010 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/fprog/fprog.c
+++ src/fprog/fprog.c
@@ -1,476 +1,180 @@
 
-#ifndef _MIPS_ISA
-#define X
-#endif
-
-#ifndef X
+#include <sys/param.h>
+#include <sdcard.h>
 #include <io.h>
 #include <sio.h>
-#include <types.h>
-#else
-#include <sys/types.h>
-#include <fcntl.h>
-#endif
+#include <spi.h>
 #include <stdio.h>
 
+#include <fatfs/ff.h>
 
-/* Host -> board commands */
-#define	FP_CMD_READID		1
-#define	FP_CMD_READ_SECTOR	2
-#define	FP_CMD_CSUM_SECTOR	3
-#define	FP_CMD_CSUM_CHIP	4
-#define	FP_CMD_ENABLE_WRITE	5
-#define	FP_CMD_DISABLE_WRITE	6
-#define	FP_CMD_ERASE_CHIP	7
-#define	FP_CMD_ERASE_SECTOR	8
-#define	FP_CMD_WRITE_SECTOR	9
-#define	FP_CMD_SET_BAUD		10
-#define	FP_CMD_DONE		11
+#define	PAGE_SIZE	4096	/* SPI flash minimum unit of work */
+#define	SECTOR_SIZE	512	/* FAT sector size */
 
-#define	FP_START_MARK		0x817e
+//void (*sio_idle_fn)(void) = NULL;
 
-#define	FP_PAGESIZE		4096
+FATFS fh;
+char buf[SECTOR_SIZE];
 
-#define	FP_MAX_BLOCKSIZE	(sizeof(struct fp_header) + FP_PAGESIZE)
+#define	CMD_READID		1
+#define	CMD_ENABLE_WRITE	2
+#define	CMD_DISABLE_WRITE	3
+#define	CMD_WRITE_PAGE		4
 
-typedef struct fp_header {
-	uint16_t	start_mark;
-	uint8_t		fp_cmd;
-	uint8_t		fp_res;
-	uint32_t	fp_addr;
-	uint32_t	payload_csum;
-	uint16_t	payload_len;
-	uint16_t	header_csum;
-} fph_t;
-
-
-static char buf[FP_MAX_BLOCKSIZE + 8];
-
-
-#ifndef X
-#define	rxbyte()	getchar()
-#define	txbyte(x)	putchar(x)
-#else
-static int sioid;
-static int
-rxbyte(void)
+static void
+spi_wait()
 {
-	char b, res;
-	
-	do {
-		res = read(sioid, &b, 1);
-		if (res == 0)
-			usleep(10000);
-	} while (res != 1);
-	return(b);
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x05); /* RDSR */
+	do {} while (spi_byte(SPI_PORT_FLASH, 0x05) & 1);
 }
-#endif
 
 
 static int
-rx_frame(void)
+dispatch(int cmd, uint addr)
 {
-	char *cp;
-	char c;
-	fph_t *fphp = (fph_t *) buf;
-	uint32_t csum;
-
-	cp = buf;
-	fphp->start_mark = FP_START_MARK;
-	
-	do {
-		c = rxbyte();
-
-		/* Detect start of record marker */
-		if (cp < &buf[2]) {
-			if (cp == &buf[0] && c == buf[0]) {
-				cp++;
-			} else if (cp == &buf[1]) {
-				if (c == buf[1])
-					cp++;
-				else 
-					cp = buf;
-			}
-			continue;
-		}
-		*cp++ = c;
-
-		/* Header complete?  Header checksum OK? */
-		if (cp == &buf[sizeof(*fphp)]) {
-			for (csum = 0, cp = buf;
-			    cp < (char *) &fphp->header_csum; cp++)
-				csum += (*cp & 0xff);
-			if (csum != fphp->header_csum)
-				return (-1);
-			if (fphp->payload_len > FP_PAGESIZE)
-				return (-2);
-			cp = &buf[sizeof(*fphp)];
-		}
-
-	} while (cp < &buf[sizeof(*fphp) + fphp->payload_len]);
-
-	/* Payload checksum OK? */
-	for (csum = 0; cp > &buf[sizeof(*fphp)];)
-		csum += (*--cp & 0xff);
-	if (csum != fphp->payload_csum)
-		return (-3);
-	else
-		return(0);
-}
-
-
-static void
-tx_frame(void)
-{
-	char *cp;
-	fph_t *fphp = (fph_t *) buf;
-	int csum, len;
-	int i;
-
-	fphp->start_mark = FP_START_MARK;
-
-	/* Compute payload checksum */
-	for (csum = 0, cp = &buf[sizeof(*fphp)];
-	    cp < &buf[sizeof(*fphp) + fphp->payload_len]; cp++)
-		csum += (*cp & 0xff);
-	fphp->payload_csum = csum;
-
-	/* Compute header checsum */
-	for (csum = 0, cp = buf; cp < (char *) &fphp->header_csum; cp++)
-		csum += (*cp & 0xff);
-	fphp->header_csum = csum;
-
-	/* Compute frame length, and add a few dummy trailer bytes */
-	len = sizeof(*fphp) + fphp->payload_len;
-	for (i = 0; i < 4; i++, len++)
-		buf[len] = 0;
-
-	/* Send it */
-#ifndef X
-	for (cp = buf; cp < &buf[len]; cp++)
-		txbyte(*cp);
-#else
-	write(sioid, buf, len);
-#endif
-
-}
-
-
-#ifndef X
-static void
-dispatch(void)
-{
-	fph_t *fphp = (fph_t *) buf;
 	int error = 0;
 	int i;
 	char *cp;
 
 	/* Check that the device is idle before issuing next command */
-	spi_stop_transaction();
-	spi_start_transaction();
-	spi_byte(0x05); /* RDSR */
-	do {} while (spi_byte(0x05) & 1);
-	spi_stop_transaction();
-	OUTB(IO_LED, rdtsc());
+	spi_wait();
 
-	switch (fphp->fp_cmd) {
-	case FP_CMD_READID:
-		/* SPI: Read JEDEC ID */
-		spi_start_transaction();
-		spi_byte(0x9f); /* JEDEC Read-ID */
-		for (i = 0, cp = &buf[sizeof(*fphp)]; i < 3; i++)
-			*cp++ = spi_byte(0);
-		spi_stop_transaction();
-		fphp->payload_len = 3;
+	switch (cmd) {
+	case CMD_ENABLE_WRITE:
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
+
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
+		spi_byte(SPI_PORT_FLASH, 0); /* Clear write-protect bits */
 		break;
 
-	case FP_CMD_ENABLE_WRITE:
-		spi_start_transaction();
-		spi_byte(0x50); /* EWSR */
-		spi_stop_transaction();
+#if 0
+	case CMD_DISABLE_WRITE:
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
 
-		spi_start_transaction();
-		spi_byte(0x01); /* WRSR */
-		spi_byte(0);	/* Clear write-protect bits */
-		spi_stop_transaction();
-
-		fphp->payload_len = 0;
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
+		spi_byte(SPI_PORT_FLASH, 0x1c);	/* Set write-protect bits */
 		break;
+#endif
 
-	case FP_CMD_DISABLE_WRITE:
-		spi_start_transaction();
-		spi_byte(0x50); /* EWSR */
-		spi_stop_transaction();
+	case CMD_WRITE_PAGE:
+		cp = buf;
 
-		spi_start_transaction();
-		spi_byte(0x01); /* WRSR */
-		spi_byte(0x1c);	/* Set write-protect bits */
-		spi_stop_transaction();
-
-		fphp->payload_len = 0;
-		break;
-
-	case FP_CMD_ERASE_CHIP:
-		spi_start_transaction();
-		spi_byte(0x06); /* WREN */
-		spi_stop_transaction();
-
-		spi_start_transaction();
-		spi_byte(0x60); /* Chip-erase */
-		spi_stop_transaction();
-
-		spi_start_transaction();
-		spi_byte(0x05); /* RDSR */
-		do {} while (spi_byte(0x05) & 1);
-		spi_stop_transaction();
-
-		spi_start_transaction();
-		spi_byte(0x04); /* WRDI */
-		spi_stop_transaction();
-
-		fphp->payload_len = 0;
-		break;
-
-	case FP_CMD_WRITE_SECTOR:
-
-		if (fphp->payload_len != FP_PAGESIZE) {
-			fphp->payload_len = 0;
-			error = -1;
-			break;
-		};
-		cp = &buf[sizeof(*fphp)];
-
-		spi_start_transaction();
-		spi_byte(0x06); /* WREN */
-		spi_stop_transaction();
-
-		spi_start_transaction();
-		spi_byte(0x20); /* Sector erase */
-		spi_byte(fphp->fp_addr >> 16);
-		spi_byte(fphp->fp_addr >> 8);
-		spi_byte(fphp->fp_addr);
-		spi_stop_transaction();
-
-		spi_start_transaction();
-		spi_byte(0x05); /* RDSR */
-		do {} while (spi_byte(0x05) & 1);
-		spi_stop_transaction();
-
-		spi_start_transaction();
-		spi_byte(0x06); /* WREN */
-		spi_stop_transaction();
 
 #if 1
-		spi_start_transaction();
-		spi_byte(0xad); /* AAI write mode */
-		spi_byte(fphp->fp_addr >> 16);
-		spi_byte(fphp->fp_addr >> 8);
-		spi_byte(fphp->fp_addr);
-		spi_byte(*cp++);
-		spi_byte(*cp++);
-		spi_stop_transaction();
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0xad); /* AAI write mode */
+		spi_byte(SPI_PORT_FLASH, addr >> 16);
+		spi_byte(SPI_PORT_FLASH, addr >> 8);
+		spi_byte(SPI_PORT_FLASH, addr);
+		spi_byte(SPI_PORT_FLASH, *cp++);
+		spi_byte(SPI_PORT_FLASH, *cp++);
 
-		for (i = 2; i < FP_PAGESIZE; i++) {
+		for (i = 2; i < PAGE_SIZE; i++) {
 
-			spi_start_transaction();
-			spi_byte(0x05); /* RDSR */
-			do {} while (spi_byte(0x05) & 1);
-			spi_stop_transaction();
+			spi_wait();
 
-			spi_start_transaction();
-			spi_byte(0xad); /* AAI write mode */
-			spi_byte(*cp++);
-			spi_byte(*cp++);
-			spi_stop_transaction();
+			spi_start_transaction(SPI_PORT_FLASH);
+			spi_byte(SPI_PORT_FLASH, 0xad); /* AAI write mode */
+			spi_byte(SPI_PORT_FLASH, *cp++);
+			spi_byte(SPI_PORT_FLASH, *cp++);
 		}
 #else
 		int j;
-		for (i = 0; i < FP_PAGESIZE; i++) {
-			spi_start_transaction();
-			spi_byte(0x02); /* Byte program */
-			j = fphp->fp_addr + i;
-			spi_byte(j >> 16);
-			spi_byte(j >> 8);
-			spi_byte(j);
-			spi_byte(*cp++);
-			spi_stop_transaction();
-		}
 #endif
 
-		spi_start_transaction();
-		spi_byte(0x05); /* RDSR */
-		do {} while (spi_byte(0x05) & 1);
-		spi_stop_transaction();
+		spi_wait();
 
-		spi_start_transaction();
-		spi_byte(0x04); /* WRDI */
-		spi_stop_transaction();
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x04); /* WRDI */
 
-		spi_start_transaction();
-		spi_byte(0x05); /* RDSR */
-		do {} while (spi_byte(0x05) & 1);
-		spi_stop_transaction();
+		spi_wait();
 
-		spi_start_transaction();
-		spi_byte(0x0b); /* High-speed read */
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_byte(0);
-		spi_stop_transaction();
-
-		fphp->payload_len = 0;
+		/* XXX cemu ovo?!? */
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x0b); /* High-speed read */
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_byte(SPI_PORT_FLASH, 0);
 		break;
 
 	default:
 		error = -1;
-		fphp->payload_len = 0;
 		break;
 	}
 
-	fphp->fp_res = error;
-	tx_frame();
+	return (error);
 }
 
 
-int
+void
 main(void)
 {
-	int error;
-	fph_t *fphp = (fph_t *) buf;
+	FIL fp;
+	int i, j, k, addr, res;
+	uint got;
 
-	printf("\n\nfprog ready, switching baudrate...\n");
+        if (sdcard_init() || sdcard_cmd(SD_CMD_SEND_CID, 0) ||
+            sdcard_read((char *) buf, 16))
+                return;
 
-	/* Set baudrate */
-	OUTW(IO_SIO_BAUD, 81250000 / 460800);
+        f_mount(0, &fh);
+	if (f_open(&fp, "ulx2s_~1.img", FA_READ))
+		return;
 
-	printf("\n\nfprog ready...\n");
+	dispatch(CMD_ENABLE_WRITE, 0);
+	for (i = 0; i < 1024; i++) {
+		printf("%d ", i);
+		OUTB(IO_LED, i / 4);
 
-	do {
-		error = rx_frame();
-		if (error) {
-			fphp->fp_res = error;
-			fphp->payload_len = 0;
-			tx_frame();
-			continue;
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
+		spi_start_transaction(SPI_PORT_FLASH);
+		spi_byte(SPI_PORT_FLASH, 0x20); /* Sector erase */
+		spi_byte(SPI_PORT_FLASH, (i * PAGE_SIZE) >> 16);
+		spi_byte(SPI_PORT_FLASH, (i * PAGE_SIZE) >> 8);
+		spi_byte(SPI_PORT_FLASH, 0);
+		spi_wait();
+
+		for (j = 0; j < PAGE_SIZE / SECTOR_SIZE; j++) {
+			if ((res = f_read(&fp, buf, SECTOR_SIZE, &got)))
+				goto done;
+			if (got != SECTOR_SIZE)
+				goto done;
+
+			for (k = 0; k < SECTOR_SIZE; k++) {
+				/* XXX do we need this? */
+				spi_start_transaction(SPI_PORT_FLASH);
+				spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
+
+				addr = i * PAGE_SIZE + j * SECTOR_SIZE + k;
+				spi_start_transaction(SPI_PORT_FLASH);
+				spi_byte(SPI_PORT_FLASH, 0x02); /* Byte prog. */
+				spi_byte(SPI_PORT_FLASH, addr >> 16);
+				spi_byte(SPI_PORT_FLASH, addr >> 8);
+				spi_byte(SPI_PORT_FLASH, addr);
+				spi_byte(SPI_PORT_FLASH, buf[k]);
+				spi_wait();
+			}
+
+			spi_start_transaction(SPI_PORT_FLASH);
+			spi_byte(SPI_PORT_FLASH, 0x04); /* WRDI */
+			spi_wait();
 		}
-
-		dispatch();
-	} while (1);
-
-	return (0);
+	}
+done:
+	dispatch(CMD_DISABLE_WRITE, 0);
 }
-#endif
-
-#ifdef X
-int
-main(int argc, char *argv[])
-{
-	int i, j, error, maxpages = 0;
-	fph_t *fphp = (fph_t *) buf;
-	char *cp;
-	int f;
-
-	/* XXX replace with cfmakeraw() and tcsetattr() */
-	error = system("stty -f /dev/cuaU0.init cs8 raw speed 460800 >/dev/null");
-        if (error < 0)
-                return (error);
-	sioid = open("/dev/cuaU0", O_RDWR|O_NONBLOCK|O_DIRECT|O_TTY_INIT);
-        if (sioid < 0)
-                return (sioid);
-
-	if (argc < 2) {
-		fprintf(stderr, "Must provide file name\n");
-		return(1);
-	}
-	f = open(argv[1], O_RDONLY);
-	if (f < 0) {
-		fprintf(stderr, "cannot open file %s\n", argv[1]);
-		return(1);
-	}
-
-	fphp->fp_cmd = FP_CMD_READID;
-	fphp->fp_addr = 0;
-	fphp->payload_len = 0;
-	tx_frame();
-	if (rx_frame()) {
-		fprintf(stderr, "FP_CMD_READID: rx_frame() failed\n");
-		return(1);
-	}
-	if (fphp->fp_res) {
-		fprintf(stderr, "FP_CMD_READID failed\n");
-		return(1);
-	}
-	cp = &buf[sizeof(*fphp)];
-	printf("JEDEC ID:");
-	for (i = 0; i < fphp->payload_len; i++)
-		printf(" %02x", *cp++ & 0xff);
-	printf("\n");
-	if ((*--cp & 0xff) == 0x4a)
-		maxpages = 1024;
-	else
-		maxpages = 512;
-maxpages = 1024;
-
-	fphp->fp_cmd = FP_CMD_ENABLE_WRITE;
-	fphp->fp_addr = 0;
-	fphp->payload_len = 0;
-	tx_frame();
-	if (rx_frame()) {
-		fprintf(stderr, "FP_CMD_ENABLE_WRITE: rx_frame() failed\n");
-		return(1);
-	}
-	if (fphp->fp_res) {
-		fprintf(stderr, "FP_CMD_ENABLE_WRITE failed\n");
-		return(1);
-	}
-	printf("Write mode enabled\n");
-
-	for (i = 0; i < maxpages; i++) {
-		fphp->fp_cmd = FP_CMD_WRITE_SECTOR;
-		fphp->fp_addr = i * FP_PAGESIZE;
-		fphp->payload_len = FP_PAGESIZE;
-
-		j = read(f, &buf[sizeof(*fphp)], FP_PAGESIZE);
-		if (j != FP_PAGESIZE)
-			break;
-
-		printf("Writing page %d\n", i);
-
-		tx_frame();
-		if (rx_frame()) {
-			fprintf(stderr,
-			    "FP_CMD_WRITE_SECTOR: rx_frame() failed\n");
-			return(1);
-		}
-		if (fphp->fp_res) {
-			fprintf(stderr, "FP_CMD_WRITE_SECTOR failed\n");
-			return(1);
-		}
-	}
-	printf("Wrote %d pages\n", i);
-
-	fphp->fp_cmd = FP_CMD_DISABLE_WRITE;
-	fphp->fp_addr = 0;
-	fphp->payload_len = 0;
-	tx_frame();
-	if (rx_frame()) {
-		fprintf(stderr, "FP_CMD_DISABLE_WRITE: rx_frame() failed\n");
-		return(1);
-	}
-	if (fphp->fp_res) {
-		fprintf(stderr, "FP_CMD_DISABLE_WRITE failed\n");
-		return(1);
-	}
-	printf("Write mode disabled\n");
-
-	return (0);
-}
-#endif
