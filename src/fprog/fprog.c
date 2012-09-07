6754707c9fe7e5c266e84ae20b007126c23795f6
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-09-07 22:08:52 +0000

    Brza varijanta programiranja SPI flash-a: brise se cijeli chip odjednom, a ne u komadima po 4096 byteova; i koristi se slijedni, tzv. auto address increment write mode, koji stedi SPI bandwidth jer se uz svaki byte ne treba slati i adresa.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1011 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/fprog/fprog.c
+++ src/fprog/fprog.c
@@ -8,18 +8,14 @@
 
 #include <fatfs/ff.h>
 
-#define	PAGE_SIZE	4096	/* SPI flash minimum unit of work */
-#define	SECTOR_SIZE	512	/* FAT sector size */
 
-//void (*sio_idle_fn)(void) = NULL;
+#define	PAGE_SIZE	4096	/* SPI flash minimum unit of work */
+#define	SECTOR_SIZE	512	/* buffer size */
+
 
 FATFS fh;
 char buf[SECTOR_SIZE];
 
-#define	CMD_READID		1
-#define	CMD_ENABLE_WRITE	2
-#define	CMD_DISABLE_WRITE	3
-#define	CMD_WRITE_PAGE		4
 
 static void
 spi_wait()
@@ -31,150 +27,82 @@ spi_wait()
 }
 
 
-static int
-dispatch(int cmd, uint addr)
-{
-	int error = 0;
-	int i;
-	char *cp;
-
-	/* Check that the device is idle before issuing next command */
-	spi_wait();
-
-	switch (cmd) {
-	case CMD_ENABLE_WRITE:
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
-
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
-		spi_byte(SPI_PORT_FLASH, 0); /* Clear write-protect bits */
-		break;
-
-#if 0
-	case CMD_DISABLE_WRITE:
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
-
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
-		spi_byte(SPI_PORT_FLASH, 0x1c);	/* Set write-protect bits */
-		break;
-#endif
-
-	case CMD_WRITE_PAGE:
-		cp = buf;
-
-
-#if 1
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0xad); /* AAI write mode */
-		spi_byte(SPI_PORT_FLASH, addr >> 16);
-		spi_byte(SPI_PORT_FLASH, addr >> 8);
-		spi_byte(SPI_PORT_FLASH, addr);
-		spi_byte(SPI_PORT_FLASH, *cp++);
-		spi_byte(SPI_PORT_FLASH, *cp++);
-
-		for (i = 2; i < PAGE_SIZE; i++) {
-
-			spi_wait();
-
-			spi_start_transaction(SPI_PORT_FLASH);
-			spi_byte(SPI_PORT_FLASH, 0xad); /* AAI write mode */
-			spi_byte(SPI_PORT_FLASH, *cp++);
-			spi_byte(SPI_PORT_FLASH, *cp++);
-		}
-#else
-		int j;
-#endif
-
-		spi_wait();
-
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x04); /* WRDI */
-
-		spi_wait();
-
-		/* XXX cemu ovo?!? */
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x0b); /* High-speed read */
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_byte(SPI_PORT_FLASH, 0);
-		break;
-
-	default:
-		error = -1;
-		break;
-	}
-
-	return (error);
-}
-
-
 void
 main(void)
 {
 	FIL fp;
-	int i, j, k, addr, res;
+	int i, j, k, res;
 	uint got;
 
-        if (sdcard_init() || sdcard_cmd(SD_CMD_SEND_CID, 0) ||
-            sdcard_read((char *) buf, 16))
-                return;
-
-        f_mount(0, &fh);
-	if (f_open(&fp, "ulx2s_~1.img", FA_READ))
+	if (sdcard_init() || sdcard_cmd(SD_CMD_SEND_CID, 0) ||
+	    sdcard_read((char *) buf, 16)) {
+		printf("Nije pronadjena MicroSD kartica!\n");
 		return;
+	}
 
-	dispatch(CMD_ENABLE_WRITE, 0);
+	f_mount(0, &fh);
+	if (f_open(&fp, "ulx2s_~1.img", FA_READ)) {
+		printf("Nije pronadjena datoteka /ulx2s_~1.img!\n");
+		return;
+	}
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
+	spi_byte(SPI_PORT_FLASH, 0); /* Clear write-protect bits */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x60); /* Chip erase */
+	spi_wait();
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
+
 	for (i = 0; i < 1024; i++) {
 		printf("%d ", i);
-		OUTB(IO_LED, i / 4);
-
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
-		spi_start_transaction(SPI_PORT_FLASH);
-		spi_byte(SPI_PORT_FLASH, 0x20); /* Sector erase */
-		spi_byte(SPI_PORT_FLASH, (i * PAGE_SIZE) >> 16);
-		spi_byte(SPI_PORT_FLASH, (i * PAGE_SIZE) >> 8);
-		spi_byte(SPI_PORT_FLASH, 0);
-		spi_wait();
+		OUTB(IO_LED, i >> 2);
 
 		for (j = 0; j < PAGE_SIZE / SECTOR_SIZE; j++) {
-			if ((res = f_read(&fp, buf, SECTOR_SIZE, &got)))
+			if ((res = f_read(&fp, buf, SECTOR_SIZE, &got))) {
+				printf("\nf_read() failed!\n");
 				goto done;
-			if (got != SECTOR_SIZE)
+			}
+			if (got != SECTOR_SIZE) {
+				printf("\nf_read(): short read!\n");
 				goto done;
-
-			for (k = 0; k < SECTOR_SIZE; k++) {
-				/* XXX do we need this? */
-				spi_start_transaction(SPI_PORT_FLASH);
-				spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
-
-				addr = i * PAGE_SIZE + j * SECTOR_SIZE + k;
-				spi_start_transaction(SPI_PORT_FLASH);
-				spi_byte(SPI_PORT_FLASH, 0x02); /* Byte prog. */
-				spi_byte(SPI_PORT_FLASH, addr >> 16);
-				spi_byte(SPI_PORT_FLASH, addr >> 8);
-				spi_byte(SPI_PORT_FLASH, addr);
-				spi_byte(SPI_PORT_FLASH, buf[k]);
-				spi_wait();
 			}
 
-			spi_start_transaction(SPI_PORT_FLASH);
-			spi_byte(SPI_PORT_FLASH, 0x04); /* WRDI */
-			spi_wait();
+			for (k = 0; k < SECTOR_SIZE; k += 2) {
+				spi_wait();
+				spi_start_transaction(SPI_PORT_FLASH);
+				spi_byte(SPI_PORT_FLASH, 0xad); /* AAI write */
+				if (i == 0 && j == 0 && k == 0) {
+					spi_byte(SPI_PORT_FLASH, 0);
+					spi_byte(SPI_PORT_FLASH, 0);
+					spi_byte(SPI_PORT_FLASH, 0);
+				}
+				spi_byte(SPI_PORT_FLASH, buf[k]);
+				spi_byte(SPI_PORT_FLASH, buf[k + 1]);
+			}
 		}
 	}
+
 done:
-	dispatch(CMD_DISABLE_WRITE, 0);
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x04); /* WRDI */
+	spi_wait();
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
+	spi_byte(SPI_PORT_FLASH, 0x1c);	/* Set write-protect bits */
+
+	printf("\nGotovo!\n");
 }
