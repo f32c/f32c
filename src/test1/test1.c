6b99d4da6c8f2a87a2feca0e9971e4d813cc3e3e
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-11-03 21:09:08 +0000

    Prvi test za SRAM: ispituje pisanje i citanje podataka sirine 1, 2 i 4 bytea, te kopira kod jednostavne funkcije u SRAM, i izvodi ju iz SRAM-a.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1090 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test1.c
+++ src/test1/test1.c
@@ -5,256 +5,78 @@
 #include <stdio.h>
 
 
-static int dds_base;
-static int fwd_rev, left_right, turr_left, turr_right, gun_elev, gun_bullet;
-static int mg_sound, gun_sound, engine_key;
+typedef int fn1_t(int, uint32_t *);
+fn1_t fn1;
 
-
-static void
-wait(int len)
-{
-	static int t0;
-	int t1, d;
-
-	do {
-		RDTSC(t1);
-		d = t1 - t0;
-		if (d < 0)
-			d = -d;
-	} while (d < len);
-	t0 = t1;
-}
-
-
-static void
-txbit(int bit, int len)
-{
-	int dds_out;
-
-	if (bit) {
-		dds_out = dds_base + 20;
-		OUTW(IO_DDS, dds_out);
-		wait(len);
-	} else {
-		dds_out = dds_base - 20;
-		OUTW(IO_DDS, dds_out);
-		wait(len);
-	}
-}
-
-
-/*
- * 32 bits = 20 ms -> 0.625 ms / bit -> 1600 bit / s
- * 1 bit = 2 symbols
- * 64 symbols = 20 ms -> 0.3125 ms / symbol
- */
-
-#define CYCLE 25390 /* za ftsc = 81.25 MHz */
-
-
-static void
-fm_tx(void)
-{
-	int i, outw, csum;
-
-	csum = 4;
-	csum ^= mg_sound;
-	csum ^= gun_sound << 1;
-	csum ^= engine_key << 1;
-	csum ^= gun_bullet << 1;
-	csum ^= gun_elev << 2;
-	csum ^= turr_left << 3;
-	csum ^= turr_right;
-	csum ^= fwd_rev << 2;
-	csum ^= fwd_rev >> 2;
-	csum ^= left_right;
-	csum ^= left_right >> 4;
-	csum &= 0xf;
-
-	outw = 0x3;
-	outw |= csum << 2;
-	outw |= mg_sound << 6;
-	outw |= engine_key << 7;
-	outw |= left_right << 8;
-	outw |= gun_bullet << 13;
-	outw |= gun_elev << 14;
-	outw |= turr_left << 15;
-	outw |= turr_right << 16;
-	outw |= gun_sound << 17;
-	outw |= fwd_rev << 18;
-	outw |= 0x3 << 23;
-
-	for (i = 0; i < 32; i++) {
-		if (outw & 0x80000000) {
-			/* 1 */
-			txbit(0, CYCLE);
-			txbit(1, CYCLE);
-		} else {
-			/* 0 */
-			txbit(1, CYCLE);
-			txbit(0, CYCLE);
-		}
-		outw <<= 1;
-	}
-
-	txbit(0, 14 * CYCLE);
-	txbit(1, 2 * CYCLE);
-}
-
-
-#if 0
-int
+void
 main(void)
 {
+	uint8_t *p8;
+	uint16_t *p16;
+	uint32_t *p32, *fn1_rom;
+	fn1_t *fn1_ram;
 	int i;
 
-	dds_base = 350321; /* 325 MHz PLL, 27.145 MHz */
-
-	turr_left = 1;
-	turr_right = 1;
-	gun_elev = 1;
-	gun_bullet = 1;
-	mg_sound = 1;
-	gun_sound = 1;
-	engine_key = 1;
-	fwd_rev = 16;
-	left_right = 16;
-
+	printf("\nuint8_t *:  ");
+	p8 = (void *) 0x80000000;
+	for (i = 0; i < 32; i++, p8 += 333)
+		*p8 = i;
+	p8 = (void *) 0x80000000;
+	for (i = 0; i < 32; i++, p8 += 333)
+		printf("%d ", *p8);
 	printf("\n");
-	printf("Palim motor...\n");
-	engine_key = 0;
-	for (i = 0; i < 200; i++)
-		fm_tx();
 
-	printf("Cekam da se zgrije motor...\n");
-	engine_key = 1;
-	for (i = 0; i < 300; i++)
-		fm_tx();
-	
-	printf("Kupola desno...\n");
-	turr_right = 0;
-	for (i = 0; i < 100; i++)
-		fm_tx();
-	turr_right = 1;
-	for (i = 0; i < 10; i++)
-		fm_tx();
+	printf("uint16_t *: ");
+	p16 = (void *) 0x80000000;
+	for (i = 0; i < 32; i++, p16 += 333)
+		*p16 = i;
+	p16 = (void *) 0x80000000;
+	for (i = 0; i < 32; i++, p16 += 333)
+		printf("%d ", *p16);
+	printf("\n");
 
-	printf("Kupola lijevo...\n");
-	turr_left = 0;
-	for (i = 0; i < 100; i++)
-		fm_tx();
-	turr_left = 1;
-	for (i = 0; i < 10; i++)
-		fm_tx();
+	printf("uint32_t *: ");
+	p32 = (void *) 0x80000000;
+	for (i = 0; i < 32; i++, p32 += 333)
+		*p32 = i;
+	p32 = (void *) 0x80000000;
+	for (i = 0; i < 32; i++, p32 += 333)
+		printf("%d ", *p32);
+	printf("\n");
 
-	printf("Gasim motor...\n");
-	engine_key = 0;
-	for (i = 0; i < 200; i++)
-		fm_tx();
+	/* Kopiraj fn1() iz BRAM u SRAM */
+	fn1_rom = (void *) &fn1;
+        fn1_ram = (void *) 0x80001000;
+        p32 = (void *) fn1_ram;
+	for (i = 0; i < 512; i++)
+		*p32++ = *fn1_rom++;
 
-	return (0);
-}
-#endif
+	/* Izvedi fn1() u BRAM */
+        p32 = (void *) 0x80000000;
+	*p32 = 0;
+	printf("BRAM: %08x\n", *p32);
+	i = fn1(1, p32);
+	printf("%08x %08x\n", i, *p32);
 
-
-#if 1
-int
-main(void)
-{
-	int c, speed;
-
+	/* Izvedi fn1() u SRAM */
+        p32 = (void *) 0x80000000;
+	*p32 = 0;
+	printf("SRAM: %08x\n", *p32);
+#if 0
+	printf("Prebaci sw(2) i sw(3) u '1' (tocno tim redom)!\n");
 	do {
-		/* Bail out to bootloader on CTRL+C */
-		c = sio_getchar(0);
-		if (c == 3)
-			return (0);
-		c = random() >> 8;
-		OUTB(IO_LED, c);
-
-		INB(c, IO_DIPSW);
-
-		/* Select vechile speed */
-		speed = (c & 0xf) >> 2;
-
-		/* Select carrier frequency */
-		switch (c & 0x3) {
-		case 0:
-			dds_base = 350321; /* CH1: 325 MHz PLL, 27.145 MHz */
-			break;
-		case 2:
-			dds_base = 349676; /* CH3: 325 MHz PLL, 27.095 MHz */
-			break;
-		default:
-			dds_base = 0; /* Do not transmit */
-			break;
-		}
-		
-		turr_left = 1;
-		turr_right = 1;
-		gun_elev = 1;
-		gun_bullet = 1;
-		mg_sound = 1;
-		gun_sound = 1;
-		engine_key = 1;
-		fwd_rev = 16;
-		left_right = 16;
-
-		INB(c, IO_PUSHBTN);
-		c &= BTN_CENTER | BTN_UP | BTN_DOWN | BTN_LEFT | BTN_RIGHT;
-		switch (c) {
-		case BTN_LEFT | BTN_CENTER | BTN_RIGHT:
-			engine_key = 0;
-			break;
-		case BTN_UP | BTN_DOWN:
-			mg_sound = 0;
-			break;
-		case BTN_CENTER | BTN_UP:
-			gun_sound = 0;
-			break;
-		case BTN_CENTER | BTN_DOWN:
-			gun_elev = 0;
-			break;
-		case BTN_CENTER | BTN_LEFT:
-			turr_left = 0;
-			break;
-		case BTN_CENTER | BTN_RIGHT:
-			turr_right = 0;
-			break;
-		case BTN_UP:
-			fwd_rev = 19 + speed;
-			break;
-		case BTN_UP | BTN_LEFT:
-			fwd_rev = 20 + speed;
-			left_right = 19;
-			break;
-		case BTN_UP | BTN_RIGHT:
-			fwd_rev = 20 + speed;
-			left_right = 13;
-			break;
-		case BTN_DOWN:
-			fwd_rev = 13 - speed;
-			break;
-		case BTN_DOWN | BTN_LEFT:
-			fwd_rev = 12 - speed;
-			left_right = 23 + speed;
-			break;
-		case BTN_DOWN | BTN_RIGHT:
-			fwd_rev = 12 - speed;
-			left_right = 9 - speed;
-			break;
-		case BTN_LEFT:
-			left_right = 23 + speed;
-			fwd_rev = 20 + speed;
-			break;
-		case BTN_RIGHT:
-			left_right = 9 - speed;
-			fwd_rev = 20 + speed;
-			break;
-		default:
-			break;
-		}
-
-		fm_tx();
-	} while (1);
-}
+		INB(i, IO_PUSHBTN + 1);
+	} while ((i & 0xc) != 0xc);
 #endif
+	i = fn1_ram(1, p32);
+	printf("%08x %08x\n", i, *p32);
+}
+
+int
+fn1(int a, uint32_t *p32)
+{
+
+	a += *p32 + 0x100;
+	*p32 = a++;
+	return (a);
+}
