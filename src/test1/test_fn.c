8ee6a4700a5fbfa97d937bb64755c4cb7d72d328
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2012-11-05 12:38:21 +0000

    Dodani testove za branch-likely bugove.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1096 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test_fn.c
+++ src/test1/test_fn.c
@@ -11,10 +11,49 @@ fn1_t fn1;
 int
 fn1(int a, uint32_t *p32)
 {
+
+#if 1
 	volatile uint32_t *p = p32;
 
-	/* Ovo radi krivo bez (utreniranog) branch predictora */
+	/* Ovo radi krivo iz SRAM-a bez (utreniranog) branch predictora */
+	/* fix: r1094 */
 	for (int i = 0; i < 3; i++)
 		*p += a + (*p >> 3);
+#endif
+
+#if 0
+	/* Ne cancelira se bnel delay slot */
+	/* fix: r1095 */
+	__asm __volatile (
+		".set noreorder;"
+		"li %0, 0;"
+		"bnel $0, $0, 1f;"
+		"li %0, 1;"
+		"addiu %0, %0, 0x10;"
+		"1:;"
+		"addiu %0, %0, 0x100;"
+		".set reorder;"
+		: "=r" (a)
+		: "r" (a), "r" (p32)
+        );
+#endif
+
+#if 0
+	/* Ne cancelira se instrukcija iza beql delay slota */
+	/* fix: r1095 */
+	__asm __volatile (
+		".set noreorder;"
+		"li %0, 0;"
+		"beql $0, $0, 1f;"
+		"li %0, 1;"
+		"addiu %0, %0, 0x10;"
+		"1:;"
+		"addiu %0, %0, 0x100;"
+		".set reorder;"
+		: "=r" (a)
+		: "r" (a), "r" (p32)
+        );
+#endif
+
 	return (a);
 }
