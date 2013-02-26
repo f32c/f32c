cf99a78143352c3ac2d7907255319189f23f0364
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-02-26 11:56:53 +0000

    Pojednostavljeni Hello, world! program koji usput ispisuje i frekvenciju takta procitanu iz posebnog procesorskog read-only registra.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1142 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test1.c
+++ src/test1/test1.c
@@ -1,104 +1,27 @@
 
 #include <sys/param.h>
 #include <io.h>
-#include <stdlib.h>
 #include <stdio.h>
 
+#include <mips/asm.h>
 
-typedef int fn1_t(int, uint32_t *);
-fn1_t fn1;
-
-void
+int
 main(void)
 {
-	uint8_t *p8;
-	uint16_t *p16;
-	uint32_t *p32, *fn1_rom;
-	fn1_t *fn1_ram;
-	int i;
+	uint32_t tmp, freq_khz;
 
-	printf("\nuint8_t *:  ");
-	p8 = (void *) 0x80000000;
-	for (i = 0; i < 32; i++, p8 += 333)
-		*p8 = i;
-	p8 = (void *) 0x80000000;
-	for (i = 0; i < 32; i++, p8 += 333)
-		printf("%d ", *p8);
-	printf("\n");
+	printf("Hello, MIPS world!\n\n");
 
-	printf("uint16_t *: ");
-	p16 = (void *) 0x80000000;
-	for (i = 0; i < 32; i++, p16 += 333)
-		*p16 = i;
-	p16 = (void *) 0x80000000;
-	for (i = 0; i < 32; i++, p16 += 333)
-		printf("%d ", *p16);
-	printf("\n");
+	mfc0_macro(tmp, MIPS_COP_0_CONFIG);
+	freq_khz = ((tmp >> 16) & 0xfff) * 1000 / ((tmp >> 29) + 1);
 
-	printf("uint32_t *: ");
-	p32 = (void *) 0x80000000;
-	for (i = 0; i < 32; i++, p32 += 333)
-		*p32 = i;
-	p32 = (void *) 0x80000000;
-	for (i = 0; i < 32; i++, p32 += 333)
-		printf("%d ", *p32);
-	printf("\n");
-
-	/* Pobrisi SRAM */
-	printf("\nPattern fill:");
-	for (int j = 255; j >= 0; j -= 59) {
-		uint32_t pat;
-
-		printf(" %d", j);
-		pat = j + (j << 8);
-		pat += pat << 16;
-		p32 = (void *) 0x80000000;
-		for (i = 0; i < 1024 * 1024 / 4;
-		    i++, pat += i + (i << 16), p32++)
-			*p32 = pat;
-		pat = j + (j << 8);
-		pat += pat << 16;
-		p32 = (void *) 0x80000000;
-		for (i = 0; i < 1024 * 1024 / 4;
-		    i++, pat += i + (i << 16), p32++)
-			if (*p32 != pat)
-				printf("greska %d %08x %08x\n", i, *p32, pat);
-	}
-	printf("\n");
-
-	/* Kopiraj fn1() iz BRAM u SRAM */
-	fn1_ram = (void *) 0x80001000;
-	fn1_rom = (void *) &fn1;
-	p32 = (void *) fn1_ram;
-	for (i = 0; i < 512; i++)
-		*p32++ = *fn1_rom++;
-
-	/* Verificiraj kopiju! */
-	fn1_rom = (void *) &fn1;
-	p32 = (void *) fn1_ram;
-	for (i = 0; i < 512; i++, p32++, fn1_rom++)
-		if (*p32 != *fn1_rom)
-			printf("greska %d %08x %08x\n", i, *p32, *fn1_rom);
-
-	/* Izvedi fn1() u BRAM */
-	//p32 = (void *) 0x80000000;
-	p32 = (void *) 0x00003000;
-	*p32 = 0x101;
-	printf("BRAM: *p32 = %08x\n", *p32);
-	i = fn1(1, p32);
-	printf("i = %08x *p32 = %08x\n", i, *p32);
-
-	/* Izvedi fn1() u SRAM */
-	//p32 = (void *) 0x80000000;
-	p32 = (void *) 0x00003000;
-	*p32 = 0x101;
-	printf("SRAM: *p32 = %08x\n", *p32);
-#if 0
-	printf("Prebaci sw(2) i sw(3) u '1' (tocno tim redom)!\n");
-	do {
-		INB(i, IO_PUSHBTN + 1);
-	} while ((i & 0xc) != 0xc);
+	printf("f32c @ %d.%03d MHz, code running from ",
+	    freq_khz / 1000, freq_khz % 1000);
+#ifdef BRAM
+	printf("FPGA block RAM.\n");
+#else
+	printf("external static RAM.\n");
 #endif
-	i = fn1_ram(1, p32);
-	printf("i = %08x *p32 = %08x\n", i, *p32);
+
+	return (0);
 }
