0d8e98efddafd0e5adf940073df01a28fa3260fb
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-03-31 22:02:37 +0000

    Rekurzivno racunanje Fibonaccijevih brojeva je tocno 16 puta sporije iz vanjskog SRAMa nego iz FPGA block-RAMa...
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1149 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test1.c
+++ src/test1/test1.c
@@ -5,10 +5,26 @@
 
 #include <mips/asm.h>
 
+
+int fib(int);
+
+
+int
+fib(int n)
+{
+
+	if (n < 2)
+		return (n);
+	else
+		return (fib(n-1) + fib(n-2));
+} 
+
+
 int
 main(void)
 {
 	uint32_t tmp, freq_khz;
+	uint32_t start, end;
 
 	printf("Hello, MIPS world!\n\n");
 
@@ -18,10 +34,16 @@ main(void)
 	printf("f32c @ %d.%03d MHz, code running from ",
 	    freq_khz / 1000, freq_khz % 1000);
 #ifdef BRAM
-	printf("FPGA block RAM.\n");
+	printf("FPGA block RAM.\n\n");
 #else
-	printf("external static RAM.\n");
+	printf("external static RAM.\n\n");
 #endif
+
+	RDTSC(start);
+	for (tmp = 0; tmp <= 30; tmp++)
+		printf("fib(%d) = %d\n", tmp, fib(tmp));
+	RDTSC(end);
+	printf("\nCompleted in %d ms\n", (end - start) / freq_khz);
 
 	return (0);
 }
