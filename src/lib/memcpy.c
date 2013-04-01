4f65d5bc1517746937bd94b46672673c141e7e88
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-04-01 10:41:36 +0000

    Dodane najjednostavnije moguce implementacije funkcija memcpy() i memset().
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1150 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/lib/memcpy.c
@@ -0,0 +1,12 @@
+
+__attribute__((optimize("-Os"))) void *
+memcpy(int *dst, const int *src, unsigned int len)
+{
+	char *from = (char *) src;
+	char *to = (char *) dst;
+	
+	for (; len > 0; len--)
+		*to++ = *from++;
+
+	return (dst);
+}
