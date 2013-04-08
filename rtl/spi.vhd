c2c1b80fab22303e6fd7f826503b99ad1d1ea838
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-04-08 10:43:39 +0000

    Dodan "turbo" nacin rada za SPI sucelja, pri cemu vise nema djeljitelja takta, nego se glavni takt koristi direktno kao SPI takt.  Izgleda da to radi OK na 81.25 MHz i za on-board Flash i za MicroSD kartice, makar je ta frekvencija takta malo iznad deklarirane granice za Flash (80 MHz) i debelo iznad granice za MicroSD (tipicno 25 do 50 MHz).
    Najveca brzina u PIO nacinu rada je sad 3 MBytes / s pri izvodjenju iz
    BRAM-a, i otprilike 10 puta sporije pri izvodjenju iz SRAM-a.  Uz DMA
    brzina prijenosa bi trebala biti 10 MBytes / s.
    
    
    
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1187 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/spi.vhd
+++ rtl/spi.vhd
@@ -6,7 +6,8 @@ use IEEE.numeric_std.ALL;
 
 entity spi is
     generic (
-	C_fixed_speed: boolean := true
+	C_fixed_speed: boolean := true;
+	C_turbo_mode: boolean := true
     );
     port (
 	ce, clk: in std_logic;
@@ -42,16 +43,18 @@ begin
     bus_out(7 downto 0) <= R_spi_byte;
 
     spi_cen <= R_cen;
-    spi_sck <= R_clk_acc(7);
+    spi_sck <= not clk and not R_bit_cnt(3) when C_turbo_mode else R_clk_acc(7);
     spi_si <= R_spi_byte(7);
 
     process(clk)
 	variable clk_acc_next: std_logic_vector(7 downto 0);
     begin
-	if C_fixed_speed then
-	    clk_acc_next := R_clk_acc xor x"80";
-	else
-	    clk_acc_next := R_clk_acc + R_clk_div;
+	if not C_turbo_mode then
+	    if C_fixed_speed then
+		clk_acc_next := R_clk_acc xor x"80";
+	    else
+		clk_acc_next := R_clk_acc + R_clk_div;
+	    end if;
 	end if;
 
 	if rising_edge(clk) then
@@ -73,10 +76,15 @@ begin
 
 	    -- tx / rx logic
 	    if R_bit_cnt(3) = '0' then
-		-- sample input on falling clock edge
-		if clk_acc_next(7) = '0' and R_clk_acc(7) = '1' then
+		if C_turbo_mode then
 		    R_spi_byte <= R_spi_byte(6 downto 0) & spi_so;
 		    R_bit_cnt <= R_bit_cnt + 1;
+		else
+		    -- sample input on falling clock edge
+		    if clk_acc_next(7) = '0' and R_clk_acc(7) = '1' then
+			R_spi_byte <= R_spi_byte(6 downto 0) & spi_so;
+			R_bit_cnt <= R_bit_cnt + 1;
+		    end if;
 		end if;
 		R_clk_acc <= clk_acc_next;
 	    end if;
