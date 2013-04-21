33bbe1b8087f345c6ba0864a6a4a46846a9aad2d
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-04-21 21:29:24 +0000

    Prvi koraci za dodavanje instrukcijskog cachea.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1223 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ rtl/lattice/bram_dp_x9.vhd
@@ -0,0 +1,88 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+library xp2;
+use xp2.components.all;
+
+
+entity bram_dp_x9 is
+    port (
+	clk_a, clk_b: in std_logic; 
+	ce_a, ce_b: in std_logic;
+	we_a, we_b: in std_logic; 
+	res: in std_logic; 
+	addr_a, addr_b: in std_logic_vector(10 downto 0); 
+	data_in_a, data_in_b: in std_logic_vector(8 downto 0); 
+	data_out_a, data_out_b: out std_logic_vector(8 downto 0)
+    );
+end bram_dp_x9;
+
+architecture Structure of bram_dp_x9 is
+begin
+    bram_16_0: DP16KB
+        generic map (
+	    CSDECODE_A => "000", CSDECODE_B =>"000",
+	    WRITEMODE_A => "WRITETHROUGH", WRITEMODE_B => "WRITETHROUGH",
+	    GSR => "DISABLED", RESETMODE => "SYNC", 
+	    REGMODE_A => "NOREG", REGMODE_B=> "NOREG",
+	    DATA_WIDTH_A => 9, DATA_WIDTH_B => 9
+	)
+        port map (
+	    DIA0 => data_in_a(0), DIA1 => data_in_a(1),
+	    DIA2 => data_in_a(2), DIA3 => data_in_a(3),
+	    DIA4 => data_in_a(4), DIA5 => data_in_a(5),
+	    DIA6 => data_in_a(6), DIA7 => data_in_a(7),
+	    DIA8 => data_in_a(8), DIA9 => '0',
+	    DIA10 => '0', DIA11 => '0',
+	    DIA12 => '0', DIA13 => '0',
+	    DIA14 => '0', DIA15 => '0',
+	    DIA16 => '0', DIA17 => '0',
+	    DIB0 => data_in_b(0), DIB1 => data_in_b(1),
+	    DIB2 => data_in_b(2), DIB3 => data_in_b(3),
+	    DIB4 => data_in_b(4), DIB5 => data_in_b(5),
+	    DIB6 => data_in_b(6), DIB7 => data_in_b(7),
+	    DIB8 => data_in_b(8), DIB9 => '0',
+	    DIB10 => '0', DIB11 => '0',
+	    DIB12 => '0', DIB13 => '0',
+	    DIB14 => '0', DIB15 => '0',
+	    DIB16 => '0', DIB17 => '0',
+
+            DOA0 => data_out_a(0), DOA1 => data_out_a(1), 
+            DOA2 => data_out_a(2), DOA3 => data_out_a(3), 
+            DOA4 => data_out_a(4), DOA5 => data_out_a(5), 
+            DOA6 => data_out_a(6), DOA7 => data_out_a(7), 
+            DOA8 => data_out_a(8), DOA9 => open, 
+            DOA10 => open, DOA11 => open, 
+            DOA12 => open, DOA13 => open, 
+            DOA14 => open, DOA15 => open, 
+            DOA16 => open, DOA17 => open, 
+            DOB0 => data_out_b(0), DOB1 => data_out_b(1), 
+            DOB2 => data_out_b(2), DOB3 => data_out_b(3), 
+            DOB4 => data_out_b(4), DOB5 => data_out_b(5), 
+            DOB6 => data_out_b(6), DOB7 => data_out_b(7), 
+            DOB8 => data_out_b(8), DOB9 => open, 
+            DOB10 => open, DOB11 => open, 
+            DOB12 => open, DOB13 => open, 
+            DOB14 => open, DOB15 => open, 
+            DOB16 => open, DOB17 => open, 
+
+            ADA0 => '0', ADA1 => '0', ADA2 => '0',
+	    ADA3 => addr_a(0), ADA4 => addr_a(1),
+	    ADA5 => addr_a(2), ADA6 => addr_a(3),
+	    ADA7 => addr_a(4), ADA8 => addr_a(5),
+	    ADA9 => addr_a(6), ADA10 => addr_a(7),
+	    ADA11 => addr_a(8), ADA12 => addr_a(9),
+	    ADA13 => addr_a(10),
+            ADB0 => '0', ADB1 => '0', ADB2 => '0',
+	    ADB3 => addr_b(0), ADB4 => addr_b(1),
+	    ADB5 => addr_b(2), ADB6 => addr_b(3),
+	    ADB7 => addr_b(4), ADB8 => addr_b(5),
+	    ADB9 => addr_b(6), ADB10 => addr_b(7),
+	    ADB11 => addr_b(8), ADB12 => addr_b(9),
+	    ADB13 => addr_b(10),
+
+	    CEA => ce_a, CLKA => clk_a, WEA => we_a, RSTA => res, 
+	    CSA0 => '0', CSA1 => '0', CSA2 => '0',
+	    CEB => ce_b, CLKB => clk_b, WEB => we_b, RSTB => res, 
+	    CSB0 => '0', CSB1 => '0', CSB2 => '0'
+	);
+end Structure;
---
+++ rtl/lattice/bram_sp_x36.vhd
@@ -0,0 +1,87 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+library xp2;
+use xp2.components.all;
+
+
+entity bram_sp_x36 is
+    port (
+        clk: in std_logic; 
+	ce: in std_logic;
+        we: in std_logic; 
+        res: in std_logic; 
+        addr: in std_logic_vector(9 downto 0); 
+        data_in: in std_logic_vector(35 downto 0); 
+        data_out: out std_logic_vector(35 downto 0)
+    );
+end bram_sp_x36;
+
+architecture Structure of bram_sp_x36 is
+begin
+    bram_16_0: DP16KB
+	generic map (
+	    CSDECODE_A => "000", CSDECODE_B => "000",
+	    WRITEMODE_A => "WRITETHROUGH", WRITEMODE_B => "WRITETHROUGH",
+	    GSR => "DISABLED", RESETMODE => "SYNC", 
+	    REGMODE_A => "NOREG", REGMODE_B => "NOREG",
+	    DATA_WIDTH_A => 18, DATA_WIDTH_B => 18
+	)
+        port map (
+	    DIA0 => data_in(0), DIA1 => data_in(1),
+	    DIA2 => data_in(2), DIA3 => data_in(3),
+	    DIA4 => data_in(4), DIA5 => data_in(5),
+	    DIA6 => data_in(6), DIA7 => data_in(7),
+	    DIA8 => data_in(8), DIA9 => data_in(9),
+	    DIA10 => data_in(10), DIA11 => data_in(11),
+	    DIA12 => data_in(12), DIA13 => data_in(13),
+	    DIA14 => data_in(14), DIA15 => data_in(15),
+	    DIA16 => data_in(16), DIA17 => data_in(17),
+	    DIB0 => data_in(18), DIB1 => data_in(19),
+	    DIB2 => data_in(20), DIB3 => data_in(21),
+	    DIB4 => data_in(22), DIB5 => data_in(23),
+	    DIB6 => data_in(24), DIB7 => data_in(25),
+	    DIB8 => data_in(26), DIB9 => data_in(27),
+	    DIB10 => data_in(28), DIB11 => data_in(29),
+	    DIB12 => data_in(30), DIB13 => data_in(31),
+	    DIB14 => data_in(32), DIB15 => data_in(33),
+	    DIB16 => data_in(34), DIB17 => data_in(35),
+
+	    DOA0 => data_out(0), DOA1 => data_out(1), 
+	    DOA2 => data_out(2), DOA3 => data_out(3), 
+	    DOA4 => data_out(4), DOA5 => data_out(5), 
+	    DOA6 => data_out(6), DOA7 => data_out(7), 
+	    DOA8 => data_out(8), DOA9 => data_out(9), 
+	    DOA10 => data_out(10), DOA11 => data_out(11), 
+	    DOA12 => data_out(12), DOA13 => data_out(13), 
+	    DOA14 => data_out(14), DOA15 => data_out(15), 
+	    DOA16 => data_out(16), DOA17 => data_out(17), 
+	    DOB0 => data_out(18), DOB1 => data_out(19), 
+	    DOB2 => data_out(20), DOB3 => data_out(21), 
+	    DOB4 => data_out(22), DOB5 => data_out(23), 
+	    DOB6 => data_out(24), DOB7 => data_out(25), 
+	    DOB8 => data_out(26), DOB9 => data_out(27), 
+	    DOB10 => data_out(28), DOB11 => data_out(29), 
+	    DOB12 => data_out(30), DOB13 => data_out(31), 
+	    DOB14 => data_out(32), DOB15 => data_out(33), 
+	    DOB16 => data_out(34), DOB17 => data_out(35), 
+
+	    ADA0 => '1', ADA1 => '1', ADA2 => '0', ADA3 => '0',
+	    ADA4 => addr(0), ADA5 => addr(1),
+	    ADA6 => addr(2), ADA7 => addr(3),
+	    ADA8 => addr(4), ADA9 => addr(5),
+	    ADA10 => addr(6), ADA11 => addr(7),
+	    ADA12 => addr(8), ADA13 => '0', 
+	    ADB0 => '1', ADB1 => '1', ADB2 => '0', ADB3 => '0',
+	    ADB4 => addr(0), ADB5 => addr(1),
+	    ADB6 => addr(2), ADB7 => addr(3),
+	    ADB8 => addr(4), ADB9 => addr(5),
+	    ADB10 => addr(6), ADB11 => addr(7),
+	    ADB12 => addr(8), ADB13 => '1', 
+
+            CEA => ce, CLKA => clk, WEA => we, RSTA => res, 
+	    CSA0 => '0', CSA1 => '0', CSA2 => '0',
+            CEB => ce, CLKB => clk, WEB => we, RSTB => res, 
+	    CSB0 => '0', CSB1 => '0', CSB2 => '0'
+	);
+end Structure;
