7aaf2d49954fdcc255674bd2d247fce183b406b5
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-08-09 20:50:46 +0000

    Brdo headera preuzetih iz FreeBSD-a.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1375 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -3,8 +3,22 @@
 #define	_STDLIB_H_
 
 
+double strtod(const char * __restrict, char ** __restrict);
+long strtol(const char * __restrict, char ** __restrict, int);
+unsigned long strtoul(const char * __restrict, char ** __restrict, int);
+
+#define rand() random()
 uint32_t random(void);
+void srand(unsigned);
+
 int atoi(const char *);
+
+void *malloc(size_t size);
+void *realloc(void *ptr, size_t size);
+void free(void *ptr);
+
+#define RAND_MAX        0x7fffffff
+
 
 #define	exit(x)								\
 	do {								\
---
+++ src/include/strings.h
@@ -0,0 +1,8 @@
+
+#ifndef _STRINGS_H_
+#define	_STRINGS_H_
+
+int	 strcasecmp(const char *, const char *) __pure;
+int	 strncasecmp(const char *, const char *, size_t) __pure;
+
+#endif /* _STRINGS_H_ */
