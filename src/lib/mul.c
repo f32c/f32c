99a92743c50980985dd847c6a77acefe92adc1ab
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-08-13 20:54:47 +0000

    Dodana rutina za cjelobrojno 64-bitno mnozenje.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1407 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/mul.c
+++ src/lib/mul.c
@@ -12,3 +12,14 @@ __mulsi3(uint32_t a, uint32_t b)
 	return (res);
 }
  
+
+uint64_t
+__muldi3(uint64_t a, uint64_t b)
+{
+	uint64_t res;
+
+	for (res = 0; b != 0; b >>= 1, a <<= 1)
+		if (b & 1)
+			res += a;
+	return (res);
+}
