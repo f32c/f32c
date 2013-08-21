5a6810c4362dbc456c47b4bd3e08752c4680b0a5
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-08-21 16:18:28 +0000

    Dodan Tiny JPEG decoder modul (library), kojeg za sad jos nis ne koristi.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1447 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/include/fatfs/integer.h
+++ src/include/fatfs/integer.h
@@ -13,22 +13,22 @@
 #else			/* Embedded platform */
 
 /* These types must be 16-bit, 32-bit or larger integer */
-typedef int				INT;
+typedef int		INT;
 typedef unsigned int	UINT;
 
 /* These types must be 8-bit integer */
-typedef char			CHAR;
+typedef char		CHAR;
 typedef unsigned char	UCHAR;
 typedef unsigned char	BYTE;
 
 /* These types must be 16-bit integer */
-typedef short			SHORT;
+typedef short		SHORT;
 typedef unsigned short	USHORT;
 typedef unsigned short	WORD;
 typedef unsigned short	WCHAR;
 
 /* These types must be 32-bit integer */
-typedef long			LONG;
+typedef long		LONG;
 typedef unsigned long	ULONG;
 typedef unsigned long	DWORD;
 
