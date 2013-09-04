3f3aaa04d4d265446c303045fefdea11b7726202
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-09-04 11:55:48 +0000

    Preslozen FAT bootloader.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1477 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/boot/fat/loader.c
+++ src/boot/fat/loader.c
@@ -7,13 +7,16 @@
 
 
 static const char *bootfiles[] = {
-	"/basic.bin",
-	"1:/basic.bin",
+	"1:/boot/bootme.bin",
+	"/boot/kernel",
+	"/boot/basic.bin",
 	NULL
 };
 
 
-#define	LOADADDR 0x80000000
+#define LOAD_COOKIE	0x10adc0de
+
+#define	LOADADDR	0x80000000
 
 
 void
@@ -22,7 +25,7 @@ main(void)
 	int i, fd = -1;
 	char *cp = (char *) LOADADDR;
 
-	printf("FAT bootloader v 0.1 "
+	printf("ULX2S FAT bootloader v 0.1 "
 #if _BYTE_ORDER == _BIG_ENDIAN
 	    "(f32c/be)"
 #else
@@ -30,8 +33,15 @@ main(void)
 #endif
 	    "\n");
 
-	for (i = 0; bootfiles[i] != NULL; i++) {
-		printf("Trying %s ... ", bootfiles[i]);
+	if (*((int *) cp) == LOAD_COOKIE) {
+		printf("Trying %s... ", &cp[4]);
+		fd = open(&cp[4], O_RDONLY);
+		if (fd < 0)
+			printf("not found\n");
+	}
+
+	for (i = 0; fd < 0 && bootfiles[i] != NULL; i++) {
+		printf("Trying %s... ", bootfiles[i]);
 		fd = open(bootfiles[i], O_RDONLY);
 		if (fd > 0)
 			break;
@@ -48,14 +58,25 @@ main(void)
 	} while (i > 0);
 	printf("OK, loaded %d bytes\n", (int) (cp - LOADADDR));
 
+	printf("Invalidating I-cache...\n");
+	cp = (char *) LOADADDR;
+	for (i = 0; i < 8192; i += 4, cp += 4) {
+		__asm __volatile__(
+			"cache	0, 0(%0)"
+			: 
+			: "r" (cp)
+		);
+	}
+
 	cp = (char *) LOADADDR;
 	printf("Starting at %p\n", cp);
 
 	__asm __volatile__(
 		".set noreorder;"
-		"lui $4, 0x8000;"       /* stack mask */
-		"lui $5, 0x0010;"       /* top of the initial stack */
-                "and $29, %0, $4;"      /* clear low bits of the stack */
+		"lui $4, 0x8000;"	/* stack mask */
+		"lui $5, 0x0010;"	/* top of the initial stack */
+                "and $29, %0, $4;"	/* clear low bits of the stack */
+                "move $31, $0;"		/* return to ROM loader when done */
 		"jr %0;"
 		"or $29, $29, $5;"      /* set the stack pointer */
 		".set reorder;"
