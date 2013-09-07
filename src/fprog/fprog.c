f08fc3e2b31d5c9bb4693af7cfe672c30a525d44
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-09-07 13:22:49 +0000

    fprog preslozen tak da koristi rutine za pisanje po SPI flash-u iz vec gotove biblioteke.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1496 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/fprog/fprog.c
+++ src/fprog/fprog.c
@@ -7,95 +7,48 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <fatfs/ff.h>
+#include <fatfs/diskio.h>
 
-#define	PAGE_SIZE	4096	/* SPI flash minimum unit of work */
-#define	SECTOR_SIZE	512	/* buffer size */
+
+#define	SECTOR_BURST	32
+#define	BLOCK_SIZE	4096 * SECTOR_BURST	/* buffer size */
 
 
-char buf[SECTOR_SIZE];
+char *buf = (char *) 0x80080000;
 
 
 //#define IMAGE_NAME	"1:ulx2s_flash1.img"
 #define IMAGE_NAME	"1:fat12_4m.img"
 
 
-static void
-spi_wait()
-{
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x05); /* RDSR */
-	do {} while (spi_byte(SPI_PORT_FLASH, 0x05) & 1);
-}
-
-
 void
 main(void)
 {
-	int fd, i, j, k, got;
+	int fd, i, got;
 
 	if ((fd = open(IMAGE_NAME, 0)) < 0) {
 		printf("Nije pronadjena datoteka %s!\n", IMAGE_NAME);
 		return;
 	}
 
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
-	spi_byte(SPI_PORT_FLASH, 0); /* Clear write-protect bits */
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x60); /* Chip erase */
-	spi_wait();
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
-
-	for (i = 0; i < 1024; i++) {
+	for (i = 0; i < 1024; i += SECTOR_BURST) {
 		printf("%d ", i);
 		OUTB(IO_LED, i >> 2);
 
-		for (j = 0; j < PAGE_SIZE / SECTOR_SIZE; j++) {
-			if ((got = read(fd, buf, SECTOR_SIZE)) < 0) {
-				printf("\nread() failed!\n");
-				goto done;
-			}
-			if (got != SECTOR_SIZE) {
-				printf("\nread(): short read!\n");
-				goto done;
-			}
-
-			for (k = 0; k < SECTOR_SIZE; k += 2) {
-				spi_wait();
-				spi_start_transaction(SPI_PORT_FLASH);
-				spi_byte(SPI_PORT_FLASH, 0xad); /* AAI write */
-				if (i == 0 && j == 0 && k == 0) {
-					spi_byte(SPI_PORT_FLASH, 0);
-					spi_byte(SPI_PORT_FLASH, 0);
-					spi_byte(SPI_PORT_FLASH, 0);
-				}
-				spi_byte(SPI_PORT_FLASH, buf[k]);
-				spi_byte(SPI_PORT_FLASH, buf[k + 1]);
-			}
+		if ((got = read(fd, buf, BLOCK_SIZE)) < 0) {
+			printf("\nread() failed!\n");
+			goto done;
 		}
+
+		if (got != BLOCK_SIZE) {
+			printf("\nread(): short read!\n");
+			goto done;
+		}
+
+		disk_write(0, (BYTE *) buf, i, SECTOR_BURST);
 	}
 
 done:
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x04); /* WRDI */
-	spi_wait();
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
-
-	spi_start_transaction(SPI_PORT_FLASH);
-	spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
-	spi_byte(SPI_PORT_FLASH, 0x1c);	/* Set write-protect bits */
-
 	printf("\nGotovo!\n");
 }
