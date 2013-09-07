455e3bc7ab99172ced160882f0de9fa9d757fe92
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-09-07 22:35:49 +0000

    Opet preslozen fprog.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1500 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/fprog/fprog.c
+++ src/fprog/fprog.c
@@ -11,28 +11,48 @@
 #include <fatfs/diskio.h>
 
 
-#define	SECTOR_BURST	32
+#define	SECTOR_BURST	16
 #define	BLOCK_SIZE	4096 * SECTOR_BURST	/* buffer size */
 
 
 char *buf = (char *) 0x80080000;
 
 
-#define IMAGE_NAME	"1:ulx2s_spi_4m.img"
+#define IMAGE_NAME	"1:ulx2s_4m.img"
 
 
 void
 main(void)
 {
 	int fd, i, got;
+	int j, sum;
 
 	if ((fd = open(IMAGE_NAME, 0)) < 0) {
 		printf("Nije pronadjena datoteka %s!\n", IMAGE_NAME);
 		return;
 	}
 
+	printf("Brisem SPI flash...\n");
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x50); /* EWSR */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x01); /* WRSR */
+	spi_byte(SPI_PORT_FLASH, 0); /* Clear write-protect bits */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x06); /* WREN */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x60); /* Chip erase */
+
+	spi_start_transaction(SPI_PORT_FLASH);
+	spi_byte(SPI_PORT_FLASH, 0x05); /* RDSR */
+	do {} while (spi_byte(SPI_PORT_FLASH, 0x05) & 1);
+
+	printf("Pisem sektore...\n");
 	for (i = 0; i < 1024; i += SECTOR_BURST) {
-		printf("%d ", i);
+		printf("\r%d", i);
 		OUTB(IO_LED, i >> 2);
 
 		if ((got = read(fd, buf, BLOCK_SIZE)) < 0) {
@@ -45,9 +65,16 @@ main(void)
 			goto done;
 		}
 
+		/* Preskoci prazne sektore */
+		for (sum = -1, j = 0; j < BLOCK_SIZE; j += 4)
+			sum &= *((int *) &buf[j]);
+		if (sum == -1)
+			continue;
+
 		disk_write(0, (BYTE *) buf, i, SECTOR_BURST);
 	}
+	printf("\r%d", i);
 
 done:
-	printf("\nGotovo!\n");
+	printf("\nGotovo!\n\n");
 }
