aabe61e298c3ce447d1f3a81a967d435aaf43673
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-09-25 12:35:40 +0000

    Dodana naredba SLEEP.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1562 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/pbasic/bas_f32c.c
+++ src/pbasic/bas_f32c.c
@@ -24,3 +24,29 @@ bauds(void)
         normret;
 }
 
+
+int
+bas_sleep(void)
+{
+	uint64_t start, end, now;
+	int c;
+
+	start = tsc_hi;
+	start = (start << 32) + tsc_lo;
+
+	evalreal();
+	check();
+	if (res.f < ZERO)
+                error(33);      /* argument error */
+	end = start + (uint64_t) (res.f * 1000.0 * freq_khz);
+
+	do {
+		now = tsc_hi;
+		now = (now << 32) + tsc_lo;
+		c = sio_getchar(0);
+	} while (c != 3 && now <= end);
+
+	if (c == 3)
+		trapped = 1;
+        normret;
+}
