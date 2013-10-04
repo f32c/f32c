020a99911a65ca224c08c01710b8615bbc59ef0d
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-10-04 21:08:11 +0000

    Malo prociscene nepotrebne opcije kod pozivanja gcc-a, uz provjeru da je DMIPS/MHz kod izvodjenja iz BRAM-a jos uvije na razini 1.63.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1597 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/conf/post.mk
+++ src/conf/post.mk
@@ -50,7 +50,8 @@ WARNS = 2
 endif
 
 # Includes
-MK_INCLUDES = -nostdinc -I${BASE_DIR}include -I.
+#MK_INCLUDES = -nostdinc -I${BASE_DIR}include -I.
+MK_INCLUDES = -nostdinc -I${BASE_DIR}include
 
 # MIPS-specific flags
 MK_CFLAGS += -march=f32c
@@ -79,7 +80,7 @@ MK_CFLAGS += -Werror
 endif
 
 MK_CFLAGS += -ffreestanding
-MK_CFLAGS += -mno-shared
+#MK_CFLAGS += -mno-shared
 
 MK_CFLAGS += -Wextra -Wsystem-headers -Wshadow
 # Too strict:
@@ -92,19 +93,21 @@ MK_CFLAGS += -g
 # Optimization options
 MK_CFLAGS += -Os
 MK_CFLAGS += -fselective-scheduling
-MK_CFLAGS += -finline-limit=4 -fmerge-all-constants
-MK_CFLAGS += -falign-functions=4 -falign-labels=4
-MK_CFLAGS += -falign-jumps=4 -falign-loops=4
 MK_CFLAGS += -fpeel-loops
-MK_CFLAGS += -fgcse-sm
 
-# Those flags improved performance with gcc-4.6, but not with gcc-4.7
+# Those flags improve performance with gcc-4.6, but not with gcc-4.8
+#MK_CFLAGS += -fgcse-sm
 #MK_CFLAGS += -fgcse-las
 #MK_CFLAGS += -fsched2-use-superblocks
 #MK_CFLAGS += -fira-algorithm=priority
 
-# No zero-filled BSS
-MK_CFLAGS += -fno-zero-initialized-in-bss
+# Further constrain code size, apparently with no impact on performance?
+MK_CFLAGS += -finline-limit=4
+#MK_CFLAGS += -falign-functions=4 -falign-labels=4
+#MK_CFLAGS += -falign-jumps=4 -falign-loops=4
+
+# Reduce initialized data as much as possible
+MK_CFLAGS += -fmerge-all-constants
 
 # Pull in any module-specific compiler flags
 MK_CFLAGS += ${CFLAGS}
