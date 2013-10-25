bb79d548f04d9666fb71edc16bdeaf77757b718a
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-10-25 12:50:49 +0000

    Maknut TCL dependency iz make pravila, kojeg je prvi na svojoj kozi osjetio Miljenko...
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1647 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/tools/makesnap.sh
+++ src/tools/makesnap.sh
@@ -5,4 +5,10 @@ svn co svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c/src src
 rm -fr src/.svn
 rm -fr src/coremark src/demo src/tools src/test1 src/boot
 
+# Pipelinanje naredbi ne radi bas pouzdano pod cygwinom,
+# pa za sad kompletno eliminiramo skriptu isa_check.tcl
+mv src/conf/post.mk src/conf/post.mk.org
+fgrep -v ISA_CHECK src/conf/post.mk.org > src/conf/post.mk
+rm src/conf/post.mk.org
+
 tar -czvf f32c_src.tgz src
