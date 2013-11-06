4e4011d8de07dee2bdf039150b161afa5d7824e4
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-11-06 10:05:27 +0000

    wavplay sad zna reproducirati sve .wav datoteke s MicroSD kartice. S jednog na drugi zapis se moze prebacivati tipkama left i right.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1679 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/wavplay/wavplay.c
+++ src/wavplay/wavplay.c
@@ -8,44 +8,116 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <io.h>
+
+#include <fatfs/ff.h>
+
+
+#define	MAX_FNAMES	256
+
+char fnames[MAX_FNAMES][256];
+int fcnt;
+
+
+void
+scan_files(char* path)
+{
+	FRESULT res;
+	FILINFO fno;
+	DIR dir;
+	int i, t;
+
+	/* Open the directory */
+	res = f_opendir(&dir, path);
+	if (res != FR_OK)
+		return;
+
+	i = strlen(path);
+	do {
+		/* Read a directory item */
+		res = f_readdir(&dir, &fno);
+		if (res != FR_OK || fno.fname[0] == 0)
+			break;
+
+		/* Ignore dot entry */
+		if (fno.fname[0] == '.')
+			continue;
+
+		/* Recursively scan subdirectories */
+		path[i] = '/';
+		strcpy(&path[i+1], fno.fname);
+		if (fno.fattrib & AM_DIR)
+			scan_files(path);
+		else {
+			t = strlen(path);
+			if (t > 4 && strcmp(&path[t - 4], ".WAV") == 0)
+				strcpy(fnames[fcnt++], path);
+		}
+		path[i] = 0;
+	} while (1);
+}
+
 
 void
 main(void)
 {
-	int i, f, block = 0, cur, got, cnt = 0, vol = 12;
+	char tmpbuf[256];
+	int i, f, fpos = 0, block, cur, got, vol = 0xfff;
 	char *buf = (void *) 0x80080000;
 
-	printf("Hello, FPGA world!\n");
+	tmpbuf[0] = '1';
+	tmpbuf[1] = ':';
+	tmpbuf[2] = 0;
+	f = open(tmpbuf, O_RDONLY);
+	if (f > 0)
+		close(f);
+	scan_files(tmpbuf);
 
-	f = open("1:/route_66.wav", O_RDONLY);
+next_file:
+	f = open(fnames[fpos], O_RDONLY);
 	if (f < 0) {
 		printf("Open failed\n");
 		exit (1);
 	}
+	printf("Playing %s, vol %d\n", fnames[fpos], vol);
 
+	block = 0;
 	got = read(f, buf, 0x8000);
 	OUTW(IO_PCM_FIRST, buf);
 	OUTW(IO_PCM_LAST, buf + 0x7ffe);
 	OUTW(IO_PCM_FREQ, 9108); /* 44.1 kHz sample rate */
+	OUTW(IO_PCM_VOLUME, vol + (vol << 16));
 
 	while (got > 0) {
 		INW(cur, IO_PCM_CUR);
 		if ((cur & 0x4000) != block) {
 			got = read(f, buf + block, 0x4000);
 			block = cur & 0x4000;
-			cnt += got;
 			INB(i, IO_PUSHBTN);
-			if ((i & BTN_UP) && vol < 15)
-				vol++;
+			if ((i & BTN_UP) && vol < 0xffff)
+				vol = (vol << 1) + 1;
 			if ((i & BTN_DOWN) && vol > 0)
-				vol--;
-			OUTH(IO_PCM_VOLUME, vol + (vol << 8));
-			printf("%d	%d\n", vol, cnt);
+				vol = vol >> 1;
+			if ((i & (BTN_UP|BTN_DOWN)))
+				printf("Playing %s, vol %d\n",
+				    fnames[fpos], vol);
+			OUTW(IO_PCM_VOLUME, vol + (vol << 16));
+			if ((i & BTN_LEFT)) {
+				close(f);
+				if (fpos == 0)
+					fpos = fcnt;
+				fpos--;
+				goto next_file;
+			}
+			if ((i & BTN_RIGHT))
+				break;
 		}
 	}
-
-	OUTW(IO_PCM_FREQ, 0); /* O Hz sample rate - stop PCM DMA */
-	OUTH(IO_PCM_VOLUME, 0); /* volume 0 on both channels - shut up PCM */
+	close(f);
+	fpos++;
+	if (fpos == fcnt)
+		fpos = 0;
+	goto next_file;
 }
