b2a6f9faadbd579df730c60d63ab0bf8857f7809
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2013-11-25 12:54:03 +0000

    Jednostavniji malloc() / free(): brzi, ali podlozniji fragmentaciji.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@1690 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/malloc.c
+++ src/lib/malloc.c
@@ -27,151 +27,90 @@
 
 #include <sys/param.h>
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MALLOC_DIAGNOSTIC
 
-extern int _end;
+extern void *_end;
 
-static char	**descr_tbl;
-static size_t	descr_tbl_len;
-static size_t	descr_tbl_max_len;
+static uint32_t *heap;
 
-#define	GET_PTR(ptr)		((char *) (((size_t) (ptr)) & ~1))
-#define	GET_FREE(ptr)		(((size_t) (ptr)) & 1)
-#define	SET_FREE(ptr, use)	((char *) ((size_t) GET_PTR(ptr) | (use & 1)))
+#define	HEAP_TOP	0x800f8000
 
-#define	DESCR_INCR_STEP	16
-#define	INIT_DESCR_SZ	DESCR_INCR_STEP
+#define	GET_LEN(len)		((len) & ~0x80000000)
+#define	IS_FREE(len)		((len) & 0x80000000)
+#define	SET_FREE(len)		((len) | 0x80000000)
 
 
-static void *
-malloc_internal(size_t size)
-{
-	size_t i, len, best_len = 0;
-	int best = -1;
-
-	/* Align request size to word length */
-	size = (size + (sizeof(int) - 1)) & ~(sizeof(int) - 1);
-
-	/* Find the smallest free chunk */
-	for (i = 0; i < descr_tbl_len - 1; i++)
-		if (GET_FREE(descr_tbl[i])) {
-			len = GET_PTR(descr_tbl[i + 1]) - GET_PTR(descr_tbl[i]);
-			if (len >= size && (best < 0 || len < best_len)) {
-				best_len = len;
-				best = i;
-				if (best_len == size)
-					break;
-			}
-		}
-
-	/* Out of memory */
-	if (best < 0)
-		return (NULL);
-
-	if (best_len > size) {
-		/* Split the free chunk in two */
-		if (GET_FREE(descr_tbl[best + 1]) == 0) {
-			for (i = descr_tbl_len - 1; i > best; i--)
-				descr_tbl[i + 1] = descr_tbl[i];
-			descr_tbl_len++;
-		}
-		descr_tbl[best + 1] = descr_tbl[best] + size;
-	}
-	descr_tbl[best] = SET_FREE(descr_tbl[best], 0);
-
-	return (descr_tbl[best]);
-}
+/*
+ * Each chunk is preceeded by a 4-byte length & type descriptor.  Length
+ * value stored in the descriptor is a sum of the payload length (rounded
+ * to * multiplies of 4) and the size of the descriptor.  LS bit indicates
+ * chunk type, which can be either used (0) or free (1).
+ */
 
 
 void
 free(void *ptr)
 {
-	size_t i, j, step;
+	uint32_t i, len;
 
 	if (ptr == NULL)
 		return;
 
-	for (step = i = descr_tbl_len / 2;;) {
-		if (step > 1)
-			step /= 2;
-		if (ptr > (void *) descr_tbl[i])
-			i += step;
-		else if (SET_FREE(ptr, 1) < descr_tbl[i])
-			i -= step;
-		else if (descr_tbl[i] == ptr) {
-			descr_tbl[i] = SET_FREE(descr_tbl[i], 1);
-			/* Attempt to merge adjacent free chunks */
-			if (GET_FREE(descr_tbl[i + 1]))
-				descr_tbl[i + 1] =
-				    SET_FREE(descr_tbl[i + 2], 1);
-			while (i > 0 && GET_FREE(descr_tbl[i - 1])) {
-				descr_tbl[i] = SET_FREE(descr_tbl[i + 1], 1);
-				i--;
-			}
-			for (j = i; GET_FREE(descr_tbl[j + 1]); j++) {}
-#ifdef MALLOC_DIAGNOSTIC
-			if (j > i &&
-			    GET_PTR(descr_tbl[j]) != descr_tbl[j + 1]) {
-				printf("XXX panic %p %p\n",
-				    descr_tbl[j], descr_tbl[j + 1]);
-				exit(1);
-			}
-#endif
-			if (j > i) {
-				do {
-					descr_tbl[++i] = descr_tbl[++j];
-				} while (j < descr_tbl_len - 1);
-				descr_tbl_len -= (j - i);
-			}
-			return;
-		} else
-			i++;
-#ifdef MALLOC_DIAGNOSTIC
-		if (i < 0 || i >= descr_tbl_len) {
-			printf("XXX Can't free %p, i = %d\n", ptr, i);
-			for (i = 0; i < descr_tbl_len; i++)
-				printf("%p\n", descr_tbl[i]);
-			exit(1);
-		}
-#endif
+	i = ((uint32_t *) ptr) - heap - 1;
+	len = heap[i];
+	if (IS_FREE(len)) {
+		printf("free(%p): block already free!\n", ptr);
+		exit(1);
 	}
+
+	/* Attempt to merge with next free block */
+	while (IS_FREE(heap[i + len])) {
+		len += GET_LEN(heap[i + len]);
+	}
+	heap[i] = SET_FREE(len);
 }
 
 
 void *
 malloc(size_t size)
 {
-	void *new, *old;
+	uint32_t i;
+	int best_i, best_len;
 
 	if (size == 0)
 		return (NULL);
 
-	if (descr_tbl == NULL) {
+	size = ((size + 3) >> 2) + 1;
+
+	if (heap == NULL) {
 		/* init */
-		descr_tbl = (void *) &_end;
-		descr_tbl[0] = SET_FREE(descr_tbl, 0);
-		descr_tbl[1] = SET_FREE(&descr_tbl[INIT_DESCR_SZ], 1);
-		descr_tbl[2] = SET_FREE((char *) 0x800f8000, 0);
-		descr_tbl_len = 3;
-		descr_tbl_max_len = INIT_DESCR_SZ;
+		heap = (void *) &_end;
+		i = (HEAP_TOP - ((uint32_t) heap)) / sizeof(*heap) - 1;
+		heap[0] = SET_FREE(i);
+		heap[i] = 0;
 	}
 
-	/* Grow descr tbl if necessary */
-	if (descr_tbl_len + 2 >= descr_tbl_max_len) {
-		new = malloc_internal(sizeof(*descr_tbl) *
-		    (descr_tbl_max_len + DESCR_INCR_STEP));
-		if (new == NULL)
-			return (NULL);
-		old = descr_tbl;
-		descr_tbl = new;
-		memcpy(new, old, descr_tbl_len * sizeof(char *));
-		descr_tbl_max_len += DESCR_INCR_STEP;
-		free(old);
+	/* Find free block, linear search, slow. */
+	best_i = -1;
+	for (i = 0; GET_LEN(heap[i]) > 0; i += GET_LEN(heap[i])) {
+		if (IS_FREE(heap[i]) && GET_LEN(heap[i]) >= size &&
+		    (best_i < 0 || GET_LEN(heap[i]) <= best_len)) {
+			best_i = i;
+			best_len = GET_LEN(heap[i]);
+			if (best_len == size)
+				break;
+		}
 	}
 
-	return (malloc_internal(size));
+	/* Out of memory */
+	if (best_i < 0)
+		return (NULL);
+
+	heap[best_i] = size;
+	if (best_len > size)
+		heap[best_i + size] = SET_FREE(best_len - size);
+
+	return (&heap[best_i + 1]);
 }
