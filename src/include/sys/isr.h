7dcbfc0d509c2721761374aff9ecc2a66e4308bd
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2014-08-13 19:26:58 +0000

    Infrastruktura za registriranje i pozivanje prekidnih rutina u C-u.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2120 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/include/sys/isr.h
@@ -0,0 +1,45 @@
+/*-
+ * Copyright (c) 2014 Marko Zec, University of Zagreb
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#ifndef _SYS_ISR_H_
+#define	_SYS_ISR_H_
+
+#include <sys/queue.h>
+
+typedef void isr_handler_fn(void);
+
+struct isr_link {
+	SLIST_ENTRY(isr_link)	 isr_le;
+	isr_handler_fn		*handler_fn;
+};
+
+
+void isr_register_handler(int, struct isr_link *);
+void isr_enable(void);
+
+#endif /* !_SYS_ISR_H_ */
+
---
+++ src/lib/isr.c
@@ -0,0 +1,60 @@
+/*-
+ * Copyright (c) 2014 Marko Zec, University of Zagreb
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <sys/isr.h>
+
+#include <mips/asm.h>
+#include <mips/regdef.h>
+#include <mips/cpuregs.h>
+
+#include <stdio.h>
+
+
+static SLIST_HEAD(, isr_link) isr_registry[8];
+
+
+void
+isr_dispatch(int filtered_irqs)
+{
+	int irq;
+	struct isr_link *isrl;
+
+	for (irq = 0; filtered_irqs != 0; filtered_irqs >>= 1, irq++) {
+		if ((filtered_irqs & 1) == 0)
+			continue;
+		SLIST_FOREACH(isrl, &isr_registry[irq], isr_le)
+			isrl->handler_fn();
+	}
+}
+
+
+void
+isr_register_handler(int irq, struct isr_link *isr_entry)
+{
+	
+	SLIST_INSERT_HEAD(&isr_registry[irq], isr_entry, isr_le);
+}
