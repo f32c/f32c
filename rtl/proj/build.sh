0b53ccb4232e6e5b0822ce3814de997a02e4ae43
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2014-08-14 17:22:30 +0000

    Diamond 3.1 -> 3.2.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2131 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/proj/build.sh
+++ rtl/proj/build.sh
@@ -3,5 +3,5 @@
 export TEMP=/tmp
 export LSC_INI_PATH=""
 export LSC_DIAMOND=true
-export TCL_LIBRARY=/usr/local/diamond/3.1/tcltk/lib/tcl8.5
-/usr/local/diamond/3.1/bin/lin/diamondc build.tcl
+export TCL_LIBRARY=/usr/local/diamond/3.2/tcltk/lib/tcl8.5
+/usr/local/diamond/3.2/bin/lin/diamondc build.tcl
