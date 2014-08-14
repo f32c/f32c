93556e6535b11e322e381e90d32a93424f2deff8
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2014-08-14 08:55:27 +0000

    Prilagodba novoj shemi prihvata i obrade prekida.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2125 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/test1/test1.c
+++ src/test1/test1.c
@@ -37,16 +37,20 @@
 #define	RDEPC(var) mfc0_macro(var, MIPS_COP_0_EXC_PC);
 #define	RDEBASE(var) mfc0_macro(var, MIPS_COP_0_EBASE);
 
+
 int isr_cnt;
 
-int cnt = 0;
 
-
-static void
+static int
 test_isr(void)
 {
+	int i;
+
+	/* Clear the 50 Hz framebuffer interrupt */
+	INB(i, IO_FB);
 
 	isr_cnt++;
+	return (1);
 }
 
 
@@ -54,109 +58,20 @@ struct isr_link test_isr_reg = {
 	.handler_fn = &test_isr
 };
 
+
 void
 main(void)
 {
-	void *epc;
+	int i = 0;
 
 	isr_register_handler(2, &test_isr_reg);
-	isr_enable();
+	__asm("ei");
 
-	epc = NULL;
-	RDEPC(epc);
-	printf("EPC = %p\n", epc);
-	epc = NULL;
-	RDEBASE(epc);
-	printf("EBASE = %p\n", epc);
-
-#if 1
-	/* Testing interrupts */
-	int i = 0;
-	int c = -1;
 	volatile int *a = &isr_cnt;
 	do {
-		RDEPC(epc);
-		if (*a > c + 20000) {
-			c = *a;
-			printf(" %p %d %d", epc, c, i);
-			printf(" A %d", random());
-			printf(" B %d", random());
-			printf(" C %d", random());
-			printf(" D %d\n", random());
-		}
-		i++;
-		putchar(8);
-		if (i == 16)
-			i = 0;
-		putchar(8);
-		if (i < 10)
-			putchar('0' + i);
-		else
-			putchar('a' + i - 10);
+		if (i != *a)
+			printf("\r%d", *a);
+		i = *a;
+		__asm("wait");
 	} while (1);
-#endif
-
-	printf("Uncached instruction stream:\n");
-again:
-	__asm __volatile (
-		".set noreorder\n"
-		"li $27, 1\n"
-		"syscall\n"
-		"nop\n"
-		".set reorder"
-	);
-	RDEPC(epc);
-	printf("%d - sequential code: %p\n", isr_cnt, epc);
-
-	__asm __volatile ("syscall");
-	sio_getchar(0);
-	RDEPC(epc);
-	printf("%d - before jal: %p\n", isr_cnt, epc);
-
-	__asm __volatile (
-		".set noreorder\n"
-		"b 1f\n"
-		"syscall\n"
-		"nop\n"
-		"1:\n"
-		".set reorder"
-	);
-	RDEPC(epc);
-	printf("%d - taken branch delay slot: %p\n", isr_cnt, epc);
-
-	__asm __volatile (
-		".set noreorder\n"
-		"beql $0,$27,1f\n"
-		"syscall\n"
-		"1:\n"
-		".set reorder"
-	);
-	RDEPC(epc);
-	printf("%d " "- after delay / nullify slot of untaken branch likely"
-	    ": %p\n", isr_cnt, epc);
-
-	__asm __volatile (
-		".set noreorder\n"
-		"beq $0,$27,1f\n"
-		"syscall\n"
-		"1:\n"
-		".set reorder"
-	);
-	RDEPC(epc);
-	printf("%d - delay slot of untaken branch: %p\n", isr_cnt, epc);
-
-	printf("\n");
-	cnt++;
-	if (cnt == 2) {
-		printf("Clearing I-cache...\n");
-		__asm __volatile ("j _start");
-	}
-	if (cnt <= 3) {
-		printf("Cached instruction stream:\n");
-		goto again;
-	}
-	printf("Done, disabling interrupts...\n\n");
-	__asm __volatile ("di");
-
-	__asm __volatile  ("j $0");
 }
