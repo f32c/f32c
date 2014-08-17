4df1179a02bd0d37caa8875acc17bd613109daa3
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2014-08-17 21:09:20 +0000

    Pojednostavljen (polling) SIO driver.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2141 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/sio.c
+++ src/lib/sio.c
@@ -31,7 +31,6 @@
 #define	SIO_RXBUFSIZE	(1 << 3)
 #define	SIO_RXBUFMASK	(SIO_RXBUFSIZE - 1)
 
-void (*sio_idle_fn)(void) = NULL;
 static char sio_rxbuf[SIO_RXBUFSIZE];
 static uint8_t sio_rxbuf_head;
 static uint8_t sio_rxbuf_tail;
@@ -66,44 +65,33 @@ sio_probe_rx(void)
 __attribute__((optimize("-Os"))) int
 sio_getchar(int blocking)
 {
-	int c;
-	int busy;
+	int c, busy;
 
 	/* Any new characters received from RS-232? */
 	do {
-		if (sio_idle_fn != NULL)
-			(*sio_idle_fn)();
 		sio_probe_rx();
 		busy = (sio_rxbuf_head == sio_rxbuf_tail);
 	} while (blocking && busy);
 
 	if (busy)
 		return (-1);
-	else {
-		c = sio_rxbuf[sio_rxbuf_tail++];
-		sio_rxbuf_tail &= SIO_RXBUFMASK;
-		return (c);
-	}
+	c = sio_rxbuf[sio_rxbuf_tail++];
+	sio_rxbuf_tail &= SIO_RXBUFMASK;
+	return (c);
 }
 
 
 __attribute__((optimize("-Os"))) int
 sio_putchar(int c, int blocking)
 {
-	int in;
-	int busy;
+	int in, busy;
 
 	do {
-		if (sio_idle_fn != NULL)
-			(*sio_idle_fn)();
 		in = sio_probe_rx();
 		busy = (in & SIO_TX_BUSY) || sio_tx_xoff;
 	} while (blocking && busy);
 
-	if (busy)
-		return (-1);
-	else {
+	if (busy == 0)
 		OUTB(IO_SIO_BYTE, c);
-		return (0);
-	}
+	return (busy);
 }
