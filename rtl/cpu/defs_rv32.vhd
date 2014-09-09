3ad9eb5152a6e3d380fac0f9eb49611a73cd47c2
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2014-09-09 16:07:19 +0000

    RV32 defs, nekompletne, ne koriste se nigdje za sad.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2193 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ rtl/cpu/defs_rv32.vhd
@@ -0,0 +1,48 @@
+--
+-- Copyright 2014 Marko Zec, University of Zagreb.
+--
+-- Neither this file nor any parts of it may be used unless an explicit 
+-- permission is obtained from the author.  The file may not be copied,
+-- disseminated or further distributed in its entirety or in part under
+-- any circumstances.
+--
+
+-- $Id$
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package rv32_pack is
+
+-- RV32I Base Instruction Set Opcodes
+constant RV32I_OP_LUI:		std_logic_vector := "0110111";
+constant RV32I_OP_AUIPC:	std_logic_vector := "0010111";
+constant RV32I_OP_JAL:		std_logic_vector := "1101111";
+constant RV32I_OP_JALR:		std_logic_vector := "1100111";
+constant RV32I_OP_BRANCH:	std_logic_vector := "1100011";
+constant RV32I_OP_LOAD:		std_logic_vector := "0000011";
+constant RV32I_OP_STORE:	std_logic_vector := "0100011";
+constant RV32I_OP_REG_IMM:	std_logic_vector := "0010011";
+constant RV32I_OP_REG_REG:	std_logic_vector := "0110011";
+constant RV32I_OP_FENCE:	std_logic_vector := "0001111";
+constant RV32I_OP_SYS:		std_logic_vector := "1110011";
+
+-- RV64I Base Instruction Set Opcodes
+constant RV64I_OP_REG_IMM:	std_logic_vector := "0011011";
+constant RV64I_OP_REG_REG:	std_logic_vector := "0111011";
+
+-- Branch conditions
+constant RV32_COND_EQ:		std_logic_vector := "000";
+constant RV32_COND_NE:		std_logic_vector := "001";
+constant RV32_COND_LT:		std_logic_vector := "100";
+constant RV32_COND_GE:		std_logic_vector := "101";
+constant RV32_COND_LTU:		std_logic_vector := "110";
+constant RV32_COND_GEU:		std_logic_vector := "111";
+
+-- Memory access width
+constant RV32_MEM_SIZE_B:	std_logic_vector := "00";
+constant RV32_MEM_SIZE_H:	std_logic_vector := "01";
+constant RV32_MEM_SIZE_W:	std_logic_vector := "10";
+constant RV32_MEM_SIZE_D:	std_logic_vector := "11";
+
+end;
