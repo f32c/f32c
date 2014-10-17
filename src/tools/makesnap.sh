904b9008c1b439fccfa37d274c8c7c47a0461dfb
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2014-10-17 17:30:35 +0000

    Azurirana skripta za snapshotiranje f32c/src stabla i pripremu za metanje na javno dostupni web.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2268 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/tools/makesnap.sh
+++ src/tools/makesnap.sh
@@ -1,14 +1,15 @@
 #!/bin/csh
 
-svn co svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c/src src
+svn co svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c f32c
+svn co svn+ssh://login.nxlab.fer.hr/fpgasvn/demo/chess f32c/src/chess
 
 rm -fr src/.svn
 rm -fr src/tools src/misc
 
 # Pipelinanje naredbi ne radi bas pouzdano pod cygwinom,
 # pa za sad kompletno eliminiramo skriptu isa_check.tcl
-mv src/conf/post.mk src/conf/post.mk.org
-fgrep -v ISA_CHECK src/conf/post.mk.org > src/conf/post.mk
-rm src/conf/post.mk.org
+mv f32c/src/conf/post.mk f32c/src/conf/post.mk.org
+fgrep -v ISA_CHECK f32c/src/conf/post.mk.org > f32c/src/conf/post.mk
+rm f32c/src/conf/post.mk.org
 
-tar -czvf f32c_src.tgz src
+tar -czvf f32c_src.tgz f32c
