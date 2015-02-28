805ac7902252915d9c7bff3039dda45250680b50
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-02-28 14:53:43 +0000

    Konzistentnije copyright poruke.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2386 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lib/div.c
+++ src/lib/div.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2013 Marko Zec
+ * Copyright (c) 2013 Marko Zec, University of Zagreb
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
