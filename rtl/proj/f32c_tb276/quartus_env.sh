064ab8281ddc39f21cbc094c0d4ddccb95405c9b
davor <davor@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-03-27 01:27:16 +0000

    tb276 sad koristi generic glue i prebačen je u altera/tb276
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2662 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/proj/f32c_tb276/quartus_env.sh
+++
@@ -1,6 +0,0 @@
-ALTERAPATH=/usr/local/altera/13.0sp1
-
-if [ -d ${ALTERAPATH}/quartus/bin/ ] ; then
-  export PATH=${ALTERAPATH}/quartus/bin/:"${PATH}"
-  export QSYS_ROOTDIR="${ALTERAPATH}/quartus/sopc_builder/bin"
-fi
