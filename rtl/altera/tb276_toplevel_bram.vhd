1c26c2ff8047df0482e60795755ce20c9bd6c397
davor <davor@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-03-28 11:24:56 +0000

    zaboravio sam uploadat toplevel modul za tb276 altera
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2674 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ rtl/altera/tb276_toplevel_bram.vhd
@@ -0,0 +1,93 @@
+--
+-- Copyright 2015 Marko Zec, University of Zagreb
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+--
+
+-- $Id: nexys3_toplevel_bram.vhd 2636 2015-03-25 15:07:09Z marko $
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+use work.f32c_pack.all;
+
+entity glue is
+    generic (
+	-- Main clock: N * 10 MHz
+	C_clk_freq: integer := 112;
+
+	-- SoC configuration options
+	C_mem_size: integer := 8;
+	C_sio: boolean := true;
+	C_leds_btns: boolean := true
+    );
+    port (
+	clk_25m: in std_logic;
+	rs232_txd: out std_logic;
+	rs232_rxd: in std_logic;
+	led: out std_logic_vector(7 downto 0);
+	gpio: inout std_logic_vector(31 downto 0);
+	btn_left, btn_right: in std_logic
+	-- sw: in std_logic_vector(7 downto 0)
+    );
+end glue;
+
+architecture Behavioral of glue is
+    signal clk, rs232_break: std_logic;
+    signal btns: std_logic_vector(7 downto 0);
+begin
+    -- clock synthesizer: Altera specific
+    clkgen: entity work.pll_25M_112M5
+    --generic map(
+    --  C_clk_freq => C_clk_freq,
+    --  C_debug => false
+    --)
+    port map(
+      inclk0 => clk_25m, c0 => clk
+    );
+
+    -- reset hard-block: Xilinx Spartan-6 specific
+    --reset: startup_spartan6
+    --port map (
+    --	clk => clk, gsr => rs232_break, gts => rs232_break,
+    --	keyclearb => '0'
+    --   );
+
+    -- generic BRAM glue
+    glue_bram: entity work.glue_bram
+    generic map (
+	C_clk_freq => C_clk_freq,
+	C_mem_size => C_mem_size
+    )
+    port map (
+	clk => clk,
+	rs232_tx => rs232_txd, rs232_rx => rs232_rxd,
+	rs232_break => rs232_break,
+	gpio => gpio(28 downto 0),
+	leds => led,
+	btns => btns,
+	sw => "00000000"
+    );
+    btns <= "000000" & btn_left & btn_right;
+end Behavioral;
