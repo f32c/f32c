6c536d77462b31948c61bf0f91f8d6f1caf22309
davor <davor@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-04-10 19:24:06 +0000

    kreirao sam directory compiler da se po njegovom imenu zna čemu ovo služi i u njega strpo patches i sscripte koje downloadaju i buildaju compiler
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2826 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/compiler/build.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+
+#MAKE=gmake -j4
+MAKE=make
+
+# prefix to install tools in /usr/local/gnu_mips_f32c
+PREFIX=/usr/local/gnu_mips_f32c
+
+MPC_ARCHIVE=$(ls mpc-*.tar.*)
+MPC_DIR=$(tar -tf $MPC_ARCHIVE | head -n 1)
+MPFR_ARCHIVE=$(ls mpfr-*.tar.*)
+MPFR_DIR=$(tar -tf $MPFR_ARCHIVE | head -n 1)
+GMP_ARCHIVE=$(ls gmp-*.tar.*)
+GMP_DIR=$(tar -tf $GMP_ARCHIVE | head -n 1)
+BINUTILS_ARCHIVE=$(ls binutils-*.tar.*)
+BINUTILS_DIR=$(tar -tf $BINUTILS_ARCHIVE | head -n 1 | cut -d/ -f 1)
+GCC_ARCHIVE=$(ls gcc-*.tar.*)
+GCC_DIR=$(tar -tf $GCC_ARCHIVE | head -n 1)
+echo "${MPC_ARCHIVE} -> ${MPC_DIR}"
+echo "${MPFR_ARCHIVE} -> ${MPFR_DIR}"
+echo "${GMP_ARCHIVE} -> ${GMP_DIR}"
+echo "${BINUTILS_ARCHIVE} -> ${BINUTILS_DIR}"
+echo "${GCC_ARCHIVE} -> ${GCC_DIR}"
+
+if true
+then
+ rm -rf $MPC_DIR
+ tar -xf $MPC_ARCHIVE
+ rm -rf $MPFR_DIR
+ tar -xf $MPFR_ARCHIVE
+ rm -rf $GMP_DIR
+ tar -xf $GMP_ARCHIVE
+ rm -rf $BINUTILS_DIR
+ tar -xf $BINUTILS_ARCHIVE
+ rm -rf $GCC_DIR
+ tar -xf $GCC_ARCHIVE
+fi
+
+if true
+then
+ cd $GCC_DIR
+ ln -sf ../$MPC_DIR   mpc
+ ln -sf ../$MPFR_DIR  mpfr
+ ln -sf ../$GMP_DIR   gmp
+ cd ..
+fi
+
+if true
+then
+ cd $BINUTILS_DIR
+ patch -p0 < ../patches/binutils-2.23.1.diff
+ cd ..
+fi
+
+if true
+then
+ cd $GCC_DIR
+ patch -p0 < ../patches/gcc-4.8.1.diff
+ cd ..
+fi
+
+if true
+then
+ cd $BINUTILS_DIR
+ ./configure --target=mips-elf --enable-languages=c,c++ \
+            --prefix=$PREFIX --mandir=$PREFIX/man \
+            --infodir=$PREFIX/info --disable-nls --disable-shared \
+            --disable-werror
+ $MAKE
+ # sudo $MAKE install
+ cd ..
+fi
+
+if true
+then
+ cd $GCC_DIR
+ ./configure --target=mips-elf --enable-languages=c,c++ \
+            --prefix=$PREFIX --mandir=$PREFIX/man \
+            --infodir=$PREFIX/info --disable-nls --disable-shared \
+	    --disable-werror
+
+ $MAKE
+ # sudo $MAKE install
+ cd ..
+fi
