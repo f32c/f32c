652795cd0c3119bad429636ee053e695b9408a40
davor <davor@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-04-10 19:24:06 +0000

    kreirao sam directory compiler da se po njegovom imenu zna čemu ovo služi i u njega strpo patches i sscripte koje downloadaju i buildaju compiler
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2826 6164e1d0-f123-df11-80f4-0019d13f85ef

---
+++ src/compiler/download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget -c "http://ftp.gnu.org/gnu/binutils/binutils-2.23.1.tar.gz"
+wget -c "https://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2"
+wget -c "ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz"
+wget -c "http://www.mpfr.org/mpfr-3.0.1/mpfr-3.0.1.tar.xz"
+wget -c "http://mirror.anl.gov/pub/gnu/gmp/gmp-5.0.5.tar.xz"
