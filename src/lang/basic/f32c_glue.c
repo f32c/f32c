6b1f5a94d243e2ee460560baba5a904a6be0aad9
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-04-17 13:15:12 +0000

    Prilagodba promjenama SPI biblioteke.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2881 6164e1d0-f123-df11-80f4-0019d13f85ef

--- src/lang/basic/f32c_glue.c
+++ src/lang/basic/f32c_glue.c
@@ -74,12 +74,16 @@ static struct isr_link fb_isr = {
 void
 setup_f32c(void)
 {
+#ifdef __mips__
 	uint32_t tmp;
 
 	mfc0_macro(tmp, MIPS_COP_0_CONFIG);
 	freq_khz = ((tmp >> 16) & 0xfff) * 1000 / ((tmp >> 29) + 1);
 	isr_register_handler(2, &fb_isr);
 	__asm("ei");
+#else
+	fb_isr.handler_fn();
+#endif
 }
 
 
