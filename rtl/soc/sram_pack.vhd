c3458d5c0eefcf8e9cb2457be1a4052a3c0a6f59
marko <marko@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-04-22 11:01:22 +0000

    BSD license za rtl/cpu/*, i konzistentni license tekst tamo gdje je vec bila nekakva BSD license poruka.
    git-svn-id: svn+ssh://login.nxlab.fer.hr/fpgasvn/f32c@2954 6164e1d0-f123-df11-80f4-0019d13f85ef

--- rtl/soc/sram_pack.vhd
+++ rtl/soc/sram_pack.vhd
@@ -1,5 +1,6 @@
 --
--- Copyright 2013 Marko Zec, University of Zagreb
+-- Copyright (c) 2013. Marko Zec, University of Zagreb
+-- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions
@@ -10,10 +11,10 @@
 --    notice, this list of conditions and the following disclaimer in the
 --    documentation and/or other materials provided with the distribution.
 --
--- THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
 -- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 -- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
--- ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
 -- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 -- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
 -- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -22,9 +23,8 @@
 -- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 -- SUCH DAMAGE.
 --
-
 -- $Id$
-
+--
 
 library ieee;
 use ieee.std_logic_1164.all;
