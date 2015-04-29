689abe9cd491639dbcaa49592fd50fa7cb1e7668
gornjas <zec@fer.hr>
2015-04-29 11:46:30 +0200

    Top-level module for the E2LP board.

---
+++ rtl/xilinx/e2lp_toplevel_bram.vhd
@@ -0,0 +1,97 @@
+--
+-- Copyright (c) 2015 Marko Zec, University of Zagreb
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+--
+-- $Id$
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+library unisim;
+use unisim.vcomponents.all;
+
+use work.f32c_pack.all;
+
+
+entity glue is
+    generic (
+	-- ISA
+	C_arch: integer := ARCH_MI32;
+
+	-- Main clock: N * 10 MHz
+	C_clk_freq: integer := 70;
+
+	-- SoC configuration options
+	C_mem_size: integer := 64;
+	C_sio: boolean := true;
+	C_leds_btns: boolean := true
+    );
+    port (
+	clk_50m: in std_logic;
+	rs232_txd: out std_logic;
+	rs232_rxd: in std_logic;
+	led: out std_logic_vector(7 downto 0);
+	sw: in std_logic_vector(7 downto 0)
+    );
+end glue;
+
+architecture Behavioral of glue is
+    signal clk, rs232_break: std_logic;
+begin
+    -- clock synthesizer: Xilinx Spartan-6 specific
+    clkgen: entity work.clkgen
+    generic map(
+	C_clk_freq => C_clk_freq
+    )
+    port map(
+	clk_50m => clk_50m, clk => clk
+    );
+
+    -- reset hard-block: Xilinx Spartan-6 specific
+    reset: startup_spartan6
+    port map (
+	clk => clk, gsr => rs232_break, gts => rs232_break,
+	keyclearb => '0'
+    );
+
+    -- generic BRAM glue
+    glue_bram: entity work.glue_bram
+    generic map (
+	C_clk_freq => C_clk_freq,
+	C_arch => C_arch,
+	C_mem_size => C_mem_size
+    )
+    port map (
+	clk => clk,
+	rs232_tx => rs232_txd, rs232_rx => rs232_rxd,
+	rs232_break => rs232_break,
+	gpio => open,
+	leds(7 downto 0) => led, leds(15 downto 8) => open,
+	lcd_7seg => open, btns => x"0000",
+	sw(7 downto 0) => sw, sw(15 downto 8) => x"00"
+    );
+end Behavioral;
