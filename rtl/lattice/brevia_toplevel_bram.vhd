8151163d9b349a3f148a685ba01d194e5ab269d5
gornjas <zec@fer.hr>
2015-05-02 13:31:17 +0200

    Lattice Brevia (non-usb version) project.

---
+++ rtl/lattice/brevia_toplevel_bram.vhd
@@ -0,0 +1,92 @@
+--
+-- Copyright (c) 2015 Marko Zec, University of Zagreb
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+--
+-- $Id$
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+use work.f32c_pack.all;
+
+
+entity glue is
+    generic (
+	-- ISA: either ARCH_MI32 or ARCH_RV32
+	C_arch: integer := ARCH_MI32;
+	C_debug: boolean := false;
+
+	-- Main clock: 50, 62, 75, 81, 87, 100, 112, 125, 137, 150 MHz
+	C_clk_freq: integer := 100;
+
+	-- SoC configuration options
+	C_mem_size: integer := 16;
+	C_sio: boolean := true;
+	C_leds_btns: boolean := true
+    );
+    port (
+	clk_25m: in std_logic;
+	rs232_tx: out std_logic;
+	rs232_rx: in std_logic;
+	led: out std_logic_vector(7 downto 0);
+	btn_left, btn_right, btn_up, btn_down, btn_center: in std_logic;
+	sw: in std_logic_vector(3 downto 0)
+    );
+end glue;
+
+architecture Behavioral of glue is
+    signal clk, rs232_break: std_logic;
+    signal btns: std_logic_vector(4 downto 0);
+begin
+    -- clock synthesizer: Lattice XP2 specific
+    clkgen: entity work.clkgen
+    generic map (
+	C_clk_freq => C_clk_freq
+    )
+    port map (
+	clk_25m => clk_25m, clk => clk, clk_325m => open,
+	ena_325m => '0', res => rs232_break
+    );
+
+    -- generic BRAM glue
+    glue_bram: entity work.glue_bram
+    generic map (
+	C_arch => C_arch,
+	C_clk_freq => C_clk_freq,
+	C_mem_size => C_mem_size,
+	C_debug => C_debug
+    )
+    port map (
+	clk => clk,
+	rs232_tx => rs232_tx, rs232_rx => rs232_rx, rs232_break => rs232_break,
+	gpio => open, lcd_7seg => open,
+	leds(7 downto 0) => led, leds(15 downto 8) => open,
+	btns(4 downto 0) => btns, btns(15 downto 5) => open,
+	sw(3 downto 0) => sw
+    );
+    btns <= btn_left & btn_right & btn_up & btn_down & btn_center;
+end Behavioral;
