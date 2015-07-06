4e043ec2ffeb9681b3ab57a64a4576b0c76a92f5
goran-mahovlic <goran.mahovlic@gmail.com>
2015-07-06 07:54:39 -0400

    Initial numato_mimas_v2_bram changed ucf for numato_mimas_v2 support Fixing numato_mimas_v2 changed part no Working numato_mimas_v2_bram Numato Mimas V2 clenup More cleanup reverting settings.sh x64 path to x32 fix clock name

---
+++ rtl/xilinx/s6_numato_mimas_v2_toplevel_bram.vhd
@@ -0,0 +1,102 @@
+--
+-- Copyright (c) 2015 Davor Jadrijevic
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+--
+-- $Id$
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+library unisim;
+use unisim.vcomponents.all;
+
+use work.f32c_pack.all;
+
+
+entity glue is
+    generic (
+	-- ISA: either ARCH_MI32 or ARCH_RV32
+	C_arch: integer := ARCH_MI32;
+	C_debug: boolean := false;
+
+	-- Main clock: 81 or 112 not used clock is 100Mhz
+	C_clk_freq: integer := 100;
+
+	-- SoC configuration options
+	C_mem_size: integer := 32;
+	C_sio: boolean := true;
+	C_leds_btns: boolean := true
+    );
+    port (
+	clk_25m: in std_logic;
+	rs232_dce_txd: out std_logic;
+	rs232_dce_rxd: in std_logic;
+	led: out std_logic_vector(7 downto 0);
+	gpio: inout std_logic_vector(39 downto 0);
+	btn_k2, btn_k3: in std_logic
+    );
+end glue;
+
+architecture Behavioral of glue is
+    signal clk, rs232_break: std_logic;
+    signal btns: std_logic_vector(1 downto 0);
+begin
+    -- clock synthesizer: Xilinx Spartan-6 specific
+
+clk100: if C_clk_freq = 100 generate
+clk <= clk_25m;
+end generate;
+
+    -- reset hard-block: Xilinx Spartan-6 specific
+    reset: startup_spartan6
+    port map (
+	
+	clk => clk, gsr => rs232_break, gts => rs232_break,
+	keyclearb => '0'
+    );
+
+    -- generic BRAM glue
+    glue_bram: entity work.glue_bram
+    generic map (
+	C_arch => C_arch,
+	C_clk_freq => C_clk_freq,
+	C_mem_size => C_mem_size,
+	C_debug => C_debug
+    )
+    port map (
+	clk => clk,
+	rs232_tx => rs232_dce_txd, rs232_rx => rs232_dce_rxd,
+	rs232_break => rs232_break,
+	gpio => gpio(31 downto 0),
+	leds(7 downto 0) => led,
+	leds(15 downto 8) => open,
+	btns(1 downto 0) => btns(1 downto 0),
+	btns(15 downto 2) => (others => '-'),
+	sw => (others => '-')
+    );
+    btns <= btn_k3 & btn_k2;
+end Behavioral;
