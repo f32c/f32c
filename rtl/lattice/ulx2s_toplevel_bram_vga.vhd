0cf3cb27454808a5b5ef61d8ed529a468fc235c4
Emard <vordah@gmail.com>
2015-07-29 23:19:28 +0200

    propagating FM/RDS related generic constants from bram_vga to toplevel module

--- rtl/lattice/ulx2s_toplevel_bram_vga.vhd
+++ rtl/lattice/ulx2s_toplevel_bram_vga.vhd
@@ -46,8 +46,11 @@ entity glue is
 	C_vgahdmi: boolean := false;
 	C_vgahdmi_mem_kb: integer := 4; -- KB, very little BRAM available on lattice
 	C_fmrds: boolean := true;
+	C_rds_msg_len: integer := 260; -- bytes of RDS binary message, usually 52 (8-char PS) or 260 (8 PS + 64 RT)
 	C_fm_cw_hz: integer := 108000000; -- Hz FM station carrier wave frequency
         C_fmdds_hz: integer := 325000000; -- Hz clk_fmdds (>2*108 MHz, e.g. 250 MHz, 325 MHz)
+        --C_rds_clock_multiply: integer := 57; -- multiply and divide from cpu clk 100 MHz
+        --C_rds_clock_divide: integer := 3125; -- to get 1.824 MHz for RDS logic
         C_rds_clock_multiply: integer := 912; -- multiply and divide from cpu clk 81.25 MHz
         C_rds_clock_divide: integer := 40625; -- to get 1.824 MHz for RDS logic
 	C_sio: integer := 1;
@@ -98,6 +101,7 @@ begin
 	C_vgahdmi => C_vgahdmi,
 	C_vgahdmi_mem_kb => C_vgahdmi_mem_kb,
 	C_fmrds => C_fmrds,
+	C_rds_msg_len => C_rds_msg_len,
 	C_fm_cw_hz => C_fm_cw_hz,
 	C_fmdds_hz => C_fmdds_hz,
         C_rds_clock_multiply => C_rds_clock_multiply,
