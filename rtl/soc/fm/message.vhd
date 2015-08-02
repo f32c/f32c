e832d595142d1b279b52f760238565cc604db70e
Emard <vordah@gmail.com>
2015-08-02 14:44:17 +0200

    FM RDS default 107.9 MHz

--- rtl/soc/fm/message.vhd
+++ rtl/soc/fm/message.vhd
@@ -7,10 +7,13 @@ use ieee.numeric_std.all;
 package message is
 type rds_msg_type is array(0 to 259) of std_logic_vector(7 downto 0);
 -- PI=0xCAFE
+-- STEREO=No
+-- TA=No
+-- AF=107.9 MHz
 -- PS="TEST1234"
 -- RT="ABCDEFGHIJKLMNOPQRSTUVWXYZ    ABCDEFGHIJKLMNOPQRSTUVWXYZ    1234"
 constant rds_msg_map: rds_msg_type := (
-x"ca",x"fe",x"a0",x"01",x"00",x"2e",x"8c",x"dc",x"da",x"cd",x"51",x"15",x"fb",
+x"ca",x"fe",x"a0",x"01",x"00",x"2e",x"8e",x"1c",x"c2",x"31",x"51",x"15",x"fb",
 x"ca",x"fe",x"a0",x"01",x"00",x"75",x"1c",x"dc",x"da",x"cd",x"4d",x"51",x"e9",
 x"ca",x"fe",x"a0",x"01",x"00",x"99",x"ac",x"dc",x"da",x"cc",x"c4",x"cb",x"c6",
 x"ca",x"fe",x"a0",x"01",x"00",x"c2",x"3c",x"dc",x"da",x"cc",x"cc",x"d2",x"51",
