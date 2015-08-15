303af902664265187286c8cba87611a0454c5448
Emard <vordah@gmail.com>
2015-08-15 11:32:27 +0200

    generic true bram, whitespace change

--- rtl/generic/bram_true2p_1clk.vhd
+++ rtl/generic/bram_true2p_1clk.vhd
@@ -10,13 +10,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity bram_true2p_1clk is
-
 	generic 
 	(
 		DATA_WIDTH : natural := 8;
 		ADDR_WIDTH : natural := 6
 	);
-
 	port 
 	(
 		clk		: in std_logic;
@@ -29,21 +27,16 @@ entity bram_true2p_1clk is
 		q_a		: out std_logic_vector((DATA_WIDTH -1) downto 0);
 		q_b		: out std_logic_vector((DATA_WIDTH -1) downto 0)
 	);
-
 end bram_true2p_1clk;
 
 architecture rtl of bram_true2p_1clk is
-
 	-- Build a 2-D array type for the RAM
 	subtype word_t is std_logic_vector((DATA_WIDTH-1) downto 0);
 	type memory_t is array(2**ADDR_WIDTH-1 downto 0) of word_t;
 
 	-- Declare the RAM 
 	shared variable ram : memory_t;
-
 begin
-
-
 	-- Port A
 	process(clk)
 	begin
@@ -65,5 +58,4 @@ begin
   	    q_b <= ram(addr_b);
 	end if;
 	end process;
-
 end rtl;
