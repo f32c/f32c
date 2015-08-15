e0223e3b3e05d4ec40a6307bef21810efc83aab1
Emard <vordah@gmail.com>
2015-08-15 11:36:56 +0200

    bram true dp, lowercasing the names - verilog mix friendly

--- rtl/generic/bram_true2p_1clk.vhd
+++ rtl/generic/bram_true2p_1clk.vhd
@@ -12,27 +12,27 @@ use ieee.std_logic_1164.all;
 entity bram_true2p_1clk is
 	generic 
 	(
-		DATA_WIDTH : natural := 8;
-		ADDR_WIDTH : natural := 6
+		data_width : natural := 8;
+		addr_width : natural := 6
 	);
 	port 
 	(
 		clk		: in std_logic;
-		addr_a	: in natural range 0 to 2**ADDR_WIDTH - 1;
-		addr_b	: in natural range 0 to 2**ADDR_WIDTH - 1;
-		data_a	: in std_logic_vector((DATA_WIDTH-1) downto 0);
-		data_b	: in std_logic_vector((DATA_WIDTH-1) downto 0);
+		addr_a	: in natural range 0 to 2**addr_width - 1;
+		addr_b	: in natural range 0 to 2**addr_width - 1;
+		data_a	: in std_logic_vector((data_width-1) downto 0);
+		data_b	: in std_logic_vector((data_width-1) downto 0);
 		we_a	: in std_logic := '1';
 		we_b	: in std_logic := '1';
-		q_a		: out std_logic_vector((DATA_WIDTH -1) downto 0);
-		q_b		: out std_logic_vector((DATA_WIDTH -1) downto 0)
+		q_a		: out std_logic_vector((data_width -1) downto 0);
+		q_b		: out std_logic_vector((data_width -1) downto 0)
 	);
 end bram_true2p_1clk;
 
 architecture rtl of bram_true2p_1clk is
 	-- Build a 2-D array type for the RAM
-	subtype word_t is std_logic_vector((DATA_WIDTH-1) downto 0);
-	type memory_t is array(2**ADDR_WIDTH-1 downto 0) of word_t;
+	subtype word_t is std_logic_vector((data_width-1) downto 0);
+	type memory_t is array(2**addr_width-1 downto 0) of word_t;
 
 	-- Declare the RAM 
 	shared variable ram : memory_t;
