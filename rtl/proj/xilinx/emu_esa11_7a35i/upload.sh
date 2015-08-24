df8fe9176e190624ffa4a945a58a76aa589da785
Emard <vordah@gmail.com>
2015-08-24 23:09:48 +0200

    comment out impact conversion bit->svf this won't work

--- rtl/proj/xilinx/emu_esa11_7a35i/upload.sh
+++ rtl/proj/xilinx/emu_esa11_7a35i/upload.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/bash -e
+
+# convert to svf
+# . /usr/local/xilinx/14.7/ISE_DS/settings32.sh
+# impact -batch ../include/bit2svf.ut
 
 openocd -f ../include/ft2232-fpu1.ocd -f esa11.ocd
