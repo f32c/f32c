b0f9623db42b579b793b608d801e1c87af7c1081
Emard <vordah@gmail.com>
2015-08-25 00:54:30 +0200

    esa11 upload over onboard ft4232h (patched esa11)

--- rtl/proj/xilinx/emu_esa11_7a35i/upload.sh
+++ rtl/proj/xilinx/emu_esa11_7a35i/upload.sh
@@ -4,4 +4,4 @@
 # . /usr/local/xilinx/14.7/ISE_DS/settings32.sh
 # impact -batch ../include/bit2svf.ut
 # openocd -f ../include/ft2232-fpu1.ocd -f esa11.ocd
-xc3sprog -c ftdi default.bit
+xc3sprog -c ft4232h -p 0 default.bit
