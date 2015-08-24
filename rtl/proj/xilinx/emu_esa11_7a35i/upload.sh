164d9110a6a2235e99cfb1ca156560fd268d4144
Emard <vordah@gmail.com>
2015-08-24 23:23:58 +0200

    upload success with xc3sprog

--- rtl/proj/xilinx/emu_esa11_7a35i/upload.sh
+++ rtl/proj/xilinx/emu_esa11_7a35i/upload.sh
@@ -3,5 +3,5 @@
 # convert to svf
 # . /usr/local/xilinx/14.7/ISE_DS/settings32.sh
 # impact -batch ../include/bit2svf.ut
-
-openocd -f ../include/ft2232-fpu1.ocd -f esa11.ocd
+# openocd -f ../include/ft2232-fpu1.ocd -f esa11.ocd
+xc3sprog -c ftdi default.bit
