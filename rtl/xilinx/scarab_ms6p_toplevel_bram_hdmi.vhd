096dc1c5bce3efce8f9b23040bc9866205b315fa
Xark <XarkLabs@users.noreply.github.com>
2015-11-22 19:16:42 -0800

    Fix up VGA textmode generics.  Add all generics.  Fix comments, indent to show dependent options. Verify all generics are set and passed through.  Modify glue defaults to be minimal resources. Modify toplevel miniSpartan6+ bram and sdram to nicer defaults.  Tested with 'boulder'. :-)

--- rtl/xilinx/scarab_ms6p_toplevel_bram_hdmi.vhd
+++ rtl/xilinx/scarab_ms6p_toplevel_bram_hdmi.vhd
@@ -39,7 +39,7 @@ use work.f32c_pack.all;
 
 
 entity glue is
-    generic (
+	generic (
 	-- ISA: either ARCH_MI32 or ARCH_RV32
 	C_arch: integer := ARCH_MI32;
 	C_debug: boolean := false;
@@ -51,15 +51,33 @@ entity glue is
 	C_mem_size: integer := 32; -- KB
 	C_vgahdmi: boolean := false;
 	C_vgahdmi_mem_kb: integer := 38; -- KB 38K full mono 640x480
-	C_vgatext: boolean := true; -- Xark's feature-ritch bitmap+textmode VGA
-	C_vgatext_label: string := "f32c: miniSpartan6+ MIPS compatible soft-core 100MHz 32KB BRAM";
-	C_vgatext_bitmap: boolean := false;
-	C_vgatext_bitmap_fifo: boolean := false;		-- true to use videofifo, else SRAM port
+	
+	C_vgatext: boolean := true; -- Xark's feature-rich bitmap+textmode VGA
+		C_vgatext_label: string :=  "f32c: miniSpartan6+ MIPS compatible soft-core 100MHz 32KB BRAM";	-- default banner in screen memory
+		C_vgatext_mode: integer := 0;				-- 0=640x480, 1=640x400, 2=800x600 (you must still provide proper pixel clock [25MHz or 40Mhz])
+		C_vgatext_bits: integer := 2;				-- bits of VGA color per red, green, blue gun (e.g., 1=8, 2=64 and 4=4096 total colors possible)
+		C_vgatext_mem: integer := 8;				-- BRAM size 1, 2, 4, 8 or 16 depending on font and screen size/memory
+		C_vgatext_palette: boolean := false;			-- true for run-time color look-up table, else 16 fixed VGA color palette
+		C_vgatext_text: boolean := true;			-- enable text generation	
+			C_vgatext_monochrome: boolean := false;		-- true for 2-color text for whole screen, else additional color attribute byte per character
+			C_vgatext_font_height: integer := 16;		-- font data height 8 or 16 for 8x8 or 8x16 font
+			C_vgatext_char_height: integer := 16;		-- font cell height (text lines will be C_visible_height / C_CHAR_HEIGHT rounded down, 19=25 lines on 480p)
+			C_vgatext_font_linedouble: boolean := false; -- double font height by doubling each line (e.g., so 8x8 font fills 8x16 cell)
+			C_vgatext_font_depth: integer := 7;			-- font char bits 7 for 128 characters or 8 for 256 characters
+			C_vgatext_text_fifo: boolean := false;		-- true to use videofifo for text+color, else BRAM for text+color memory
+				C_vgatext_text_fifo_step: integer := (80*2)/4; -- step for the fifo refill and rewind
+				C_vgatext_text_fifo_width: integer := 6; 	-- width of FIFO address space (default=4) len = 2^width * 4 byte
+		C_vgatext_bitmap: boolean := false;			-- true to enable bitmap generation
+		C_vgatext_bitmap_depth: integer := 8;		-- bitmap bits per pixel (1, 2, 4, 8)
+			C_vgatext_bitmap_fifo: boolean := false;		-- true to use videofifo, else SRAM port for bitmap memory
+				C_vgatext_bitmap_fifo_step: integer := 0;	-- bitmap step for the fifo refill and rewind (0 unless repeating lines)
+				C_vgatext_bitmap_fifo_width: integer := 8;	-- bitmap width of FIFO address space len = 2^width * 4 byte
+	
 	C_fmrds: boolean := true;
 	C_rds_msg_len: integer := 260; -- bytes of RAM for RDS binary message
-        C_fmdds_hz: integer := 250000000; -- Hz clk_fmdds (>2*108 MHz, e.g. 250 MHz, 325 MHz)
-        C_rds_clock_multiply: integer := 57; -- multiply and divide from cpu clk 100 MHz
-        C_rds_clock_divide: integer := 3125; -- to get 1.824 MHz for RDS logic
+		C_fmdds_hz: integer := 250000000; -- Hz clk_fmdds (>2*108 MHz, e.g. 250 MHz, 325 MHz)
+		C_rds_clock_multiply: integer := 57; -- multiply and divide from cpu clk 100 MHz
+		C_rds_clock_divide: integer := 3125; -- to get 1.824 MHz for RDS logic
 	-- warning long compile time on ISE 14.7
 	-- C_pids = 2: 1 hour
 	-- C_pids = 4: 4 hours
@@ -72,8 +90,8 @@ entity glue is
 	C_spi: integer := 2;
 	C_gpio: integer := 32;
 	C_simple_io: boolean := true
-    );
-    port (
+	);
+	port (
 	clk_50MHz: in std_logic;
 	rs232_tx: out std_logic;
 	rs232_rx: in std_logic;
@@ -88,66 +106,82 @@ entity glue is
 	TMDS_out_P, TMDS_out_N: out std_logic_vector(2 downto 0);
 	TMDS_out_CLK_P, TMDS_out_CLK_N: out std_logic;
 	sw: in std_logic_vector(4 downto 1)
-    );
+	);
 end glue;
 
 architecture Behavioral of glue is
-    signal clk, rs232_break: std_logic;
-    signal btns: std_logic_vector(1 downto 0);
-    signal clk_25MHz, clk_250MHz: std_logic := '0';
-    signal tmds_out_rgb: std_logic_vector(2 downto 0);
+	signal clk, rs232_break: std_logic;
+	signal btns: std_logic_vector(1 downto 0);
+	signal clk_25MHz, clk_250MHz: std_logic := '0';
+	signal tmds_out_rgb: std_logic_vector(2 downto 0);
 begin
-    -- clock synthesizer: Xilinx Spartan-6 specific
-    
-    clk112: if C_clk_freq = 112 generate
-    clkgen112: entity work.pll_50M_112M5
-    port map(
-      clk_in1 => clk_50MHz, clk_out1 => clk
-    );
-    end generate;
+	-- clock synthesizer: Xilinx Spartan-6 specific
+	
+	clk112: if C_clk_freq = 112 generate
+	clkgen112: entity work.pll_50M_112M5
+	port map(
+	  clk_in1 => clk_50MHz, clk_out1 => clk
+	);
+	end generate;
 
-    clk100: if C_clk_freq = 100 generate
-    clkgen100: entity work.pll_50M_100M_25M_250M
-    port map(
-      clk_in1 => clk_50MHz, clk_out1 => clk, clk_out2 => clk_25MHz, clk_out3 => clk_250MHz
-    );
-    end generate;
+	clk100: if C_clk_freq = 100 generate
+	clkgen100: entity work.pll_50M_100M_25M_250M
+	port map(
+	  clk_in1 => clk_50MHz, clk_out1 => clk, clk_out2 => clk_25MHz, clk_out3 => clk_250MHz
+	);
+	end generate;
 
-    clk81: if C_clk_freq = 81 generate
-    clkgen81: entity work.pll_50M_81M25
-    port map(
-      clk_in1 => clk_50MHz, clk_out1 => clk
-    );
-    end generate;
+	clk81: if C_clk_freq = 81 generate
+	clkgen81: entity work.pll_50M_81M25
+	port map(
+	  clk_in1 => clk_50MHz, clk_out1 => clk
+	);
+	end generate;
 
-    clk50: if C_clk_freq = 50 generate
-      clk <= clk_50MHz;
-    end generate;
+	clk50: if C_clk_freq = 50 generate
+	  clk <= clk_50MHz;
+	end generate;
 
-    -- reset hard-block: Xilinx Spartan-6 specific
-    reset: startup_spartan6
-    port map (
+	-- reset hard-block: Xilinx Spartan-6 specific
+	reset: startup_spartan6
+	port map (
 	clk => clk, gsr => rs232_break, gts => rs232_break,
 	keyclearb => '0'
-    );
+	);
 
-    -- generic BRAM glue
-    glue_bram: entity work.glue_bram
-    generic map (
+	-- generic BRAM glue
+	glue_bram: entity work.glue_bram
+	generic map (
 	C_arch => C_arch,
 	C_clk_freq => C_clk_freq,
 	C_mem_size => C_mem_size,
 	C_vgahdmi => C_vgahdmi,
 	C_vgahdmi_mem_kb => C_vgahdmi_mem_kb,
-        C_vgatext => C_vgatext,
+	C_vgatext => C_vgatext,
 	C_vgatext_label => C_vgatext_label,
-        C_vgatext_bitmap => C_vgatext_bitmap,
+	C_vgatext_mode => C_vgatext_mode,
+	C_vgatext_bits => C_vgatext_bits,
+	C_vgatext_mem => C_vgatext_mem,
+	C_vgatext_palette => C_vgatext_palette,
+	C_vgatext_text => C_vgatext_text,
+	C_vgatext_monochrome => C_vgatext_monochrome,
+	C_vgatext_font_height => C_vgatext_font_height,
+	C_vgatext_char_height => C_vgatext_char_height,
+	C_vgatext_font_linedouble => C_vgatext_font_linedouble,
+	C_vgatext_font_depth => C_vgatext_font_depth,
+	C_vgatext_text_fifo => C_vgatext_text_fifo,
+	C_vgatext_text_fifo_step => C_vgatext_text_fifo_step,
+	C_vgatext_text_fifo_width => C_vgatext_text_fifo_width,
+	C_vgatext_bitmap => C_vgatext_bitmap,
+	C_vgatext_bitmap_depth => C_vgatext_bitmap_depth,
 	C_vgatext_bitmap_fifo => C_vgatext_bitmap_fifo,
+	C_vgatext_bitmap_fifo_step => C_vgatext_bitmap_fifo_step,
+	C_vgatext_bitmap_fifo_width => C_vgatext_bitmap_fifo_width,
 	C_fmrds => C_fmrds,
 	C_fmdds_hz => C_fmdds_hz,
 	C_rds_msg_len => C_rds_msg_len,
-        C_rds_clock_multiply => C_rds_clock_multiply,
-        C_rds_clock_divide => C_rds_clock_divide,
+		C_rds_clock_multiply => C_rds_clock_multiply,
+		C_rds_clock_divide => C_rds_clock_divide,
 	C_gpio => C_gpio,
 	C_sio => C_sio,
 	C_spi => C_spi,
@@ -158,8 +192,8 @@ begin
 	C_pid_precision => C_pid_precision, -- fixed point PID precision
 	C_pid_pwm_bits => C_pid_pwm_bits, -- clock divider bits define PWM output frequency
 	C_debug => C_debug
-    )
-    port map (
+	)
+	port map (
 	clk => clk,
 	clk_25MHz => clk_25MHz, -- pixel clock
 	clk_250MHz => clk_250MHz, -- tmds clock
@@ -181,17 +215,17 @@ begin
 	simple_in(31 downto 20) => open,
 	fm_antenna => portd(0),
 	tmds_out_rgb => tmds_out_rgb
-    );
-    
-    -- differential output buffering for HDMI clock and video
-    hdmi_output: entity work.hdmi_out
-      port map (
-        tmds_in_clk => clk_25MHz,
-        tmds_out_clk_p => tmds_out_clk_p,
-        tmds_out_clk_n => tmds_out_clk_n,
-        tmds_in_rgb => tmds_out_rgb,
-        tmds_out_rgb_p => tmds_out_p,
-        tmds_out_rgb_n => tmds_out_n
-      );
+	);
+	
+	-- differential output buffering for HDMI clock and video
+	hdmi_output: entity work.hdmi_out
+	  port map (
+		tmds_in_clk => clk_25MHz,
+		tmds_out_clk_p => tmds_out_clk_p,
+		tmds_out_clk_n => tmds_out_clk_n,
+		tmds_in_rgb => tmds_out_rgb,
+		tmds_out_rgb_p => tmds_out_p,
+		tmds_out_rgb_n => tmds_out_n
+	  );
 
 end Behavioral;
