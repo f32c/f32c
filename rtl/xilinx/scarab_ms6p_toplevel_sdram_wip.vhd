659aa734573385f036f9755f33bbc86d6e36ecac
Emard <vordah@gmail.com>
2015-12-16 18:15:30 +0100

    scarab toplevel, changes towards 8bpp compositing in textmode

--- rtl/xilinx/scarab_ms6p_toplevel_sdram_wip.vhd
+++ rtl/xilinx/scarab_ms6p_toplevel_sdram_wip.vhd
@@ -104,16 +104,18 @@ entity glue is
           --C_vgatext_bitmap_fifo_step: integer := 0; -- disabled
           --C_vgatext_bitmap_fifo_postpone_step: integer := 0; -- disabled
           --C_vgatext_bitmap_fifo_compositing_length: integer := 0; -- disabled
-          --C_vgatext_bitmap_fifo_width: integer := 8; -- bitmap width of FIFO address space length = 2^width * 4 byte
+          --C_vgatext_bitmap_fifo_addr_width: integer := 8; -- bitmap width of FIFO address space length = 2^width * 4 byte
 
           -- step=10*length make 680 bytes, contains 640 pixels and 20 16-bit offsets for compositing
           C_vgatext_bitmap_fifo_step: integer := 10*17;
           -- postpone step as much as possible to avoid flickering of a left sprite moved right
-          C_vgatext_bitmap_fifo_postpone_step: integer := 10*17-4;
+          C_vgatext_bitmap_fifo_postpone_step: integer := 10*17-8;
           -- word length for H-compositing thin sprite, including offset word (tiny sprites one pixel high)
           C_vgatext_bitmap_fifo_compositing_length: integer := 17;
+          -- output data width 8bpp
+          C_vgatext_bitmap_fifo_data_width: integer := 8;
           -- bitmap width of FIFO address space length = 2^width * 4 byte
-          C_vgatext_bitmap_fifo_width: integer := 10;
+          C_vgatext_bitmap_fifo_addr_width: integer := 10;
 
       C_fmrds: boolean := true;
       C_sio: integer := 1;
@@ -278,7 +280,8 @@ begin
       C_vgatext_bitmap_fifo_step => C_vgatext_bitmap_fifo_step,
       C_vgatext_bitmap_fifo_postpone_step => C_vgatext_bitmap_fifo_postpone_step,
       C_vgatext_bitmap_fifo_compositing_length => C_vgatext_bitmap_fifo_compositing_length,
-      C_vgatext_bitmap_fifo_width => C_vgatext_bitmap_fifo_width,
+      C_vgatext_bitmap_fifo_data_width => C_vgatext_bitmap_fifo_data_width,
+      C_vgatext_bitmap_fifo_addr_width => C_vgatext_bitmap_fifo_addr_width,
       C_fmrds => C_fmrds,
       C_debug => C_debug
     )
