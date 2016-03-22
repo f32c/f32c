3652dcc1787c4987e302d5d222e3017d9da6fab3
davor <davor@ssd.lan>
2016-03-22 09:57:15 +0100

    add cygwin example helper script to set environment

---
+++ src/tools/cygwin.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#
+#       Standard defines for F32C
+#
+export PATH=/usr/local/gnu_mips_f32c/bin:${PATH}
+export MAKEFILES=/cygdrive/c/tmp/f32c.git/src/conf/f32c.mk
+export vivado=/cygdrive/c/Xilinx/Vivado/2015.4/bin/vivado
+
+echo "remember to call this it with single DOT first"
+echo ". ./cygwin.sh"
