f32c/f32c

commit 47904a86d6d2402b667f5430a2a5144b9d499d75

scarab unbreak compiling after latest bootloader and sram refresh additions

rtl/lattice/ulx2s_toplevel_sram.vhd
@@ -33,6 +33,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 use work.f32c_pack.all;
 use work.sram_pack.all;
+use work.boot_block_pack.all;
+use work.boot_rom_mi32el.all;
 
 
 entity glue is
@@ -85,7 +87,7 @@ entity glue is
 	C_sdcard: boolean := true;
 	C_framebuffer: boolean := true;
 	C_pcm: boolean := true;
-	C_timer: boolean := false;
+	C_timer: boolean := true;
 	C_tx433: boolean := false; -- set (C_framebuffer := false, C_dds := false) for 433MHz transmitter
 	C_dds: boolean := false
     );
@@ -154,7 +156,7 @@ architecture Behavioral of glue is
 
     -- Block RAM
     signal bram_i_to_cpu, bram_d_to_cpu: std_logic_vector(31 downto 0);
-    signal bram_i_ready, bram_d_ready, dmem_bram_enable: std_logic;
+    signal bram_i_ready, bram_d_ready, dmem_bram_enable, dmem_bram_write: std_logic;
 
     -- I/O
     signal io_write: std_logic;
@@ -531,17 +533,19 @@ begin
     G_i_d_ram:
     if not C_i_rom_only generate
     begin
-    dmem_bram_enable <= dmem_addr_strobe(0) when dmem_addr(0)(31) /= '1'
+    dmem_bram_write <= dmem_addr_strobe(0) and dmem_write(0) when dmem_addr(0)(31) /= '1'
       else '0';
+    bram_i_ready <= imem_addr_strobe(0);
+    bram_d_ready <= dmem_addr_strobe(0);
     bram: entity work.bram
     generic map (
+        boot_block => boot_rom_mi32el,
 	C_mem_size => C_bram_size
     )
     port map (
-	clk => clk, imem_addr_strobe => imem_addr_strobe(0),
+	clk => clk,
 	imem_addr => imem_addr(0), imem_data_out => bram_i_to_cpu,
-	imem_data_ready => bram_i_ready, dmem_data_ready => bram_d_ready,
-	dmem_addr_strobe => dmem_bram_enable, dmem_write => dmem_write(0),
+	dmem_write => dmem_bram_write,
 	dmem_byte_sel => dmem_byte_sel(0), dmem_addr => dmem_addr(0),
 	dmem_data_out => bram_d_to_cpu, dmem_data_in => cpu_to_dmem(0)
     );
@@ -550,15 +554,17 @@ begin
     G_i_rom:
     if C_i_rom_only generate
     begin
+    bram_i_ready <= imem_addr_strobe(0);
+    bram_d_ready <= '1';
     bram: entity work.bram
     generic map (
+        boot_block => boot_rom_mi32el,
 	C_mem_size => C_bram_size
     )
     port map (
-	clk => clk, imem_addr_strobe => imem_addr_strobe(0),
+	clk => clk,
 	imem_addr => imem_addr(0), imem_data_out => bram_i_to_cpu,
-	imem_data_ready => bram_i_ready, dmem_data_ready => open,
-	dmem_addr_strobe => '0', dmem_write => '0',
+	dmem_write => '0',
 	dmem_byte_sel => x"0", dmem_addr => (others => '0'),
 	dmem_data_out => open, dmem_data_in => (others => '0')
     );

rtl/lattice/ulx2s_toplevel_xram.vhd
@@ -71,7 +71,9 @@ entity toplevel is
     -- ISA options
     C_arch: integer := ARCH_MI32;
     C_big_endian: boolean := false;
-    C_boot_rom: boolean := true; -- true: bootloader will try to chainboot SPI flash ROM, false: serial bootloader only
+    -- C_boot_rom = true: bootloader will try to chainboot SPI flash ROM, fallback to serial
+    -- C_boot_rom = false: serial bootloader only
+    C_boot_rom: boolean := false;
     C_mult_enable: boolean := true;
     C_branch_likely: boolean := true;
     C_sign_extend: boolean := true;
@@ -104,6 +106,8 @@ entity toplevel is
       C_icache_size: integer := 2;	-- 0, 2, 4 or 8 KBytes
       C_dcache_size: integer := 2;	-- 0, 2, 4 or 8 KBytes
 
+    C_xram_base: std_logic_vector(31 downto 28) := x"8"; -- RAM start address e.g. x"8" -> 0x80000000
+
     C_sram: boolean := true;
       C_sram_refresh: boolean := true; -- RED ULX2S need it, others don't (exclusive: textmode or refresh)
       C_sram_wait_cycles: integer := 4; -- ISSI, OK do 87.5 MHz
@@ -124,7 +128,7 @@ entity toplevel is
 
     C_framebuffer: boolean := false; -- TV framebuffer (not yet supported in glue_xram)
 
-    C_vgahdmi: boolean := false; -- simple VGA bitmap with compositing
+    C_vgahdmi: boolean := true; -- simple VGA bitmap with compositing
       C_vgahdmi_test_picture: integer := 0;
       -- number of pixels for line step 640 for no compositing
       -- 680 for compositing
@@ -321,12 +325,12 @@ begin
       C_icache_expire => C_icache_expire,
       C_icache_size => C_icache_size,	-- 0, 2, 4 or 8 KBytes
       C_dcache_size => C_dcache_size,	-- 0, 2, 4 or 8 KBytes
+      C_xram_base => C_xram_base,
       C_sram => C_sram,
       C_sram_refresh => C_sram_refresh,
       C_sram_wait_cycles => C_sram_wait_cycles, -- ISSI, OK do 87.5 MHz
       C_pipelined_read => C_pipelined_read, -- works only at 81.25 MHz !!!
       C_sdram => C_sdram,
-      C_sdram_base => C_sdram_base,
       C_sdram_separate_arbiter => C_sdram_separate_arbiter,
       C_sdram_address_width => 24,
       C_sio => C_sio,

rtl/proj/lattice/ulx2s_sram/project.ldf
@@ -42,7 +42,13 @@
         <Source name="../../../lattice/bptrace.vhd" type="VHDL" type_short="VHDL">
             <Options/>
         </Source>
-        <Source name="../../../lattice/bram.vhd" type="VHDL" type_short="VHDL">
+        <Source name="../../../generic/bram.vhd" type="VHDL" type_short="VHDL">
+            <Options/>
+        </Source>
+        <Source name="../../../generic/boot_block_pack.vhd" type="VHDL" type_short="VHDL">
+            <Options/>
+        </Source>
+        <Source name="../../../generic/bootloader/boot_rom_mi32el.vhd" type="VHDL" type_short="VHDL">
             <Options/>
         </Source>
         <Source name="../../../lattice/bram_dp_x9.vhd" type="VHDL" type_short="VHDL">

rtl/proj/lattice/ulx2s_xram_vga/project.ldf
@@ -123,7 +123,7 @@
         <Source name="../../../soc/vgahdmi/font8x16_xark.vhd" type="VHDL" type_short="VHDL">
             <Options/>
         </Source>
-        <Source name="../../../soc/vgahdmi/compositing_fifo2.vhd" type="VHDL" type_short="VHDL">
+        <Source name="../../../soc/vgahdmi/compositing_fifo.vhd" type="VHDL" type_short="VHDL">
             <Options/>
         </Source>
         <Source name="../../../soc/vgahdmi/videofifo.vhd" type="VHDL" type_short="VHDL">

rtl/proj/xilinx/scarab_ms6p_sdram_hdmi/Makefile
@@ -36,6 +36,7 @@ vhdfiles = \
 ../../../generic/bootloader/boot_sio_mi32el.vhd \
 ../../../generic/bootloader/boot_sio_mi32eb.vhd \
 ../../../generic/bootloader/boot_sio_rv32el.vhd \
+../../../generic/bootloader/boot_rom_mi32el.vhd \
 ../../../generic/bram_true2p_1clk.vhd \
 ../../../generic/bram_true2p_2clk.vhd \
 ../../../generic/bptrace.vhd \
@@ -54,6 +55,7 @@ vhdfiles = \
 ../../../cpu/debug.vhd \
 ../../../soc/sram_pack.vhd \
 ../../../soc/sram.vhd \
+../../../soc/sram_refresh.vhd \
 ../../../soc/sram8.vhd \
 ../../../soc/arbiter.vhd \
 ../../../soc/sdram_ctrl.vhd \

rtl/xilinx/scarab_ms6p_toplevel_sdram.vhd
@@ -50,8 +50,8 @@ entity glue is
     C_icache_expire: boolean := false; -- false: normal i-cache, true: passthru buggy i-cache
     C_icache_size: integer := 32; -- 0, 2, 4, 8, 16, 32 KBytes
     C_dcache_size: integer := 8; -- 0, 2, 4, 8, 16, 32 KBytes
+    C_xram_base: std_logic_vector(31 downto 28) := x"8"; -- RAM start address e.g. x"8" -> 0x80000000
     C_sdram: boolean := true;
-    C_sdram_base: std_logic_vector(31 downto 28) := x"8"; -- RAM start address e.g. x"8" -> 0x80000000
     C_sdram_separate_arbiter: boolean := false;
     C_ram_emu_addr_width: integer := 0; -- RAM emulation (0:disable, 11:8K, 12:16K ...)
     C_ram_emu_wait_states: integer := 2; -- 0 doesn't work, 1 and more works
@@ -299,8 +299,8 @@ begin
       C_gpio => C_gpio,
       C_sio => C_sio,
       C_spi => C_spi,
+      C_xram_base => C_xram_base,
       C_sdram => C_sdram,
-      C_sdram_base => C_sdram_base,
       C_sdram_separate_arbiter => C_sdram_separate_arbiter,
       C_sdram_address_width => 24,
       C_sdram_column_bits => 9,

commit 7b61607be97eb17824b589988e55cd8ed6a1e00c

Back out 47904a86d6d2402b667f5430a2a5144b9d499d75, which broke ULX2S SRAM on XP2-5E based boards, because among other issues is unnecessarily wasteful on BRAM usage, so the design could not fit in 9 BRAMs.
ULX2S boards with XP2-5E FPGAs are our reference boards which we
MUST continue to support, so please be extra careful not to break
their designs.

Note that this commit only backs out the change to ulx2s_toplevel_sram.vhd
but doesn't touch other files in an attempt to not introduce additional
harm, so the state of our BRAM infrastructure is a complete mess and we
have to revise it carefully.

rtl/lattice/ulx2s_toplevel_sram.vhd
@@ -33,8 +33,6 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 use work.f32c_pack.all;
 use work.sram_pack.all;
-use work.boot_block_pack.all;
-use work.boot_rom_mi32el.all;
 
 
 entity glue is
@@ -86,7 +84,7 @@ entity glue is
 	C_sdcard: boolean := true;
 	C_framebuffer: boolean := true;
 	C_pcm: boolean := true;
-	C_timer: boolean := true;
+	C_timer: boolean := false;
 	C_tx433: boolean := false; -- set (C_framebuffer := false, C_dds := false) for 433MHz transmitter
 	C_dds: boolean := false
     );
@@ -155,7 +153,7 @@ architecture Behavioral of glue is
 
     -- Block RAM
     signal bram_i_to_cpu, bram_d_to_cpu: std_logic_vector(31 downto 0);
-    signal bram_i_ready, bram_d_ready, dmem_bram_enable, dmem_bram_write: std_logic;
+    signal bram_i_ready, bram_d_ready, dmem_bram_enable: std_logic;
 
     -- I/O
     signal io_write: std_logic;
@@ -531,19 +529,17 @@ begin
     G_i_d_ram:
     if not C_i_rom_only generate
     begin
-    dmem_bram_write <= dmem_addr_strobe(0) and dmem_write(0) when dmem_addr(0)(31) /= '1'
+    dmem_bram_enable <= dmem_addr_strobe(0) when dmem_addr(0)(31) /= '1'
       else '0';
-    bram_i_ready <= imem_addr_strobe(0);
-    bram_d_ready <= dmem_addr_strobe(0);
     bram: entity work.bram
     generic map (
-        boot_block => boot_rom_mi32el,
 	C_mem_size => C_bram_size
     )
     port map (
-	clk => clk,
+	clk => clk, imem_addr_strobe => imem_addr_strobe(0),
 	imem_addr => imem_addr(0), imem_data_out => bram_i_to_cpu,
-	dmem_write => dmem_bram_write,
+	imem_data_ready => bram_i_ready, dmem_data_ready => bram_d_ready,
+	dmem_addr_strobe => dmem_bram_enable, dmem_write => dmem_write(0),
 	dmem_byte_sel => dmem_byte_sel(0), dmem_addr => dmem_addr(0),
 	dmem_data_out => bram_d_to_cpu, dmem_data_in => cpu_to_dmem(0)
     );
@@ -552,17 +548,15 @@ begin
     G_i_rom:
     if C_i_rom_only generate
     begin
-    bram_i_ready <= imem_addr_strobe(0);
-    bram_d_ready <= '1';
     bram: entity work.bram
     generic map (
-        boot_block => boot_rom_mi32el,
 	C_mem_size => C_bram_size
     )
     port map (
-	clk => clk,
+	clk => clk, imem_addr_strobe => imem_addr_strobe(0),
 	imem_addr => imem_addr(0), imem_data_out => bram_i_to_cpu,
-	dmem_write => '0',
+	imem_data_ready => bram_i_ready, dmem_data_ready => open,
+	dmem_addr_strobe => '0', dmem_write => '0',
 	dmem_byte_sel => x"0", dmem_addr => (others => '0'),
 	dmem_data_out => open, dmem_data_in => (others => '0')
     );