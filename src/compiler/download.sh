d9d2dd6e19c95cd67a5c9d8dcba52295a6394a73
Juraj Oršulić <juraj5@gmail.com>
2016-04-16 00:20:20 +0200

    Update patches to support new binutils & gcc 5.3.0
    Amended post.mk to fix quirk introduced in binutils 2.25

--- src/compiler/download.sh
+++ src/compiler/download.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget -c "http://ftp.gnu.org/gnu/binutils/binutils-2.23.1.tar.gz"
-wget -c "https://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2"
-wget -c "ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz"
-wget -c "http://www.mpfr.org/mpfr-3.0.1/mpfr-3.0.1.tar.xz"
-wget -c "ftp://ftp.gnu.org/gnu/gmp/gmp-5.0.5.tar.xz"
+wget -c "http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz"
+#wget -c "https://ftp.gnu.org/gnu/gcc/gcc-4.8.5/gcc-4.8.5.tar.bz2"
+wget -c "https://ftp.gnu.org/gnu/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
+wget -c "ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
+wget -c "http://www.mpfr.org/mpfr-current/mpfr-3.1.4.tar.xz"
+wget -c "ftp://ftp.gnu.org/gnu/gmp/gmp-6.1.0.tar.xz"
