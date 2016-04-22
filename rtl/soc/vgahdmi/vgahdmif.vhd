5a59ba88034b4236e4e9e2a60badb4f0ebfd6e1e
Emard <vordah@gmail.com>
2016-04-22 21:31:20 +0200

    vgahdmi video output using vga2dvid.vhd (not using internal tmds encoder from vgahdmi)

--- rtl/soc/vgahdmi/vgahdmif.vhd
+++ rtl/soc/vgahdmi/vgahdmif.vhd
@@ -27,7 +27,7 @@ entity vgahdmi is
     red_byte, green_byte, blue_byte, bright_byte: in  std_logic_vector(7 downto 0); -- pixel data from FIFO
     vga_r, vga_g, vga_b:  out std_logic_vector(7 downto 0); -- VGA video signal
     vga_hsync, vga_vsync: out std_logic; -- VGA sync
-    vga_vblank: out std_logic; -- Video blank
+    vga_vblank, vga_blank: out std_logic; -- Video blank
     line_repeat: out std_logic;
     TMDS_out_RGB : out std_logic_vector(2 downto 0) -- HDMI output
   );
@@ -47,7 +47,7 @@ architecture syn of vgahdmi is
       red_byte, green_byte, blue_byte, bright_byte: in  std_logic_vector(7 downto 0); -- pixel data from FIFO
       vga_r, vga_g, vga_b:  out std_logic_vector(7 downto 0); -- VGA video signal
       vga_hsync, vga_vsync: out std_logic; -- VGA sync, positive logic: active HIGH
-      vga_vblank: out std_logic; -- VGA vertical blank, positive logic: active HIGH
+      vga_vblank, vga_blank: out std_logic; -- VGA vertical blank, positive logic: active HIGH
       line_repeat: out std_logic;
       TMDS_out_RGB : out std_logic_vector(2 downto 0) -- HDMI output
     );
@@ -74,6 +74,7 @@ begin
       vga_hsync => vga_hsync,
       vga_vsync => vga_vsync,
       vga_vblank => vga_vblank,
+      vga_blank => vga_blank,
       line_repeat => line_repeat,
       TMDS_out_RGB => TMDS_out_RGB
   );
