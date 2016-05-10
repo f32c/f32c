2f86017aa8fcaaf7618664e0c002cc16e969deae
Marko Zec <zec@fer.hr>
2016-05-10 13:26:02 +0200

    SMP test prints out each CPU's ID in an infite loop.
    CPUs other than #0 seem to have issues with reliable startup, but on
    a 4-way SMP SoC this particular test program apparently works fine.

--- src/examples/hello_smp/hello.c
+++ src/examples/hello_smp/hello.c
@@ -5,8 +5,11 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <dev/io.h>
+#include <dev/sio.h>
 
 #include <mips/asm.h>
 #include <mips/cpuregs.h>
@@ -26,18 +29,48 @@ void
 main(void)
 {
 	int in, out = 0;
-	int tmp, cpuid;
+	uint32_t tmp, cpuid;
+	char c;
 
-        mfc0_macro(tmp, MIPS_COP_0_CONFIG);
+	mfc0_macro(tmp, MIPS_COP_0_CONFIG);
 	cpuid = tmp & 0xf;
 
 	if (cpuid == 0) {
+		/* Wake up all CPUs */
 		printf("Hello, f32c/%s world!\n", arch);
-		/* Wake up other CPUs, and exit */
-		OUTW(IO_CPU_RESET, 0);
-	} else do {
-		OUTB(IO_LED, out >> 20);
-		out++;
+		OUTB(IO_CPU_RESET, 0);
+	}
+
+	if (cpuid == 4) {
+		OUTB(IO_CPU_RESET, 1);
+	}
+
+#if 1
+	tmp = 0x80180000 + 0x1000 * cpuid;
+	__asm __volatile__(
+		"move $29, %0;"	/* set SP */
+		:
+		: "r" (tmp)
+	);
+#endif
+
+	/* It's a mystery why we need this to fire up CPU #3 */
+	OUTB(IO_CPU_RESET, 0);
+
+	tmp = 1 << cpuid;
+	do {
 		INB(in, IO_PUSHBTN);
-	} while ((in & BTN_ANY) == 0);
+		if ((in & BTN_ANY) == 0)
+			OUTB(IO_LED, tmp);
+		out += (cpuid + 13);
+		if (out > 0x1000000) {
+			OUTB(IO_SIO_BYTE, ' ');
+			do {
+				INB(c, IO_SIO_STATUS);
+			} while (c & SIO_TX_BUSY);
+			c = '0' + cpuid;
+			out = 0;
+			OUTB(IO_SIO_BYTE, c);
+		}
+	} while (1);
 }
