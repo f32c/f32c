e17171bfd0136c83e3294f7f7260d28d08358de3
Emard <vordah@gmail.com>
2016-06-07 15:37:14 +0200

    numato mimas v2 ddr2 correct config

--- rtl/xilinx/numato/ddr2/config_pkg.vhd
+++ rtl/xilinx/numato/ddr2/config_pkg.vhd
@@ -8,5 +8,13 @@ use ieee.numeric_std.all;
 library work;
 
 package config is
-constant cfg_ddr_ck_cycle: natural := 1000; -- check is this value correct
+  constant CFG_CLK_BITLINK_PERIOD_NS : integer := 8;
+  constant CFG_CLK_CPU_DIVIDE : integer := 20;
+  constant CFG_CLK_CPU_PERIOD_NS : integer := 20;
+  constant CFG_CLK_MEM_2X_DIVIDE : integer := 10;
+  constant CFG_CLK_MEM_PERIOD_NS : integer := 20;
+  constant CFG_DDRDQ_WIDTH : integer := 16;
+  constant CFG_DDR_CK_CYCLE : integer := 20;
+  constant CFG_DDR_READ_SAMPLE_TM : integer := 2;
+  constant CFG_SA_WIDTH : integer := 13;
 end package;
