fc28453699ba0e035cc503a49533ca17ba6a50ab
Emard <vordah@gmail.com>
2016-06-18 13:02:17 +0200

    vecotor: simplify vector I/O to bram interface

--- rtl/soc/vector/axi_vector_dma.vhd
+++ rtl/soc/vector/axi_vector_dma.vhd
@@ -10,57 +10,83 @@ use work.axi_pack.all;
 
 -- Vector I/O DMA module for AXI bus
 
--- at rising edge of the strobe
--- following signals are latched: addr, wr
--- store_data must remain on the bus until next_data becomes 1
--- after next_data=1, in next clk cycle store_data must change into the next value
+-- once activated, this unit will become
+-- master to both bram and to external RAM
+-- (AXI in this case)
 
--- the RAM is dereferenced and when next data are needed,
--- the request_next is set to '1'
+-- using a given ram address, it will
+-- load or store vectors to/from ram
+-- determine the end of
+-- data and provide signal bit when done
 
--- vector.vhd module must in next clock cycle respond to next_data signal
--- either by providing next data to store
--- or consuming provided load data storing them into vector
+-- FSM for propagating RAM pointer structs
+-- should be separated from this module
+-- to use in non-axi configuration
 
--- a simplification:
--- don't allow asynchronous cancel of operation but
--- provide counter of max vector length
--- when this counter exceeds, RAM transfer will stop
-
--- also provide signal for laste element in the vector
+-- at rising edge of the request
+-- following signals are latched: addr, store_mode
 
 entity axi_vector_dma is
   generic
   (
     C_vaddr_bits: integer := 11; -- bits that represent max vector length e.g. 11 -> 2^11 -> 2048 elements
+    C_vdata_bits: integer := 32;
     C_burst_max: integer := 64
   );
   port
   (
     clk: in std_logic;
-    addr: in std_logic_vector(29 downto 2) := (others => '0'); -- vector struct address
-    strobe: in std_logic := '0'; -- hold strobe while data available, release to cancel I/O operation
-    store_mode: in std_logic := '0'; -- '1' write to RAM (store mode), '0' read from RAM (load mode)
-    next_data: out std_logic; -- at '1' upstream module must provide next store data, or be ready to accept next load data
-    last_data: out std_logic := '0'; -- parallel to next_data issue the last_data when end of RAM data or reach full vector size 
-    store_data: in std_logic_vector(31 downto 0) := (others => '0'); -- vector to RAM
-    load_data: out std_logic_vector(31 downto 0); -- RAM to vector
-    axi_in: in T_axi_miso; -- axi bus interface
+
+    -- vector processor control
+    addr: in std_logic_vector(29 downto 2) := (others => '0'); -- vector struct address in RAM
+    request: in std_logic := '0'; -- hold request while data available, release to cancel I/O operation
+    store_mode: in std_logic := '0'; -- '1' write to RAM (vector store mode), '0' read from RAM (vector load mode)
+    done: out std_logic := '0';
+
+    -- bram interface
+    bram_we: out std_logic := '0'; -- bram write enable
+    bram_addr: out std_logic_vector(C_vaddr_bits-1 downto 0);
+    bram_wdata: out std_logic_vector(C_vdata_bits-1 downto 0);
+    bram_rdata: in std_logic_vector(C_vdata_bits-1 downto 0);
+
+    -- AXI ram interface
+    axi_in: in T_axi_miso;
     axi_out: out T_axi_mosi
   );
 end axi_vector_dma;
 
 architecture arch of axi_vector_dma is
-  signal R_a: std_logic_vector(15 downto 0);
+  signal R_store_mode: std_logic;
+  signal R_addr: std_logic_vector(29 downto 2);
+  signal R_bram_addr: std_logic_vector(C_vaddr_bits downto 0) := (others => '1'); -- external counter for RAM load/store
+  signal R_state: integer range 0 to 1 := 0;
 begin
   process(clk)
   begin
     if rising_edge(clk) then
-      R_a <= R_a + 1;
+      if R_state=0 then
+        if request='1' then
+          R_addr <= addr;
+          R_store_mode <= store_mode;
+          R_bram_addr <= (others => '0');
+          R_state <= 1;
+        end if;
+      end if;
+      if R_state=1 then
+        if R_bram_addr(C_vaddr_bits)='1' then
+          R_state <= 0;
+        else
+          R_bram_addr <= R_bram_addr + 1;
+        end if;
+      end if;
     end if;
-    axi_out.araddr <= "00" & addr & "00";
-    axi_out.wdata <= store_data;
-    load_data <= axi_in.rdata;
-    next_data <= '0';
   end process;
+  axi_out.araddr <= "00" & R_addr & "00";
+  bram_wdata <= axi_in.rdata;
+
+  axi_out.awaddr <= "00" & R_addr & "00";
+  axi_out.wdata <= bram_rdata;
+  
+  bram_addr <= R_bram_addr(C_vaddr_bits-1 downto 0);
+  done <= R_bram_addr(C_vaddr_bits); -- MSB bit of bram addr counter means DONE
 end;
