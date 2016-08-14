103286ffa1aa98491b98c1b73bc93c5f4c32c5bb
gornjas <zec@fer.hr>
2016-08-14 11:24:46 +0200

    Add experimental mutiply-accumulate logic (MI32 only).
    On Lattice XP2 this required weird intermediate register placement
    in order to appease STA, since the tool assumes the multipler is
    always clocked on the positive edge, while in reality it is triggered
    on the falling edge, so it was difficult to bring the synthesis and
    STA tools in agreement.  Further experimentation is required to
    determine how will current pipeline constructs synthesize on other
    FPGAs using different synthesis tools.
    
    By default MAC logic is turned off, so unless C_mul_acc is enabled,
    this should be a NOP change.

--- rtl/cpu/idecode_rv32.vhd
+++ rtl/cpu/idecode_rv32.vhd
@@ -60,7 +60,7 @@ entity idecode_rv32 is
 	mem_write: out std_logic;
 	mem_size: out std_logic_vector(1 downto 0);
 	mem_read_sign_extend: out std_logic; -- LB / LH
-	mult, mult_signed, mthi, mtlo: out boolean;
+	mult, mult_signed, madd, mthi, mtlo: out boolean;
 	ll, sc: out boolean;
 	flush_i_line, flush_d_line: out std_logic;
 	latency: out std_logic_vector(1 downto 0);
@@ -139,6 +139,7 @@ begin
 	flush_d_line <= '0';
 	mult <= false;
 	mult_signed <= false;
+	madd <= false;
 	mthi <= false;
 	mtlo <= false;
 	ll <= false;
