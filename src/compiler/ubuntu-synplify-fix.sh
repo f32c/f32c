6890a74311c9c43b534c2b72ff935838fa45fa06
Juraj Oršulić <juraj5@gmail.com>
2016-09-21 14:59:00 +0200

    Update Ubuntu quirks for Lattice Diamond 3.8

--- src/compiler/ubuntu-synplify-fix.sh
+++ src/compiler/ubuntu-synplify-fix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /usr/local/diamond/3.7_x64/synpbase/bin
+cd /usr/local/diamond/3.8_x64/synpbase/bin
 sudo perl -i -pe 's/#!\/bin\/sh/#!\/bin\/bash/g' `ls -p | grep -v /`
 sudo perl -i -pe 's/3\.\* \| 2\.4\.\* \| 2\.6\.\*  \)/4\.\* \| 3\.\* \| 2\.4\.\* \| 2\.6\.\*  \)/g' config/platform_check
