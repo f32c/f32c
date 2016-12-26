d57c3b468362a6c57cd9644b54d98c79fbfc8435
Emard <vordah@gmail.com>
2016-12-26 12:22:27 +0100

    esa11: removing unused projects.

--- rtl/proj/xilinx/esa11/emu_esa11_7a35i/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# convert to svf
-# . /usr/local/xilinx/14.7/ISE_DS/settings32.sh
-# impact -batch ../include/bit2svf.ut
-# openocd -f ../include/ft2232-fpu1.ocd -f esa11.ocd
-xc3sprog -c ft4232h -p 0 default.bit
