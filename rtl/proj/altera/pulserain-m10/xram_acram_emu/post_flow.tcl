a49453910954c32f895dd2cbc490f25b9d381399
Emard <vordah@gmail.com>
2017-11-29 21:35:00 +0100

    pulserain M10: complete post_flow.tcl script to build full_image.pof

--- rtl/proj/altera/pulserain-m10/xram_acram_emu/post_flow.tcl
+++ rtl/proj/altera/pulserain-m10/xram_acram_emu/post_flow.tcl
@@ -1,6 +1,6 @@
 post_message "Generating images for onchip flash..."
 exec quartus_cpf -c dual_image.cof
-#file copy -force f32c_dual_boot.pof full_image.pof
+file copy -force f32c_dual_boot.pof full_image.pof
 file copy -force f32c_dual_boot_cfm1_auto.rpd cfm.bin
-#file copy -force f32c_dual_boot_ufm_auto.rpd ufm.bin
+file copy -force f32c_dual_boot_ufm_auto.rpd ufm.bin
 post_message " -- Flash images: full_image.pof cfm.bin ufm.bin"
