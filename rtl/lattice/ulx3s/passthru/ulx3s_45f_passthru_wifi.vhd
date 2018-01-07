04f942a455c89bac9d23f25f649618cc10a9d763
Emard <vordah@gmail.com>
2018-01-07 18:34:13 +0100

    ulx3s passthru which allows ESP32 access SD card and OLED at the same time

--- rtl/lattice/ulx3s/passthru/ulx3s_45f_passthru_wifi.vhd
+++ rtl/lattice/ulx3s/passthru/ulx3s_45f_passthru_wifi.vhd
@@ -63,8 +63,10 @@ entity ulx3s_passthru_wifi is
   --flash_csn    : out     std_logic;
 
   -- SD card (SPI1)
-  sd_dat3_csn, sd_cmd_di, sd_dat0_do, sd_dat1_irq, sd_dat2: inout std_logic := 'Z';
-  sd_clk: inout std_logic := 'Z';
+  sd_dat0_do: in std_logic := 'Z';
+  sd_dat3_csn, sd_cmd_di: out std_logic := '1';
+  sd_clk: out std_logic := '0';
+  sd_dat1_irq, sd_dat2: inout std_logic := 'Z';
   sd_cdn, sd_wp: inout std_logic := 'Z'
   );
 end;
@@ -72,8 +74,8 @@ end;
 architecture Behavioral of ulx3s_passthru_wifi is
   signal clk: std_logic;
   signal R_blinky: std_logic_vector(26 downto 0);
-  signal S_hspi_miso, S_hspi_mosi, S_hspi_sck, S_hspi_ss: std_logic;
-  signal S_hspi_csn, S_hspi_dc, S_hspi_resn: std_logic;
+  signal S_hspi_miso, S_hspi_mosi, S_hspi_sck: std_logic;
+  signal S_hspi_sd_csn, S_hspi_oled_csn, S_hspi_oled_dc, S_hspi_oled_resn: std_logic;
   signal S_prog_in, S_prog_out: std_logic_vector(1 downto 0);
 begin
   clk <= clk_25MHz;
@@ -101,28 +103,36 @@ begin
   -- wifi_en <= ftdi_nrts;
   -- wifi_gpio0 <= ftdi_ndtr;
 
-  gp(9) <= '0'; -- WiFi GPIO12 selects flash voltage 3.3V
+  gp(9) <= '0' when S_hspi_sd_csn = '1' else S_hspi_miso; -- WiFi GPIO12 selects flash voltage 3.3V
   gn(9) <= 'Z';
   wifi_gpio15 <= 'Z';
   wifi_gpio16 <= 'Z';
 
   g_x: if true generate
   -- OLED display passthru (using pins on J1 shared with wifi)
-  S_hspi_miso <= gp(9); -- wifi gpio12
+  --gp(9) <= S_hspi_miso; -- wifi gpio12
   S_hspi_mosi <= gn(9); -- wifi gpio13
   S_hspi_sck <= gn(10); -- wifi gpio14
-  S_hspi_csn <= wifi_gpio15; -- wifi gpio15
-  S_hspi_dc <= wifi_gpio2; -- wifi gpio25
-  S_hspi_resn <= gn(11); -- wifi gpio26
+  S_hspi_sd_csn <= gn(11); -- wifi gpio26
+  S_hspi_oled_csn <= wifi_gpio15;
+  S_hspi_oled_dc <= wifi_gpio2;
+  S_hspi_oled_resn <= gp(11); -- wifi gpio25
 
-  oled_csn <= S_hspi_csn;
+  oled_csn <= S_hspi_oled_csn;
   oled_clk <= S_hspi_sck;
   oled_mosi <= S_hspi_mosi;
-  oled_dc <= S_hspi_dc;
-  oled_resn <= S_hspi_resn;
+  oled_dc <= S_hspi_oled_dc;
+  oled_resn <= S_hspi_oled_resn;
 
   -- show OLED signals on the LEDs
-  led(4 downto 0) <= S_hspi_csn & S_hspi_dc & S_hspi_resn & S_hspi_mosi & S_hspi_sck;
+  -- led(4 downto 0) <= S_hspi_csn & S_hspi_dc & S_hspi_resn & S_hspi_mosi & S_hspi_sck;
+
+  sd_dat3_csn <= S_hspi_sd_csn;
+  sd_clk <= S_hspi_sck;
+  sd_cmd_di <= S_hspi_mosi;
+  S_hspi_miso <= sd_dat0_do;
+  -- show OLED signals on the LEDs
+  led(6 downto 0) <= S_hspi_sd_csn & S_hspi_oled_csn & S_hspi_oled_resn & S_hspi_oled_dc & S_hspi_miso & S_hspi_mosi & S_hspi_sck;
   
   -- Pushbuttons passthru (using pins on J1 shared with wifi)
   gp(12) <= btn(3); -- up 
