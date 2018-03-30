b848aac68f08ee4d3c250cf029e4e1cea2d29216
Emard <vordah@gmail.com>
2018-03-30 21:08:06 +0200

    ffm toplevel: compiles, does LED fade but uploaded blinkled blinks erractically

--- rtl/altera/ffm/top/top_xram_sdram_vector.vhd
+++ rtl/altera/ffm/top/top_xram_sdram_vector.vhd
@@ -16,8 +16,8 @@ entity ffm_xram_sdram_vector is
 	C_debug: boolean := false;
 	C_branch_prediction: boolean := true;
 
-	-- Main clock: 81/83/112 (83 for hdmi video)
-	C_clk_freq: integer := 50;
+	-- Main clock: 50/75/100
+	C_clk_freq: integer := 75;
 
 	-- SoC configuration options
 	C_bram_size: integer := 2;
@@ -26,7 +26,7 @@ entity ffm_xram_sdram_vector is
         C_acram: boolean := false;
         C_sdram: boolean := true;
 
-        C_vector: boolean := true; -- vector processor unit (wip)
+        C_vector: boolean := false; -- vector processor unit (wip)
         C_vector_axi: boolean := false; -- vector processor bus type (false: normal f32c)
         C_vector_registers: integer := 8; -- number of internal vector registers min 2, each takes 8K
         C_vector_bram_pass_thru: boolean := true; -- Cyclone-V won't compile with pass_thru=false
@@ -36,11 +36,12 @@ entity ffm_xram_sdram_vector is
         C_vector_float_multiply: boolean := true; -- false will not have float multiply (*)
         C_vector_float_divide: boolean := true; -- false will not have float divide (/) will save much LUTs and DSPs
 
+        C_video_mode: integer := 1; -- 0:640x360, 1:640x480, 2:800x480, 3:800x600, 5:1024x768
         C_hdmi_out: boolean := false;
 
-        C_vgahdmi: boolean := true; -- simple VGA bitmap with compositing
+        C_vgahdmi: boolean := false; -- simple VGA bitmap with compositing
         C_vgahdmi_cache_size: integer := 0; -- KB (0 to disable, 2,4,8,16,32 to enable)
-        -- normally this should be  actual bits per pixel
+        -- normally this should be actual bits per pixel
         C_vgahdmi_fifo_data_width: integer range 8 to 32 := 8;
 
 	C_sio: integer := 1;
@@ -72,6 +73,8 @@ entity ffm_xram_sdram_vector is
 	dr_dqm: out std_logic_vector(3 downto 0);
 	dr_d: inout std_logic_vector(31 downto 0);
 	dr_we_n: out std_logic;
+	-- FFM Module IO
+	fio: inout std_logic_vector(93 downto 0);
 	-- HDMI video out
 	vid_d_p, vid_d_n: out std_logic_vector(2 downto 0);
 	vid_clk_p, vid_clk_n: out std_logic
@@ -80,9 +83,22 @@ end;
 
 architecture Behavioral of ffm_xram_sdram_vector is
   signal clk: std_logic;
-  signal clk_pixel, clk_pixel_shift: std_logic;
+  signal clk_pixel, clk_pixel_shift, clk_pixel_shift_n: std_logic;
   signal tmds_rgb: std_logic_vector(2 downto 0);
   signal tmds_clk: std_logic;
+  alias dv_clk: std_logic is fio(32);
+  alias dv_sda: std_logic is fio(33);
+  alias dv_scl: std_logic is fio(34);
+  alias dv_int: std_logic is fio(35);
+  alias dv_de: std_logic is fio(36);
+  alias dv_hsync: std_logic is fio(37);
+  alias dv_vsync: std_logic is fio(38);
+  alias dv_spdif: std_logic is fio(39);
+  alias dv_mclk: std_logic is fio(40);
+  alias dv_i2s: std_logic_vector(3 downto 0) is fio(44 downto 41);
+  alias dv_sclk: std_logic is fio(45);
+  alias dv_lrclk: std_logic is fio(46);
+  alias dv_d: std_logic_vector(23 downto 0) is fio(93 downto 70); -- attention, here must be reverse ordered bits
   signal ram_en             : std_logic;
   signal ram_byte_we        : std_logic_vector(3 downto 0) := (others => '0');
   signal ram_address        : std_logic_vector(31 downto 0) := (others => '0');
@@ -92,21 +108,24 @@ architecture Behavioral of ffm_xram_sdram_vector is
 begin
     -- clock: generic, direct from onboard oscillator
     G_generic_clk:
-    if C_clk_freq /= 81 and C_clk_freq /= 83 generate
+    if C_clk_freq = 50 generate
       clk <= clock_50a;
       clk_pixel <= clock_50a; -- wrong, should be 25 MHz
       clk_pixel_shift <= clock_50a; -- wrong, should be 250 MHz
     end generate;
 
-    --G_83m333_clk: if C_clk_freq = 83 generate
-    --clkgen: entity work.pll_50M_250M_25M_83M333
-    --port map(
-    --  inclk0 => clk_50MHz,      --  50 MHz input from board
-    --  c0 => clk_pixel_shift,  -- 250 MHz
-    --  c1 => clk_pixel,        --  25 MHz
-    --  c2 => clk               --  83.333 MHz
-    --);
-    --end generate;
+    G_75MHz_clk: if C_clk_freq = 75 generate
+    clkgen_75: entity work.clk_125p_125n_25_75_100
+    port map(
+      refclk   => clock_50a,         --   50 MHz input from board
+      rst      => '0',               --    0:don't reset
+      outclk_0 => clk_pixel_shift,   --  125 MHz phase 0
+      outclk_1 => clk_pixel_shift_n, --  125 MHz phase 180
+      outclk_2 => clk_pixel,         --   25 MHz pixel clock
+      outclk_3 => clk,               --   75 MHz CPU clock
+      outclk_4 => open               --  100 MHz unused clock
+    );
+    end generate;
 
     -- generic XRAM glue
     glue_xram: entity work.glue_xram
@@ -135,6 +154,7 @@ begin
       C_vector_float_divide => C_vector_float_divide,
       -- vga simple bitmap
       C_vgahdmi => C_vgahdmi,
+      C_vgahdmi_mode => C_video_mode,
       C_vgahdmi_cache_size => C_vgahdmi_cache_size,
       C_vgahdmi_fifo_data_width => C_vgahdmi_fifo_data_width,
       C_timer => C_timer,
@@ -164,6 +184,12 @@ begin
       sdram_ras => dr_ras_n, sdram_cas => dr_cas_n,
       sdram_cke => dr_cke, sdram_clk => dr_clk,
       sdram_we => dr_we_n, sdram_cs => dr_cs_n,
+      -- ***** VGA *****
+      vga_vsync => dv_vsync,
+      vga_hsync => dv_hsync,
+      vga_r => dv_d(23 downto 16),
+      vga_g => dv_d(15 downto 8),
+      vga_b => dv_d(7 downto 0),
       -- ***** HDMI *****
       dvid_red(0)   => tmds_rgb(2), dvid_red(1)   => open,
       dvid_green(0) => tmds_rgb(1), dvid_green(1) => open,
@@ -172,11 +198,13 @@ begin
       simple_out(0) => led, simple_out(31 downto 1) => open,
       simple_in(1 downto 0) => (others => '0'), simple_in(31 downto 2) => open
     );
-    
+    dv_clk <= clk_pixel;
+
     -- unused RAM upper 16 bits
     dr_dqm(3 downto 2) <= (others => '0');
     dr_d(31 downto 16) <= (others => 'Z');
 
+    G_yes_acram: if C_acram generate
     acram_emulation: entity work.acram_emu
     generic map
     (
@@ -192,6 +220,7 @@ begin
       acram_ready => ram_ready,
       acram_en => ram_en
     );
+    end generate;
 
     -- differential output buffering for HDMI clock and video
     G_hdmi_out: if C_hdmi_out generate
