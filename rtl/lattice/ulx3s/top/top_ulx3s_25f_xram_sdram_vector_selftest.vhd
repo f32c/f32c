66d66d3de2dc2cf2f731c98bd5c6cbd71fbee3f6
Emard <vordah@gmail.com>
2018-09-15 23:12:03 +0200

    ulx3s 25f make it boot from flash

--- rtl/lattice/ulx3s/top/top_ulx3s_25f_xram_sdram_vector_selftest.vhd
+++ rtl/lattice/ulx3s/top/top_ulx3s_25f_xram_sdram_vector_selftest.vhd
@@ -197,6 +197,8 @@ entity ulx3s_xram_sdram_vector is
   sd_d: inout std_logic_vector(3 downto 0);
   sd_clk: inout std_logic := 'Z';
   sd_cdn, sd_wp: in std_logic;
+  
+  flash_wpn, flash_holdn: out std_logic := '1';
 
   nc: inout std_logic_vector(8 downto 0) -- not connected pins to force compiler to use some logic
   );
