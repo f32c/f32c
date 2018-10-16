30655b054976206ff821413cb60e7ce663bc7d52
emard <vordah@gmail.com>
2018-10-16 09:55:54 +0200

    tb299 upload updated

--- rtl/proj/xilinx/tb299/xram_acram_emu/upload.sh
+++ rtl/proj/xilinx/tb299/xram_acram_emu/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # openocd --file=interface/altera-usb-blaster.cfg --file=xc6slx9.ocd
-openocd --file=remote.ocd --file=xc6slx9.ocd
+openocd_ft232r --file=../../include/ft232r.ocd --file=xc6slx9.ocd
