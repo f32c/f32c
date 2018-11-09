dd46b837282bb4bcfa8efaab0a3b537a475dd9cb
emard <vordah@gmail.com>
2018-11-09 01:44:50 +0100

    spi.vhd reworked. SPI bus waveform has changed for non-turbo mode hopefully now according to flash and sd-card datasheets: Update mosi at falling edge of the spi clock. Sample miso at rising edge of the spi clock.

--- rtl/soc/spi.vhd
+++ rtl/soc/spi.vhd
@@ -60,31 +60,43 @@ end spi;
 --
 architecture Behavioral of spi is
     signal R_bit_cnt: std_logic_vector(3 downto 0);
-    signal R_spi_byte: std_logic_vector(7 downto 0);
+    signal R_spi_byte_in, R_spi_byte_out: std_logic_vector(7 downto 0);
     signal R_clk_div: std_logic_vector(7 downto 0) := x"80";
     signal R_clk_acc: std_logic_vector(7 downto 0);
-    signal R_cen: std_logic := '1'; -- SPI disabled by default
-
+    signal R_spi_cen: std_logic := '1'; -- SPI disabled by default
+    signal clk_acc_next: std_logic_vector(7 downto 0);
+    signal R_clk_prev: std_logic;
 begin
     bus_out(31 downto 9) <= (others => '-');
     bus_out(8) <= R_bit_cnt(3);
-    bus_out(7 downto 0) <= R_spi_byte;
+    bus_out(7 downto 0) <= R_spi_byte_in;
 
-    spi_cen <= R_cen;
-    spi_sck <= not clk and not R_bit_cnt(3) when C_turbo_mode else R_clk_acc(7);
-    spi_mosi <= R_spi_byte(7);
+    spi_cen <= R_spi_cen;
+
+    G_not_turbo_mode:
+    if not C_turbo_mode generate
+    spi_sck <= R_clk_acc(7);
+    spi_mosi <= R_spi_byte_out(7);
+
+    G_yes_fixed_speed:
+    if C_fixed_speed generate
+	clk_acc_next <= R_clk_acc xor x"80";
+    end generate G_yes_fixed_speed;
+
+    G_not_fixed_speed:
+    if not C_fixed_speed generate
+	clk_acc_next <= R_clk_acc + R_clk_div;
+    end generate G_not_fixed_speed;
 
     process(clk)
-	variable clk_acc_next: std_logic_vector(7 downto 0);
     begin
-	if not C_turbo_mode then
-	    if C_fixed_speed then
-		clk_acc_next := R_clk_acc xor x"80";
-	    else
-		clk_acc_next := R_clk_acc + R_clk_div;
-	    end if;
+	if rising_edge(clk) then
+	    R_clk_prev <= R_clk_acc(7);
 	end if;
+    end process;
 
+    process(clk)
+    begin
 	if rising_edge(clk) then
 	    -- bus interface logic
 	    if ce = '1' and bus_write = '1' then
@@ -93,30 +105,63 @@ begin
 			R_clk_div <= bus_in(15 downto 8);
 		    end if;
 		    R_clk_acc <= (others => '0');
-		    R_cen <= '1';
-		    R_bit_cnt <= x"7";
+		    R_spi_cen <= '1';
+		    R_bit_cnt <= x"8"; -- spi inactive
 		elsif byte_sel(0) = '1' then
-		    R_spi_byte <= bus_in(7 downto 0);
-		    R_bit_cnt <= x"0";
-		    R_cen <= '0';
+		    R_spi_byte_out <= bus_in(7 downto 0);
+		    R_bit_cnt <= x"0"; -- spi active
+		    R_spi_cen <= '0';
 		end if;
-	    end if;
-
-	    -- tx / rx logic
-	    if R_bit_cnt(3) = '0' then
-		if C_turbo_mode then
-		    R_spi_byte <= R_spi_byte(6 downto 0) & spi_miso;
-		    R_bit_cnt <= R_bit_cnt + 1;
-		else
-		    -- sample input on falling clock edge
-		    if clk_acc_next(7) = '0' and R_clk_acc(7) = '1' then
-			R_spi_byte <= R_spi_byte(6 downto 0) & spi_miso;
-			R_bit_cnt <= R_bit_cnt + 1;
+	    else -- not write
+	    	-- tx / rx logic
+		if R_bit_cnt(3) = '0' then -- spi active
+		    -- rising edge of spi clock
+		    if R_clk_prev = '0' and R_clk_acc(7) = '1' then
+		    	-- sample input and shift input reg
+		    	R_spi_byte_in <= R_spi_byte_in(6 downto 0) & spi_miso;
+		    	R_bit_cnt <= R_bit_cnt + 1;
 		    end if;
+		    -- falling edge of spi clock
+		    if R_clk_prev = '1' and R_clk_acc(7) = '0' then
+		    	-- shift output reg
+		    	R_spi_byte_out <= R_spi_byte_out(6 downto 0) & '0';
+		    end if;
+		    R_clk_acc <= clk_acc_next;
 		end if;
-		R_clk_acc <= clk_acc_next;
-	    end if;
-	end if;
+	    end if; -- not write
+	end if; -- rising edge
     end process;
+    end generate G_not_turbo_mode;
+
+    G_yes_turbo_mode:
+    if C_turbo_mode generate
+    spi_sck <= (not clk) and not R_bit_cnt(3);
+    spi_mosi <= R_spi_byte_out(7);
+    process(clk)
+    begin
+	if rising_edge(clk) then
+	    -- bus interface logic
+	    if ce = '1' and bus_write = '1' then
+		if byte_sel(1) = '1' then
+		    R_clk_acc <= (others => '0');
+		    R_spi_cen <= '1';
+		    R_bit_cnt <= x"8";
+		elsif byte_sel(0) = '1' then
+		    R_spi_byte_out <= bus_in(7 downto 0);
+		    R_bit_cnt <= x"0";
+		    R_spi_cen <= '0';
+		end if;
+	    else -- not write, rising cpu edge (but falling spi edge)
+	    	-- tx / rx logic
+	    	if R_bit_cnt(3) = '0' then -- spi active
+		    R_spi_byte_in <= R_spi_byte_in(6 downto 0) & spi_miso;
+		    R_spi_byte_out <= R_spi_byte_out(6 downto 0) & '0';
+		    R_bit_cnt <= R_bit_cnt + 1;
+		    R_clk_acc <= clk_acc_next;
+		end if;
+	    end if; -- not write, rising cpu edge (but falling spi edge)
+	end if; -- rising edge
+    end process;
+    end generate G_yes_turbo_mode;
 end Behavioral;
 
