3be8d336a2853bd849f044f47741f537c6d4ecb2
emard <vordah@gmail.com>
2019-01-18 17:51:56 +0100

    ulx3s: fixing toplevels for external spi

--- rtl/lattice/ulx3s/top/top_ulx3s_25f_xram_sdram_vector.vhd
+++ rtl/lattice/ulx3s/top/top_ulx3s_25f_xram_sdram_vector.vhd
@@ -34,17 +34,28 @@ entity ulx3s_xram_sdram_vector is
     C_boot_write_protect: boolean := true; -- true default, may leave boot block writeable to save some LUTs
     C_boot_rom_data_bits: integer := 32; -- number of bits in output from bootrom_emu
     C_boot_spi: boolean := true; -- SPI bootloader is larger and allows setting of baudrate
+    --  RAM    C_xram_base  C_PC_mask    C_cached_addr_bits  C_bram_size  C_xboot_rom  Comment
+    --  32 MB  "8"          x"81ffffff"  25                  2            false        ULX3S default
+    --  32 MB  "0"          x"01ffffff"  25                  0            true         XRAM only, no BRAM
+    --   1 MB  "8"          x"800fffff"  20                  2            false        ULX2S simulaton
+    --   1 MB  "1"          x"100fffff"  20                  2            false        custom
+    -- RAM start address x"8" -> 0x80000000 (needs C_PC_mask x"800fffff" for 1MB or x"81ffffff" for 32MB)
     C_xram_base: std_logic_vector(31 downto 28) := x"8"; -- 8 default for C_xboot_rom=false, 0 for C_xboot_rom=true, sets XRAM base address
+    C_PC_mask: std_logic_vector(31 downto 0) := x"81ffffff"; -- ULX3S default 32MB
+    -- C_PC_mask: std_logic_vector(31 downto 0) := x"800fffff"; -- ULX2S simulation 1MB
+    C_cached_addr_bits: integer := 25; -- ULX3S default 32MB
+    -- C_cached_addr_bits: integer := 20; -- ULX2S simulation 1MB
     C_acram: boolean := false; -- false default (ulx3s has sdram chip)
     C_acram_wait_cycles: integer := 3; -- 3 or more
     C_acram_emu_kb: integer := 128; -- KB axi_cache emulation (power of 2)
     C_sdram: boolean := true; -- true default
     C_icache_size: integer := 2; -- 2 default
     C_dcache_size: integer := 2; -- 2 default
-    C_cached_addr_bits: integer := 25; -- lower address bits than C_cached_addr_bits are cached
     C_branch_prediction: boolean := false; -- false default
     C_sio: integer := 2; -- 2 default
-    C_spi: integer := 2; -- 2 default
+    C_spi: integer := 4; -- 2 default
+    C_spi_turbo_mode: std_logic_vector := "0000";
+    C_spi_fixed_speed: std_logic_vector := "1100";
     C_simple_io: boolean := true; -- true default
     C_gpio: integer := 64; -- 64 default for ulx3s
     C_gpio_pullup: boolean := false; -- false default
@@ -54,10 +65,11 @@ entity ulx3s_xram_sdram_vector is
     C_synth: boolean := false; -- Polyphonic synth
       C_synth_zero_cross: boolean := true; -- volume changes at zero-cross, spend 1 BRAM to remove clicks
       C_synth_amplify: integer := 0; -- 0 for 24-bit digital reproduction, 5 for PWM (clipping possible)
-    C_spdif: boolean := false; -- SPDIF output
+    C_spdif: boolean := true; -- SPDIF output
     C_cw_simple_out: integer := 7; -- 7 default, simple_out bit for 433MHz modulator. -1 to disable. for 433MHz transmitter set (C_framebuffer => false, C_dds => false)
 
-    C_passthru_autodetect: boolean := true; -- false: normal, true: autodetect programming of ESP32 and passthru serial port
+    -- enabling passthru autodetect reduces fmax or vector divide must be disabled on 45f
+    C_passthru_autodetect: boolean := false; -- false: normal, true: autodetect programming of ESP32 and passthru serial port
     C_passthru_clk_Hz: real := 25.0E6; -- passthru state machine uses 25 MHz clock
     C_passthru_break: real := 10.0E-3; -- seconds (approximately) to detect serial break and enter f32c mode
 
@@ -185,16 +197,16 @@ entity ulx3s_xram_sdram_vector is
   gpdi_scl, gpdi_sda: inout std_logic;
 
   -- Flash ROM (SPI0)
-  -- commented out because it can't be used as GPIO
-  -- when bitstream is loaded from config flash
-  --flash_miso   : in      std_logic;
-  --flash_mosi   : out     std_logic;
-  --flash_clk    : out     std_logic;
-  --flash_csn    : out     std_logic;
+  flash_miso   : in      std_logic;
+  flash_mosi   : out     std_logic;
+  --flash_clk    : out     std_logic; -- not GPIO, needs vendor-specific module
+  flash_csn    : out     std_logic;
+  flash_holdn  : out     std_logic := '1';
+  flash_wpn    : out     std_logic := '1';
 
   -- SD card (SPI1)
   sd_cmd: inout std_logic := 'Z';
-  sd_d: inout std_logic_vector(3 downto 0);
+  sd_d: inout std_logic_vector(3 downto 0) := (others => 'Z');
   sd_clk: inout std_logic := 'Z';
   sd_cdn, sd_wp: in std_logic;
 
@@ -237,12 +249,15 @@ architecture Behavioral of ulx3s_xram_sdram_vector is
   signal S_rom_valid: std_logic;
 
   -- dual ESP32/f32c programming mode
+  alias wifi_rxd2: std_logic is wifi_gpio16;
+  alias wifi_txd2: std_logic is wifi_gpio17;
   signal S_rxd, S_txd: std_logic; -- mix USB and WiFi
   signal S_prog_in, S_prog_out: std_logic_vector(1 downto 0);
   signal R_esp32_mode: std_logic := '0';
   constant C_break_counter_bits: integer := 1+ceil_log2(integer(C_passthru_clk_Hz*C_passthru_break));
   signal R_break_counter: std_logic_vector(C_break_counter_bits-1 downto 0) := (others => '0');
   signal S_f32c_sd_csn, S_f32c_sd_clk, S_f32c_sd_miso, S_f32c_sd_mosi: std_logic;
+  signal S_flash_csn, S_flash_clk: std_logic;
 
   component OLVDS
     port(A: in std_logic; Z, ZN: out std_logic);
@@ -287,8 +302,8 @@ begin
      );
   end generate;
 
-  ddr_640x480_100MHz: if C_clk_freq=125 and (C_video_mode=0 or C_video_mode=1) generate
-  clk_100M: entity work.clk_25_100_125_25
+  ddr_640x480_125MHz: if C_clk_freq=125 and (C_video_mode=0 or C_video_mode=1) generate
+  clk_125M: entity work.clk_25_100_125_25
     port map(
       CLKI        =>  clk_25MHz,
       CLKOP       =>  clk_pixel_shift,   -- 125 MHz
@@ -372,6 +387,11 @@ begin
     ftdi_rxd <= S_txd;
     wifi_rxd <= S_txd;
     wifi_gpio0 <= btn(0); -- pressing BTN0 will escape to ESP32 file select menu
+    sd_d(3) <= S_f32c_sd_csn;
+    sd_clk <= S_f32c_sd_clk;
+    S_f32c_sd_miso <= sd_d(0);
+    sd_cmd <= S_f32c_sd_mosi;
+    sd_d(2 downto 1) <= (others => '1');
   end generate;
   
   -- hold pushbutton BTN1 to upload to f32c over USB
@@ -387,6 +407,7 @@ begin
     C_boot_write_protect => C_boot_write_protect,
     C_boot_spi => C_boot_spi,
     C_branch_prediction => C_branch_prediction,
+    C_PC_mask => C_PC_mask,
     C_acram => C_acram,
     C_acram_wait_cycles => C_acram_wait_cycles,
     C_sdram => C_sdram,
@@ -406,6 +427,8 @@ begin
     C_debug => C_debug,
     C_sio => C_sio,
     C_spi => C_spi,
+    C_spi_turbo_mode => C_spi_turbo_mode,
+    C_spi_fixed_speed => C_spi_fixed_speed,
     C_gpio => C_gpio,
     C_gpio_pullup => C_gpio_pullup,
     C_gpio_adc => C_gpio_adc,
@@ -484,13 +507,15 @@ begin
     sio_break(0) => rs232_break,
     sio_break(1) => rs232_break2,
 
-    spi_sck(0)  => open,  spi_sck(1)  => S_f32c_sd_clk,   -- sd_clk,
-    spi_ss(0)   => open,  spi_ss(1)   => S_f32c_sd_csn,   -- sd_d(3),
-    spi_mosi(0) => open,  spi_mosi(1) => S_f32c_sd_mosi,  -- sd_cmd,
-    spi_miso(0) => '0',   spi_miso(1) => S_f32c_sd_miso,  -- sd_d(0),
+    spi_ss(0)   => S_flash_csn,  spi_ss(1)   => S_f32c_sd_csn,   spi_ss(2)   => oled_csn,   spi_ss(3)   => gn(24),
+    spi_sck(0)  => S_flash_clk,  spi_sck(1)  => S_f32c_sd_clk,   spi_sck(2)  => oled_clk,   spi_sck(3)  => gn(21),
+    spi_mosi(0) => flash_mosi,   spi_mosi(1) => S_f32c_sd_mosi,  spi_mosi(2) => oled_mosi,  spi_mosi(3) => gn(23),
+    spi_miso(0) => flash_miso,   spi_miso(1) => S_f32c_sd_miso,  spi_miso(2) => open,       spi_miso(3) => gn(22),
 
     gpio(127 downto 28+32) => open,
-    gpio(27+32 downto 32) => gn(27 downto 0),
+    gpio(27+32 downto 25+32) => gn(27 downto 25),
+    gpio(24+32 downto 21+32) => open,
+    gpio(20+32 downto 32) => gn(20 downto 0),
     gpio(31 downto 30) => open,
     gpio(29) => gpdi_sda,
     gpio(28) => gpdi_scl,
@@ -502,11 +527,11 @@ begin
     simple_out(15) => open,
     simple_out(14) => open, -- wifi_en
     simple_out(13) => shutdown,
-    simple_out(12) => oled_csn,
+    simple_out(12) => open,
     simple_out(11) => oled_dc,
     simple_out(10) => oled_resn,
-    simple_out(9) => oled_mosi,
-    simple_out(8) => oled_clk,
+    simple_out(9) => open,
+    simple_out(8) => open,
     simple_out(7 downto 0) => led(7 downto 0),
     simple_in(31 downto 21) => (others => '0'),
     simple_in(20) => adc_miso,
@@ -514,7 +539,7 @@ begin
     simple_in(15 downto 7) => (others => '0'),
     simple_in(6 downto 0) => btn,
 
-    -- v1.7: 2 MSB audio channel bits should not be used or board resets.
+    -- v1.7: 2 MSB audio channel bits are not used in "default" setup.
     --audio_l(3 downto 2) => audio_l(1 downto 0),
     --audio_r(3 downto 2) => audio_r(1 downto 0),
     -- 4-bit could be used down to 75 ohm load
@@ -629,5 +654,13 @@ begin
       gpdi_diff: OLVDS port map(A => ddr_d(i), Z => gpdi_dp(i), ZN => gpdi_dn(i));
     end generate;
   end generate;
+
+  flash_clock: entity work.ecp5_flash_clk
+  port map
+  (
+    flash_csn => rs232_break,
+    flash_clk => S_flash_clk
+  );
+  flash_csn <= S_flash_csn;
 
 end Behavioral;
--- rtl/lattice/ulx3s/top/top_ulx3s_45f_xram_sdram_433.vhd
+++ rtl/lattice/ulx3s/top/top_ulx3s_45f_xram_sdram_433.vhd
@@ -44,7 +44,9 @@ entity ulx3s_xram_sdram_vector is
     C_dcache_size: integer := 2; -- 2 default
     C_branch_prediction: boolean := false; -- false default
     C_sio: integer := 2; -- 2 default
-    C_spi: integer := 2; -- 2 default
+    C_spi: integer := 4; -- 2 default
+    C_spi_turbo_mode: std_logic_vector := "0000";
+    C_spi_fixed_speed: std_logic_vector := "1100";
     C_simple_io: boolean := true; -- true default
     C_gpio: integer := 64; -- 64 default for ulx3s
     C_gpio_pullup: boolean := false; -- false default
@@ -244,6 +246,7 @@ architecture Behavioral of ulx3s_xram_sdram_vector is
   constant C_break_counter_bits: integer := 1+ceil_log2(integer(C_passthru_clk_Hz*C_passthru_break));
   signal R_break_counter: std_logic_vector(C_break_counter_bits-1 downto 0) := (others => '0');
   signal S_f32c_sd_csn, S_f32c_sd_clk, S_f32c_sd_miso, S_f32c_sd_mosi: std_logic;
+  signal S_flash_csn, S_flash_clk: std_logic;
 
   component OLVDS
     port(A: in std_logic; Z, ZN: out std_logic);
@@ -378,6 +381,8 @@ begin
     C_debug => C_debug,
     C_sio => C_sio,
     C_spi => C_spi,
+    C_spi_turbo_mode => C_spi_turbo_mode,
+    C_spi_fixed_speed => C_spi_fixed_speed,
     C_gpio => C_gpio,
     C_gpio_pullup => C_gpio_pullup,
     C_gpio_adc => C_gpio_adc,
@@ -457,13 +462,14 @@ begin
     sio_break(0) => rs232_break,
     sio_break(1) => rs232_break2,
 
-    spi_sck(0)  => open,  spi_sck(1)  => S_f32c_sd_clk,   -- sd_clk,
-    spi_ss(0)   => open,  spi_ss(1)   => S_f32c_sd_csn,   -- sd_d(3),
-    spi_mosi(0) => open,  spi_mosi(1) => S_f32c_sd_mosi,  -- sd_cmd,
-    spi_miso(0) => '0',   spi_miso(1) => S_f32c_sd_miso,  -- sd_d(0),
+    spi_ss(0)   => S_flash_csn,  spi_ss(1)   => S_f32c_sd_csn,   spi_ss(2)   => oled_csn,   spi_ss(3)   => gn(24),
+    spi_sck(0)  => S_flash_clk,  spi_sck(1)  => S_f32c_sd_clk,   spi_sck(2)  => oled_clk,   spi_sck(3)  => gn(21),
+    spi_mosi(0) => flash_mosi,   spi_mosi(1) => S_f32c_sd_mosi,  spi_mosi(2) => oled_mosi,  spi_mosi(3) => gn(23),
+    spi_miso(0) => flash_miso,   spi_miso(1) => S_f32c_sd_miso,  spi_miso(2) => open,       spi_miso(3) => gn(22),
 
     gpio(127 downto 28+32) => open,
-    gpio(27+32 downto 32) => gn(27 downto 0),
+    gpio(27+32 downto 25+32) => gn(27 downto 25),
+    gpio(24+32 downto 21+32) => open,
     gpio(31 downto 30) => open,
     gpio(29) => gpdi_sda,
     gpio(28) => gpdi_scl,
@@ -475,11 +481,11 @@ begin
     simple_out(15) => open,
     simple_out(14) => open, -- wifi_en
     simple_out(13) => shutdown,
-    simple_out(12) => oled_csn,
+    simple_out(12) => open,
     simple_out(11) => oled_dc,
     simple_out(10) => oled_resn,
-    simple_out(9) => oled_mosi,
-    simple_out(8) => oled_clk,
+    simple_out(9) => open,
+    simple_out(8) => open,
     simple_out(7 downto 0) => led(7 downto 0),
     simple_in(31 downto 21) => (others => '0'),
     simple_in(20) => adc_miso,
@@ -602,5 +608,13 @@ begin
       gpdi_diff: OLVDS port map(A => ddr_d(i), Z => gpdi_dp(i), ZN => gpdi_dn(i));
     end generate;
   end generate;
+
+  flash_clock: entity work.ecp5_flash_clk
+  port map
+  (
+    flash_csn => rs232_break,
+    flash_clk => S_flash_clk
+  );
+  flash_csn <= S_flash_csn;
 
 end Behavioral;
