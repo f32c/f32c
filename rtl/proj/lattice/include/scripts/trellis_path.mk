3f8d32ccae03f6e052a58deeb03a04495b90fd80
emard <vordah@gmail.com>
2019-04-08 18:59:35 +0200

    ulx3s proj commit forgotten trellis_path.mk

---
+++ rtl/proj/lattice/include/scripts/trellis_path.mk
@@ -0,0 +1,9 @@
+# ******* tools installation paths *******
+# https://github.com/ldoolitt/vhd2vl
+VHDL2VL ?= /mt/scratch/tmp/openfpga/vhd2vl/src/vhd2vl
+# https://github.com/YosysHQ/yosys
+YOSYS ?= /mt/scratch/tmp/openfpga/yosys/yosys
+# https://github.com/YosysHQ/nextpnr
+NEXTPNR-ECP5 ?= /mt/scratch/tmp/openfpga/nextpnr/nextpnr-ecp5
+# https://github.com/SymbiFlow/prjtrellis
+TRELLIS ?= /mt/scratch/tmp/openfpga/prjtrellis
