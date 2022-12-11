154dcd6738426984ee2eb768e86007eccab9ddde
Marko Zec <zec@fer.hr>
2022-12-11 12:13:03 +0100

    Update the interrupt handler showcase by including the "wait" instruction, which could have stucked the CPU forever if the reset would arrive, before this was fixed in 96fa3ba9869968bf419e2b477e898fdd875603ff.

--- src/examples/intr_test/intr.c
+++ src/examples/intr_test/intr.c
@@ -1,10 +1,8 @@
 /*
- * Print out something each second from interrupt context.  Even after
+ * Print out something each 1/2 second from interrupt context.  Even after
  * the program terminates once a button gets pressed, interrupts remain
  * enabled so messages will continue to be displayed while the bootloader
  * processes serial input.
- *
- * $Id$
  */
 
 #include <stdio.h>
@@ -16,7 +14,7 @@
 
 #define BTN_ANY (BTN_CENTER | BTN_UP | BTN_DOWN | BTN_LEFT | BTN_RIGHT)
 
-static uint32_t next_t, freq_khz, tsc_hi, tsc_lo;
+static uint32_t next_t, freq_khz, tsc_lo;
 
 
 static int
@@ -25,14 +23,12 @@ tsc_update(void)
 	int32_t tsc;
 
 	mfc0_macro(tsc, MIPS_COP_0_COUNT);
-	if (tsc < tsc_lo)
-		tsc_hi++;
 
 	printf("%d ticks passed since the last interrupt.\n", tsc - tsc_lo);
 
 	tsc_lo = tsc;
 
-	next_t = next_t + freq_khz * 1000;
+	next_t = next_t + freq_khz * 500;
 	mtc0_macro(next_t, MIPS_COP_0_COMPARE);
 
 	return (1);
@@ -54,15 +50,15 @@ main(void)
 	printf("Clock ticks at %f MHz.\n", freq_khz / 1000.0);
 
 	mfc0_macro(tmp, MIPS_COP_0_COUNT);
-	next_t = tmp + freq_khz * 1000;
+	next_t = tmp + freq_khz * 500;
 	mtc0_macro(next_t, MIPS_COP_0_COMPARE);
 
 	isr_register_handler(7, &tick_isr);
 	asm("ei");
 
 	do {
-		OUTB(IO_LED, out >> 20);
-		out++;
+		OUTB(IO_LED, out++);
+		asm("wait");
 		INB(in, IO_PUSHBTN);
 	} while ((in & BTN_ANY) == 0);
 }
