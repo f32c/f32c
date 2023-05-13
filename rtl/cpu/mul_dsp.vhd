b9f6c2f29d301748a62d9cbd94bb8b5aa7ff8906
Marko Zec <zec@fer.hr>
2023-05-13 21:49:07 +0200

    Add one cycle of latency to the DSP multiplier, aiming at relaxeing timing closure pressure.  This permits us to cleanly pass PnR with DSP multiplier on ECP5 at above 90 MHz (using Lattice Diamond 3.10).  The extra cycle is clearly visible in benchmarks, but having nice benchmark scores which work correctly at reasonable Fmax only on (now ancient) Lattice XP2 was crippling us by far too long on other, more modern platforms.

--- rtl/cpu/mul_dsp.vhd
+++ rtl/cpu/mul_dsp.vhd
@@ -43,18 +43,13 @@ architecture arch_x of mul is
     signal R_mul_res: signed(66 downto 0);
     signal R_mul_x: signed(32 downto 0);
     signal R_mul_y: signed(33 downto 0);
-    signal R_mul_run: std_logic;
+    signal R_mul_done: boolean;
 begin
-
-    done <= true; -- this is a single-cycle multiplier
 
     process(clk)
     begin
-	if rising_edge(clk) then
-	    R_mul_run <= clk_enable;
-	end if;
-	if falling_edge(clk) then
-	    if clk_enable = '1' or R_mul_run = '1' then
+	if rising_edge(clk) and clk_enable = '1' then
+	    if start then
 		R_mul_x(31 downto 0) <= CONV_SIGNED(UNSIGNED(x), 32);
 		R_mul_y(31 downto 0) <= CONV_SIGNED(UNSIGNED(y), 32);
 		if mult_signed then
@@ -67,11 +62,13 @@ begin
 		if mthi then
 		    R_mul_y(32) <= '1';
 		end if;
-		R_mul_res <= R_mul_x * R_mul_y;
 	    end if;
+	    R_mul_done <= not start;
+	    R_mul_res <= R_mul_x * R_mul_y;
 	end if;
     end process;
 
     hi_lo(63 downto 32) <= conv_std_logic_vector(R_mul_res(63 downto 32), 32);
     hi_lo(31 downto 0) <= conv_std_logic_vector(R_mul_res(31 downto 0), 32);
+    done <= R_mul_done and not start;
 end arch_x;
