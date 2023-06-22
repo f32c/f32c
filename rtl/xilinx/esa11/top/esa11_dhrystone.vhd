4dda2db8949147eefdb635a06d79d1a4a043201a
Marko Zec <zec@fer.hr>
2023-06-22 23:08:53 +0200

    Prune unused debugging signal.

--- rtl/xilinx/esa11/top/esa11_dhrystone.vhd
+++ rtl/xilinx/esa11/top/esa11_dhrystone.vhd
@@ -131,7 +131,7 @@ begin
 	debug_in_data => x"00", debug_in_strobe => '0',
 	debug_in_busy => open, debug_out_data => open,
 	debug_out_strobe => open, debug_out_busy => '0',
-	debug_debug => open, debug_active => open
+	debug_active => open
     );
     final_to_cpu <= io_to_cpu when io_addr_strobe = '1' else dmem_to_cpu;
     io_addr_strobe <= dmem_addr_strobe when dmem_addr(31 downto 30) = "11"
