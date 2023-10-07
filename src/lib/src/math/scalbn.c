43bcb5fee0da7c902609ae912ebbcbacbae59f0b
gornjas <zec@fer.hr>
2023-10-07 10:58:42 +0200

    src/lib: import more math cruft from FreeBSD

--- src/lib/src/math/scalbn.c
+++ src/lib/src/math/scalbn.c
@@ -1,33 +1,47 @@
-#include "math_private.h"
+/*
+ * Copyright (c) 2005-2020 Rich Felker, et al.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Please see https://git.musl-libc.org/cgit/musl/tree/COPYRIGHT
+ * for all contributors to musl.
+ */
+#include <float.h>
+#include <math.h>
+#include <stdint.h>
 
 double scalbn(double x, int n)
 {
-	double scale;
+	union {double f; uint64_t i;} u;
+	double_t y = x;
 
 	if (n > 1023) {
-		x *= 0x1p1023;
+		y *= 0x1p1023;
 		n -= 1023;
 		if (n > 1023) {
-			x *= 0x1p1023;
+			y *= 0x1p1023;
 			n -= 1023;
-			if (n > 1023) {
-				STRICT_ASSIGN(double, x, x * 0x1p1023);
-				return x;
-			}
+			if (n > 1023)
+				n = 1023;
 		}
 	} else if (n < -1022) {
-		x *= 0x1p-1022;
-		n += 1022;
+		/* make sure final n < -53 to avoid double
+		   rounding in the subnormal range */
+		y *= 0x1p-1022 * 0x1p53;
+		n += 1022 - 53;
 		if (n < -1022) {
-			x *= 0x1p-1022;
-			n += 1022;
-			if (n < -1022) {
-				STRICT_ASSIGN(double, x, x * 0x1p-1022);
-				return x;
-			}
+			y *= 0x1p-1022 * 0x1p53;
+			n += 1022 - 53;
+			if (n < -1022)
+				n = -1022;
 		}
 	}
-	INSERT_WORDS(scale, (uint32_t)(0x3ff+n)<<20, 0);
-	STRICT_ASSIGN(double, x, x * scale);
+	u.i = (uint64_t)(0x3ff+n)<<52;
+	x = y * u.f;
 	return x;
 }
+
+#if (LDBL_MANT_DIG == 53) && !defined(scalbn)
+__weak_reference(scalbn, ldexpl);
+__weak_reference(scalbn, scalbnl);
+#endif
