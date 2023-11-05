9f1a10adcc42e51c064ab0a11f4b34083f2054b2
gornjas <zec@fer.hr>
2023-11-05 10:53:24 +0100

    rtl/soc: add a few exotic ulx3s-specific frequencies to the HZ table.

--- rtl/soc/rtc.vhd
+++ rtl/soc/rtc.vhd
@@ -58,14 +58,20 @@ architecture x of rtc is
 		eff_hz := 33333333;
 	    elsif mhz = 66 or mhz = 67 then
 		eff_hz := 66666667;
+	    elsif mhz = 74 then
+		eff_hz := 74250000;
 	    elsif mhz = 84 then
 		eff_hz := 84375000;
-	    elsif mhz = 93 or mhz = 94 then
+	    elsif mhz = 93 then
+		eff_hz := 92812500;
+	    elsif mhz = 94 then
 		eff_hz := 93750000;
 	    elsif mhz = 96 then
 		eff_hz := 96428571;
 	    elsif mhz = 112 or mhz = 113 then
 		eff_hz := 112500000;
+	    elsif mhz = 124 then
+		eff_hz := 123750000;
 	    elsif mhz = 133 then
 		eff_hz := 133333333;
 	    elsif mhz = 166 or mhz = 167 then
