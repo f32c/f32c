19976113045458f55eddbbd5cc6f7dcc84f3f6b8
gornjas <zec@fer.hr>
2023-11-25 15:37:58 +0100

    src: lib: move strcpy() to libc instead of inlining it via string.h.

--- src/include/string.h
+++ src/include/string.h
@@ -40,104 +40,18 @@ int memcmp(const void *, const void *, size_t) __pure;
 #define	strncpy(dst, src, len) __builtin_strncpy((dst), (src), (len))
 #define	strrchr(buf, ch) __builtin_strrchr((buf), (ch))
 
-#ifdef USE_BUILTIN_STRCPY
+int strcmp(const char * __restrict, const char * __restrict);
 #define	strcpy(dst, src) __builtin_strcpy((dst), (src))
-#else
-#define	strcpy(dst, src) _strcpy(dst, src);
-#endif
 
 
-static inline int
-strcmp(const char *s1, const char *s2)
+static inline void
+bzero(void *dst, int len)
 {
-	int c1, c2;
-#ifdef FAST_STRCMP
-	int b1, b2;
-	uint32_t v0;
-	const uint32_t t0 = 0x01010101;
-	const uint32_t t1 = 0x80808080;
+	char *cp = (char *) dst;
 
-	/* Check for unaligned pointers */
-	if (__predict_false(((int)s1 | (int)s2) & 0x3)) {
-#endif /* FAST_STRCMP */
-		do {
-			c1 = *(const unsigned char *)s1++;
-			c2 = *(const unsigned char *)s2++;
-		} while (c1 != 0 && c1 == c2);
-		return (c1 - c2);
-#ifdef FAST_STRCMP
-	}
-
-	for (;;) {
-		/* Check whether words are equal */
-		c1 = *((int *)s1);
-		c2 = *((int *)s2);
-		v0 = ((uint32_t)c1) - t0;
-		if (c1 != c2)
-			break;
-		v0 &= t1;
-		/* Check if the word contains any zero bytes */
-		if (v0 && __predict_false(v0 & ~((uint32_t)c1))) 
-			return(0);
-#ifndef UNROLLED_STRCMP
-		s1 += 4;
-		s2 += 4;
-#else
-		/* Check whether words are equal */
-		c1 = *((int *)s1 + 1);
-		c2 = *((int *)s2 + 1);
-		v0 = ((uint32_t)c1) - t0;
-		if (c1 != c2)
-			break;
-		v0 &= t1;
-		/* Check if the word contains any zero bytes */
-		if (v0 && __predict_false(v0 & ~((uint32_t)c1))) 
-			return(0);
-		s1 += 8;
-		s2 += 8;
-#endif
-	}
-
-#if _BYTE_ORDER == _LITTLE_ENDIAN
-	b1 = c1 & 0xff;
-	b2 = c2 & 0xff;
-	if (__predict_false(b1 == 0 || b1 != b2))
-		return (b1 - b2);
-	b1 = c1 & 0xff00;
-	b2 = c2 & 0xff00;
-	if (b1 == 0 || b1 != b2)
-		return (b1 - b2);
-	c1 >>= 16;
-	c2 >>= 16;
-	b1 = c1 & 0xff;
-	b2 = c2 & 0xff;
-	if (__predict_false(b1 == 0 || b1 != b2))
-		return (b1 - b2);
-	b1 = c1 & 0xff00;
-	b2 = c2 & 0xff00;
-	return (b1 - b2);
-#elif _BYTE_ORDER == _BIG_ENDIAN
-	b1 = (c1 >> 24) & 0xff;
-	b2 = (c2 >> 24) & 0xff;
-	if (b1 == 0 || b1 != b2)
-		return (b1 - b2);
-	b1 = (c1 >> 16) & 0xff;
-	b2 = (c2 >> 16) & 0xff;
-	if (b1 == 0 || b1 != b2)
-		return (b1 - b2);
-	b1 = c1 & 0xff00;
-	b2 = c2 & 0xff00;
-	if (b1 == 0 || b1 != b2)
-		return (b1 - b2);
-	b1 = c1 & 0xff;
-	b2 = c2 & 0xff;
-	return (b1 - b2);
-#else
-#error "Unsupported byte order."
-#endif
-#endif /* FAST_STRCMP */
+	while (len--)
+		*cp++ = 0;
 }
-
 
 static inline void *
 _memcpy(void *dst, const void *src, int len)
@@ -158,31 +72,6 @@ _memcpy(void *dst, const void *src, int len)
 		*dst1++ = *src1++;
 
 	return (dst);
-}
-
-
-static inline char *
-_strcpy(char *dst, const char *src)
-{
-	char *ret = dst;
-	int c;
-
-	do {
-		c = *src++;
-		*dst++ = c;
-	} while (c != 0);
-
-	return(ret);
-}
-
-
-static inline void
-bzero(void *dst, int len)
-{
-	char *cp = (char *) dst;
-
-	while (len--)
-		*cp++ = 0;
 }
 
 
