c369a4d2de850624f7990100e60a1dcef0d36a73
gornjas <zec@fer.hr>
2024-07-06 20:47:47 +0200

    rtl/generic: improve SDRAM emulator:
    - sample inbound signals on falling clock edge, change outputs on rising
    
    - configurable CAS delay
    
    - ignore address input changes during read / write burst cycles

--- rtl/generic/sdram_emu.vhd
+++ rtl/generic/sdram_emu.vhd
@@ -5,7 +5,8 @@ use ieee.std_logic_unsigned.all;
 entity sdram_emu is
     generic (
 	C_size_k: natural := 32;
-	C_column_bits: natural := 9
+	C_column_bits: natural := 9;
+	C_cas: natural := 2
     );
     port (
 	clk: in std_logic;
@@ -34,9 +35,11 @@ architecture x of sdram_emu is
 
     signal R_row: std_logic_vector(9 downto 0);
     signal R_bank: std_logic_vector(1 downto 0);
+    signal R_a: std_logic_vector(12 downto 0);
     signal R_c1: std_logic;
-    signal R_from_bram, R_from_bram1: std_logic_vector(15 downto 0);
-    signal R_read_cycle, R_read_cycle1: boolean;
+    signal R_from_bram: std_logic_vector(15 downto 0);
+    signal R_from_bram_2ws, R_from_bram_3ws: std_logic_vector(15 downto 0);
+    signal R_read_cycle: std_logic_vector(C_cas - 1 downto 0);
     signal R_write_cycle: boolean;
 
     signal cmd: std_logic_vector(3 downto 0);
@@ -44,21 +47,22 @@ architecture x of sdram_emu is
 
 begin
     cmd <= csn & rasn & casn & wen;
-    ea <= conv_integer(R_row & R_bank & a(C_column_bits - 1 downto 0) & R_c1);
+    ea <= conv_integer(R_row & R_bank & a(C_column_bits - 1 downto 0) & '0')
+      when R_c1 = '0'
+      else conv_integer(R_row & R_bank & R_a(C_column_bits - 1 downto 0) & '1');
 
     process(clk)
     begin
     if rising_edge(clk) then
-	R_read_cycle <= false;
-	R_read_cycle1 <= R_read_cycle;
-	d <= (others => 'Z');
-	R_from_bram <= M_bram_hi(ea) & M_bram_lo(ea);
-	R_from_bram1 <= R_from_bram;
-	R_c1 <= '0';
-
-	if R_read_cycle or R_read_cycle1 then
-	    d <= R_from_bram;
+	R_write_cycle <= false;
+	R_read_cycle <= R_read_cycle(R_read_cycle'high - 1 downto 0) & '0';
+	if R_read_cycle(0) = '1' and R_c1 = '1' then
+	    R_read_cycle(0) <= '1';
 	end if;
+	R_from_bram <= M_bram_hi(ea) & M_bram_lo(ea);
+	R_from_bram_2ws <= R_from_bram;
+	R_from_bram_3ws <= R_from_bram_2ws;
+	R_c1 <= '0';
 
 	if R_write_cycle or cmd = C_cmd_write then
 	    if dqm(0) = '0' then
@@ -74,13 +78,29 @@ begin
 	    R_row <= a(9 downto 0);
 	    R_bank <= ba;
 	when C_cmd_read =>
+	    R_a <= a;
 	    R_c1 <= '1';
-	    R_read_cycle <= R_c1 = '0';
+	    R_read_cycle(0) <= '1';
 	when C_cmd_write =>
-	    R_write_cycle <= R_c1 = '0';
+	    R_a <= a;
 	    R_c1 <= '1';
+	    R_write_cycle <= R_c1 = '0';
 	when others =>
 	end case;
+    end if;
+    end process;
+
+    process(clk)
+    begin
+    if falling_edge(clk) then
+	d <= (others => 'Z');
+	if R_read_cycle(R_read_cycle'high) = '1' then
+	    if C_cas = 2 then
+		d <= R_from_bram_2ws;
+	    else
+		d <= R_from_bram_3ws;
+	    end if;
+	end if;
     end if;
     end process;
 end x;
