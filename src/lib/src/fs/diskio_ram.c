ef82dd5cdeea862c5a735c0f82d8811a3b78a9ca
gornjas <zec@fer.hr>
2024-11-14 23:28:09 +0100

    src/lib: reintroduce RAM disk as diskio object

---
+++ src/lib/src/fs/diskio_ram.c
@@ -0,0 +1,114 @@
+/*-
+ * Copyright (c) 2013, 2014 Marko Zec
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include <fatfs/ff.h>
+#include <fatfs/diskio.h>
+
+
+#define	RAMDISK_SS	512
+
+
+static void
+ram_cpy(void *dst, const void *src, int seccnt)
+{
+	const uint32_t *r = src;
+	uint32_t *w = dst;
+	int cnt = seccnt * RAMDISK_SS / 4 / 4;
+	uint32_t a, b, c, d;
+
+	do {
+		a = r[0]; b = r[1]; c = r[2]; d = r[3];
+		r = &r[4];
+		w[0] = a; w[1] = b; w[2] = c; w[3] = d;
+		w = &w[4];
+	} while (--cnt != 0);
+}
+
+
+DSTATUS
+ramdisk_init_status(diskio_t di)
+{
+
+	if (di->priv_ptr == NULL || di->priv_data == 0)
+		return(STA_NOINIT);
+	return(0);
+}
+
+
+DRESULT
+ramdisk_read(diskio_t di, BYTE *buf, LBA_t sector, UINT count)
+{
+	char *ramdisk = di->priv_ptr;
+
+	ram_cpy(buf, &ramdisk[sector * RAMDISK_SS], count);
+	return (RES_OK);
+}
+
+
+DRESULT
+ramdisk_write(diskio_t di, const BYTE *buf, LBA_t sector, UINT count)
+{
+	char *ramdisk = di->priv_ptr;
+
+	ram_cpy(&ramdisk[sector * RAMDISK_SS], buf, count);
+	return (RES_OK);
+}
+
+
+DRESULT
+ramdisk_ioctl(diskio_t di, BYTE cmd, void *buf)
+{
+	WORD *up = buf;
+
+	switch (cmd) {
+	case GET_SECTOR_SIZE:
+		*up = RAMDISK_SS;
+		return (RES_OK);
+#ifndef DISKIO_RO
+	case GET_SECTOR_COUNT:
+		*up = di->priv_data / RAMDISK_SS;
+		return (RES_OK);
+	case GET_BLOCK_SIZE:
+		*up = 1;
+		return (RES_OK);
+#endif /* !DISKIO_RO */
+	case CTRL_SYNC:
+		return (RES_OK);
+	default:
+		return (RES_ERROR);
+	}
+}
+
+
+struct diskio_sw ramdisk_sw = {
+        .read	= ramdisk_read,
+        .write	= ramdisk_write,
+        .ioctl	= ramdisk_ioctl,
+        .status	= ramdisk_init_status,
+        .init	= ramdisk_init_status,
+};
