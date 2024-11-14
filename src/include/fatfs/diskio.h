78e3b94dc781fde4a68bd7fdf3159814b50f14a0
gornjas <zec@fer.hr>
2024-11-14 23:26:32 +0100

    src/include/diskio.h: introduce structs dskio_sw and diskio_inst

--- src/include/fatfs/diskio.h
+++ src/include/fatfs/diskio.h
@@ -44,9 +44,9 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 
 /* Generic command (Used by FatFs) */
 #define CTRL_SYNC			0	/* Complete pending write process (needed at FF_FS_READONLY == 0) */
-#define GET_SECTOR_COUNT	1	/* Get media size (needed at FF_USE_MKFS == 1) */
-#define GET_SECTOR_SIZE		2	/* Get sector size (needed at FF_MAX_SS != FF_MIN_SS) */
-#define GET_BLOCK_SIZE		3	/* Get erase block size (needed at FF_USE_MKFS == 1) */
+#define GET_SECTOR_COUNT		1	/* Get media size (needed at FF_USE_MKFS == 1) */
+#define GET_SECTOR_SIZE			2	/* Get sector size (needed at FF_MAX_SS != FF_MIN_SS) */
+#define GET_BLOCK_SIZE			3	/* Get erase block size (needed at FF_USE_MKFS == 1) */
 #define CTRL_TRIM			4	/* Inform device that the data on the block of sectors is no longer used (needed at FF_USE_TRIM == 1) */
 
 /* Generic command (Not used by FatFs) */
@@ -56,19 +56,54 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 #define CTRL_FORMAT			8	/* Create physical format on the media */
 
 /* MMC/SDC specific ioctl command */
-#define MMC_GET_TYPE		10	/* Get card type */
+#define MMC_GET_TYPE			10	/* Get card type */
 #define MMC_GET_CSD			11	/* Get CSD */
 #define MMC_GET_CID			12	/* Get CID */
 #define MMC_GET_OCR			13	/* Get OCR */
-#define MMC_GET_SDSTAT		14	/* Get SD status */
+#define MMC_GET_SDSTAT			14	/* Get SD status */
 #define ISDIO_READ			55	/* Read data form SD iSDIO register */
 #define ISDIO_WRITE			56	/* Write data to SD iSDIO register */
 #define ISDIO_MRITE			57	/* Masked write data to SD iSDIO register */
 
 /* ATA/CF specific ioctl command */
 #define ATA_GET_REV			20	/* Get F/W revision */
-#define ATA_GET_MODEL		21	/* Get model name */
+#define ATA_GET_MODEL			21	/* Get model name */
 #define ATA_GET_SN			22	/* Get serial number */
+
+
+/* f32c diskio bindings, MZ 2024 */
+
+struct diskio_inst;
+typedef struct diskio_inst *diskio_t;
+
+typedef DSTATUS d_init(diskio_t);
+typedef DSTATUS d_status(diskio_t);
+typedef DRESULT d_read(diskio_t, BYTE *, LBA_t, UINT);
+typedef DRESULT d_write(diskio_t, const BYTE *, LBA_t, UINT);
+typedef DRESULT d_ioctl(diskio_t, BYTE, void *);
+
+struct diskio_sw {
+	d_read		*read;
+	d_write		*write;
+	d_ioctl		*ioctl;
+	d_status	*status;
+	d_init		*init;
+};
+
+struct diskio_inst {
+	const struct diskio_sw *sw;	/* diskio methods */
+	uint16_t	unit_major;	/* device id, major */
+	uint16_t	unit_minor;	/* device id, minor */
+	char		*prefix;	/* voume id, such as "C:" */
+	uint32_t	priv_data;	/* opaque private data */
+	void		*priv_ptr;	/* opaque private data */
+};
+
+void diskio_register(diskio_t);
+
+#if defined(_FS_READONLY) && _FS_READONLY == 1
+#define	DISKIO_RO
+#endif
 
 #ifdef __cplusplus
 }
