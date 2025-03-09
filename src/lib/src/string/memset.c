e15df0b30ba041ab2790059d7968afa28b11590e
gornjas <zec@fer.hr>
2025-03-09 11:08:13 +0100

    src/lib: optimize memset()

--- src/lib/src/string/memset.c
+++ src/lib/src/string/memset.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2013 Marko Zec, University of Zagreb
+ * Copyright (c) 2013, 2025 Marko Zec
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -21,16 +21,33 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $Id$
  */
 
-__attribute__((optimize("-Os"))) void *
-memset(char *b, int c, unsigned int len)
+void *
+memset(void *dest, int c, size_t len)
 {
+	char *cp = dest;
+	uint32_t val;
 
-	for (; len != 0; len--)
-		*b++ = c;
+	val = c & 0xff;
+	val |= val << 8;
+	val |= val << 16;
 
-	return (b);
+	for (; (((int) cp & 0x3) != 0) && len > 0; len--)
+		*cp++ = val;
+
+	for (; len >= 16; len -= 16, cp += 16) {
+		*((int *) &cp[0]) = val;
+		*((int *) &cp[4]) = val;
+		*((int *) &cp[8]) = val;
+		*((int *) &cp[12]) = val;
+	}
+
+	for (; len >= 4; len -= 4, cp += 4)
+		*((int *) cp) = val;
+
+	for (; len > 0; len--)
+		*cp++ = val;
+
+	return (dest);
 }
