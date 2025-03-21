8a6d45d04086261e5e1372207fd2eaccbe678f4d
Marko Zec <mzec@koncar-institut.hr>
2025-03-21 13:56:53 +0100

    src/lib: import gmtime_r() and timegm() from FreeBSD

--- src/lib/src/stdtime/time.c
+++ src/lib/src/stdtime/time.c
@@ -29,9 +29,6 @@
 #include <dev/io.h>
 
 
-static char month_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-
 time_t
 time(time_t *res)
 {
@@ -48,61 +45,3 @@ time(time_t *res)
 		*res = t;
 	return (t);
 }
-
-
-/*
- * A crude approximation of gmtime() and friends, valid from 1970 through 2099
- */
-struct tm *
-gmtime_r(const time_t *restrict time, struct tm *restrict res) {
-	uint32_t sec, day, month, year;
-	uint32_t four_yrs, days, leap;
-
-	if (time == NULL || res == NULL)
-		return (NULL);
-
-	sec = *time;
-	day = sec / 86400;
-	sec = sec % 86400;
-
-	res->tm_wday = (day + 4) % 7;
-
-	four_yrs = day / (366 + 3 * 365);
-	day = day % (366 + 3 * 365);
-	for (year = 0, leap = 0; year < 4; year++) {
-		if (year == 2) {
-			leap = 1;
-			days = 366;
-		} else
-			days = 365;
-		if (day < days)
-			break;
-		day -= days;
-	}
-	res->tm_yday = day;
-
-	year += 1970 + four_yrs * 4;
-	for (month = 0; month < 12; month++) {
-		days = month_days[month];
-		if (month == 1)
-			days += leap;
-		if (day < days)
-			break;
-		day -= days;
-	}
-
-	res->tm_year = year - 1900;
-	res->tm_mon = month;
-	res->tm_mday = day + 1;
-
-	res->tm_hour = sec / (60 * 60);
-	res->tm_min = sec % (60 * 60) / 60;
-	res->tm_sec = sec % 60;
-
-	res->tm_isdst = 0;
-	res->tm_gmtoff = 0;
-	res->tm_zone = NULL;
-
-	return (res);
-}
-
