dfb50b931ded823d227392802bcbe882b26016cc
gornjas <zec@fer.hr>
2025-04-16 11:59:36 +0200

    rtl/lattice: ulx3s: add ESP32 control logic to sdram_min toplevel
    C/P from sdram_dv toplevel, which requires around 1100 LUTs more
    than sdram_min

--- rtl/lattice/ulx3s/top/top_sdram_min.vhd
+++ rtl/lattice/ulx3s/top/top_sdram_min.vhd
@@ -54,9 +54,11 @@ entity top_sdram is
 	-- SIO0 (FTDI)
 	rs232_tx: out std_logic;
 	rs232_rx: in std_logic;
+	rs232_rts: in std_logic;
+	rs232_dtr: in std_logic;
 
 	-- Digital Video (differential outputs)
-	gpdi_dp, gpdi_dn: out std_logic_vector(3 downto 0);
+	gpdi_dp: out std_logic_vector(3 downto 0);
 
 	-- i2c shared for digital video and RTC
 	gpdi_scl, gpdi_sda: inout std_logic;
@@ -81,6 +83,19 @@ entity top_sdram is
 	adc_mosi: inout std_logic;
 	adc_miso: inout std_logic;
 
+	-- ESP32
+	esp32_rxd: out std_logic;
+	esp32_txd: in std_logic;
+	esp32_en: inout std_logic := 'Z';
+	esp32_gpio0: inout std_logic := 'Z';
+	esp32_gpio19: inout std_logic := 'Z';
+	esp32_gpio21: inout std_logic := 'Z';
+	esp32_gpio22: inout std_logic := 'Z';
+	esp32_gpio25: inout std_logic := 'Z';
+	esp32_gpio26: inout std_logic := 'Z';
+	esp32_gpio27: inout std_logic := 'Z';
+	esp32_gpio35: inout std_logic := 'Z';
+
 	-- PCB antenna
 	ant: out std_logic;
 
@@ -90,15 +105,24 @@ entity top_sdram is
 end top_sdram;
 
 architecture x of top_sdram is
-    signal clk, pll_lock: std_logic;
+    signal clk: std_logic;
     signal clk_123m75, clk_112m5: std_logic;
     signal clk_92m8125, clk_90m, clk_74m25: std_logic;
-    signal reset: std_logic;
-    signal sio_break: std_logic;
+    signal reset, pll_lock: std_logic;
+    signal f32c_rxd, f32c_txd, sio_break: std_logic;
+    signal sio_sel: std_logic_vector(3 downto 0);
     signal flash_sck: std_logic;
     signal flash_csn: std_logic;
 
     signal R_simple_in: std_logic_vector(19 downto 0);
+
+    constant C_esp32_cnt_max: natural := C_clk_freq * 1000 * 10; -- 10 ms
+    signal R_esp32_cnt: natural range 0 to C_esp32_cnt_max := C_esp32_cnt_max;
+    signal R_rts, R_dtr: std_logic;
+    signal R_esp32_pwrup_wait: boolean := true;
+    signal R_esp32_en, R_esp32_gpio0: boolean;
+    signal cons_f32c: boolean;
+    signal cons_esp32: boolean;
 
 begin
     -- f32c SoC
@@ -133,8 +157,8 @@ begin
 	sdram_cas => sdram_casn,
 	sdram_addr => sdram_a,
 	sdram_data => sdram_d,
-	sio_rxd(0) => rs232_rx,
-	sio_txd(0) => rs232_tx,
+	sio_rxd(0) => f32c_rxd,
+	sio_txd(0) => f32c_txd,
 	sio_break(0) => sio_break,
 	simple_in => R_simple_in,
 	simple_out => led,
@@ -182,4 +206,66 @@ begin
       else clk_25m when C_clk_freq = 25
       else '0';
     reset <= not pll_lock or sio_break;
+
+    I_sbrk_cnt: entity work.sbrk_cnt
+    generic map (
+	C_clk_freq_hz => C_clk_freq * 1000000
+    )
+    port map (
+	clk => clk,
+	rxd => rs232_rx,
+	sel => sio_sel
+    );
+    cons_esp32 <= sio_sel = x"0";
+    cons_f32c <= sio_sel = x"1";
+
+    -- SIO -> f32c
+    f32c_rxd <= rs232_rx when cons_f32c else '1';
+
+    -- SIO -> ESP32
+    esp32_rxd <= rs232_rx when cons_esp32 else '1';
+
+    -- ESP32, f32c -> SIO
+    rs232_tx <= esp32_txd when cons_esp32 else f32c_txd when cons_f32c else '1';
+
+    --
+    -- ESP32 reset logic.  We emulate the following hardware circuit:
+    -- DTR 1, RTS 0: pull down EN, a small capacitor holds it low for a while
+    -- DTR 0, RTS 1: pull down IO0
+    --
+    -- At powerup we MUST generate a reset pulse, otherwise ESP32 won't
+    -- boot reliably (occasionally gets stuck in bootloader), because
+    -- ULX3S doesn't have a built-in capacitor between EN pin and GND.
+    --
+    process(clk)
+    begin
+    if rising_edge(clk) then
+	R_rts <= rs232_rts;
+	R_dtr <= rs232_dtr;
+	if R_esp32_pwrup_wait then
+	    if R_esp32_cnt = 0 then
+		R_esp32_pwrup_wait <= false;
+		R_esp32_en <= false;
+	    else
+		R_esp32_cnt <= R_esp32_cnt - 1;
+		R_esp32_en <= true;
+	    end if;
+	elsif cons_esp32 and R_rts = '0' and R_dtr = '1' then
+	    R_esp32_en <= true;
+	    R_esp32_gpio0 <= true;
+	    R_esp32_cnt <= C_esp32_cnt_max;
+	elsif R_esp32_cnt = 0 then
+	    R_esp32_gpio0 <= false;
+	else
+	    R_esp32_cnt <= R_esp32_cnt - 1;
+	    if R_esp32_cnt = C_esp32_cnt_max / 2 then
+		R_esp32_en <= false;
+	    end if;
+	end if;
+    end if;
+    end process;
+
+    esp32_en <= '0' when R_esp32_en else 'Z';
+    esp32_gpio0 <= '1' when R_esp32_pwrup_wait
+      else rs232_dtr when R_esp32_gpio0 else 'Z';
 end x;
